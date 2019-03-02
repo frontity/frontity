9b8c267c37693b4b9ef3b844bb1822b27a688d66
Luis Herranz <luisherranz@gmail.com>
2019-03-02 22:24:01 +0100

    build(core): add typescript to babel

--- packages/core/src/config/babel.config.ts
+++ packages/core/src/config/babel.config.ts
@@ -1,49 +1,51 @@
-import { Env, Mode, BabelConfig } from './types';
+import { TransformOptions } from "babel-core";
+import { Env, Mode, BabelConfigs } from "./types";
 
 const targets = {
   module: { esmodules: true },
   es5: {
     browsers: [
-      'and_chr >= 67',
-      'and_ff >= 18',
-      'and_uc >= 11.8',
-      'android >= 67',
-      'not android <= 4.4.4',
-      'chrome >= 49',
-      'edge >= 12',
-      'firefox >= 18',
-      'ios_saf >= 10',
-      'not op_mini all',
-      'op_mob >= 46',
-      'opera >= 36',
-      'safari >= 10',
-      'samsung >= 5',
-    ],
+      "and_chr >= 67",
+      "and_ff >= 18",
+      "and_uc >= 11.8",
+      "android >= 67",
+      "not android <= 4.4.4",
+      "chrome >= 49",
+      "edge >= 12",
+      "firefox >= 18",
+      "ios_saf >= 10",
+      "not op_mini all",
+      "op_mob >= 46",
+      "opera >= 36",
+      "safari >= 10",
+      "samsung >= 5"
+    ]
   },
-  node: { node: '8.10' },
+  node: { node: "8.10" }
 };
 
-export default (mode: Mode): BabelConfig => {
-  const getConfig = (env: Env) => {
+export default (mode: Mode): BabelConfigs => {
+  const getConfig = (env: Env): TransformOptions => {
     const presets = [
-      ['env', { useBuiltIns: 'usage', targets: targets[env] }],
-      'react',
+      "typescript",
+      ["env", { useBuiltIns: "usage", targets: targets[env] }],
+      "react"
     ];
     const plugins = [
-      'transform-object-rest-spread',
-      'transform-class-properties',
+      "transform-object-rest-spread",
+      "transform-class-properties"
     ];
-    if (mode === 'development' && env === 'module')
-      plugins.push('react-hot-loader/babel');
+    if (mode === "development" && env === "module")
+      plugins.push("react-hot-loader/babel");
     return {
       presets,
-      plugins,
+      plugins
     };
   };
 
   return {
-    module: getConfig('module'),
-    es5: getConfig('es5'),
-    node: getConfig('node'),
+    module: getConfig("module"),
+    es5: getConfig("es5"),
+    node: getConfig("node")
   };
 };
