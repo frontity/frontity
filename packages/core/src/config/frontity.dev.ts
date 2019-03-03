c3f1d41db073a56552fb0019d25ed9d5b47f26b4
Luis Herranz <luisherranz@gmail.com>
2019-03-03 11:02:32 +0100

    refactor(core): remove old config files

--- packages/core/src/config/frontity.dev.ts
+++
@@ -1,9 +0,0 @@
-import FrontityConfig from '.';
-
-const config = (config: FrontityConfig) => {
-  config.staticHeaders['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-  config.dynamicHeaders['Cache-Control'] =
-    'no-cache, no-store, must-revalidate';
-};
-
-export default config;
