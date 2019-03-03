af168e80d70d94804f7da2cb6c8335cf807b6256
Luis Herranz <luisherranz@gmail.com>
2019-03-03 20:45:31 +0100

    build(core): dev script starting webpack [WIP]

--- packages/core/src/scripts/dev.ts
+++ packages/core/src/scripts/dev.ts
@@ -1,15 +1,28 @@
-import { ensureDir, emptyDir } from 'fs-extra';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackHotServerMiddleware from 'webpack-hot-server-middleware';
-import clientConfig from '../webpack/client.dev';
-import serverConfig from '../webpack/server.dev';
-import { createApp } from './app';
+import { ensureDir, emptyDir } from "fs-extra";
+import * as webpack from "webpack";
+import * as webpackDevMiddleware from "webpack-dev-middleware";
+import * as webpackHotMiddleware from "webpack-hot-middleware";
+import webpackHotServerMiddleware = require("webpack-hot-server-middleware");
+import getConfig from "../config";
+import { Mode } from "../types";
+import { createApp } from "./express";
 
-const buildDir = 'build';
+const buildDir = "build";
 
-const dev = async ({ isHttps }: { isHttps: boolean }): Promise<void> => {
+// Disable Webpack deprecation warning:
+// (node:33456) DeprecationWarning: Tapable.plugin is deprecated. Use new API on `.hooks` instead
+// @ts-ignore
+process.noDeprecation = true;
+
+const dev = async ({
+  isHttps,
+  mode,
+  port
+}: {
+  port: number;
+  isHttps: boolean;
+  mode: Mode;
+}): Promise<void> => {
   // Create the build directory if it doesn't exist.
   await ensureDir(buildDir);
 
@@ -17,20 +30,36 @@ const dev = async ({ isHttps }: { isHttps: boolean }): Promise<void> => {
   await emptyDir(buildDir);
 
   // Start dev using webpack dev server with express.
-  const { app, done } = await createApp();
+  const { app, done } = await createApp({ mode, port, isHttps });
 
-  const compiler = webpack([clientConfig, serverConfig]);
+  // Get FrontityConfig for webpack.
+  const frontityConfig = getConfig({ mode });
+
+  // Start a custom webpack-dev-server.
+  const compiler = webpack([
+    frontityConfig.webpack.module,
+    frontityConfig.webpack.node
+  ]);
   const clientCompiler = compiler.compilers[0];
-  const options = { stats: { colors: true, progress: true } };
+  const options = {
+    stats: { colors: true, progress: true },
+    writeToDisk: true
+  };
   app.use(webpackDevMiddleware(compiler, options));
   app.use(webpackHotMiddleware(clientCompiler));
   app.use(webpackHotServerMiddleware(compiler));
-  compiler.plugin('done', done);
+  compiler.plugin("done", done);
 };
 
-process.on('unhandledRejection', (error: Error) => {
+process.on("unhandledRejection", (error: Error) => {
   console.error(error);
   process.exit(1);
+});
+
+dev({
+  mode: "development",
+  port: 3000,
+  isHttps: false
 });
 
 export default dev;
--- packages/core/src/scripts/express.ts
+++ packages/core/src/scripts/express.ts
@@ -1,26 +1,20 @@
-import { readFile } from 'fs-extra';
-import express, {
-  Express,
-  Router,
-  Request,
-  Response,
-  NextFunction,
-} from 'express';
-import { Headers, Robots } from '../config/types';
-import * as http from 'http';
-import * as https from 'https';
+import { readFile } from "fs-extra";
+import * as express from "express";
+import { Headers, Robots, Mode } from "../types";
+import * as http from "http";
+import * as https from "https";
 
 // Create HTTP or HTTPS server using a self-signed certificate.
 const createServer = async ({
   app,
-  isHttps = true,
+  isHttps = true
 }: {
-  app: Express;
+  app: express.Express;
   isHttps: boolean;
 }): Promise<http.Server | https.Server> => {
   if (isHttps) {
-    const key = await readFile('../certs/localhost.key');
-    const cert = await readFile('../certs/localhost.cert');
+    const key = await readFile("../certs/localhost.key");
+    const cert = await readFile("../certs/localhost.cert");
     const options: https.ServerOptions = { key, cert };
     return https.createServer(options, app);
   }
@@ -29,9 +23,9 @@ const createServer = async ({
 
 // Add headers from frontity.config.js.
 export const headers = ({ headers }: { headers: Headers }) => (
-  req: Request,
-  res: Response,
-  next: NextFunction
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
 ) => {
   Object.entries(headers).forEach(([key, value]) => {
     res.header(key, value);
@@ -40,10 +34,10 @@ export const headers = ({ headers }: { headers: Headers }) => (
 };
 
 // Return robots from frontity.config.js.
-export const robots = ({ robots }: { robots: Robots }): Router => {
-  const router: Router = Router();
-  router.get('/robots.txt', function(req, res) {
-    res.type('text/plain');
+export const robots = ({ robots }: { robots: Robots }): express.Router => {
+  const router: express.Router = express.Router();
+  router.get("/robots.txt", function(req, res) {
+    res.type("text/plain");
     res.send(robots);
   });
   return router;
@@ -52,39 +46,50 @@ export const robots = ({ robots }: { robots: Robots }): Router => {
 // Serve static files and set the headers for them.
 export const staticFiles = ({
   buildDir,
-  staticHeaders = {},
+  staticHeaders = {}
 }: {
   buildDir: string;
   staticHeaders: Headers;
-}): Router => {
-  const router: Router = Router();
+}): express.Router => {
+  const router: express.Router = express.Router();
   router.get(
-    '/static',
+    "/static",
     express.static(`${buildDir}/static/`, {
       setHeaders: res => {
         Object.entries(staticHeaders).forEach(([key, value]) => {
           res.header(key, value);
         });
-      },
+      }
     })
   );
   return router;
 };
 
-export const createApp = async () => {
+export const createApp = async ({
+  mode,
+  port = 3000,
+  isHttps = false
+}: {
+  mode: Mode;
+  port: number;
+  isHttps: boolean;
+}): Promise<{
+  app: express.Express;
+  done: () => http.Server | https.Server;
+}> => {
   // Create the server.
   const app = express();
   // Create a function to start listening after webpack has finished.
   let isBuilt = false;
-  const server = await createServer(app);
+  const server = await createServer({ app, isHttps });
   const done = () =>
     !isBuilt &&
-    server.listen(process.env.PORT, () => {
+    server.listen(port, () => {
       isBuilt = true;
       console.log(
-        `\nSERVER STARTED (${process.env.MODE}) -- Listening @ ${
-          process.env.HTTPS_SERVER ? 'https' : 'http'
-        }://localhost:${process.env.PORT}`
+        `\nSERVER STARTED (${mode}) -- Listening @ ${
+          isHttps ? "https" : "http"
+        }://localhost:${port}`
       );
     });
   return { app, done };
