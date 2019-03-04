6cab60586b64360d769b61466b2fbe293b134cbf
Luis Herranz <luisherranz@gmail.com>
2019-03-04 19:30:22 +0100

    refactor(core): refactor file

--- packages/core/src/config/webpack/plugins.ts
+++ packages/core/src/config/webpack/plugins.ts
@@ -1,4 +1,8 @@
-import { HotModuleReplacementPlugin, Configuration, optimize } from "webpack";
+import {
+  HotModuleReplacementPlugin,
+  Configuration,
+  optimize
+} from "webpack";
 import LoadablePlugin from "@loadable/webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { Target, Mode } from "../../types";
@@ -20,12 +24,12 @@ export default ({
       openAnalyzer: false
     })
   ];
-  config.push();
   // Support HMR in development. Only needed in client.
   if (target !== "node" && mode === "development")
     config.push(new HotModuleReplacementPlugin());
   // Avoid code splitting in node.
-  if (target === "node")
+  if (target === "node") {
     config.push(new optimize.LimitChunkCountPlugin({ maxChunks: 1 }));
+  }
   return config;
 };
