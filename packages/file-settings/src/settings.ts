61f2d5abc764f697fc8495e02f968c57e9472903
Eduardo Campaña <asrielo@gmail.com>
2019-03-19 17:03:47 +0100

    chore(settings): changes comments
    fixes https://github.com/frontity/frontity/issues/7

--- packages/file-settings/src/settings.ts
+++ packages/file-settings/src/settings.ts
@@ -11,44 +11,41 @@ export default async ({
   name?: string;
   url: string;
 }): Promise<Settings> => {
-  // Import the custom or default settings.
+  // Import the custom or default settings from a file.
   const allSettings: AllSettings = await importSettingsFromFile();
 
-  // If `allSettings` is an object, return it as settings.
+  // 1. Return settings when `allSettings` is an object.
   if (!Array.isArray(allSettings)) return allSettings;
 
-  // If `allSettings` is an array with only one element,
-  // return that element as settings.
+  // 2. Return settings when `allSettings` is an array of only
+  // one element.
   if (allSettings.length === 1) return allSettings[0];
 
-  // If `name` param is passed, use it to find the required settings.
+  // 3. Return settings when `name` param is passed and exist
+  // settings with that name.
   if (name) {
     const settings = allSettings.find(s => s.name === name);
-    // If settings with that name were found, return them.
     if (settings) return settings;
   }
 
-  // Collect all the matches of all the settings in one array.
-  const allMatches = allSettings.reduce((final, current) => {
-    if (current.matches) final = final.concat(current.matches);
-    return final;
-  }, []);
-
-  // Filter the matches that match the `url` param and
-  // sort them by length. Finally, pick the largest one
-  // (as it should be the most specific match).
-  const validMatch = allMatches
+  // 4. Return settings when `matches` match the param `url`.
+  const validMatch = allSettings
+    // Get all matches in one array.
+    .reduce((final, current) => {
+      if (current.matches) final = final.concat(current.matches);
+      return final;
+    }, [])
+    // Filter them to get only the ones that match the `url`
     .filter(match => new RegExp(match).test(url))
+    // Sort them by length and pick the longest one.
     .sort((a, b) => b.length - a.length)[0];
-
   // If a valid match was found return those settings.
   if (validMatch)
     return allSettings.find(
       settings => settings.matches && settings.matches.includes(validMatch)
     );
 
-  // As a last resort, send the first settings without
-  // a match defined in `AllSettings` or, if all of them have matches,
-  // the first one in the array.
+  // 5. Return the first settings without a match defined or,
+  // if all of them have matches, the first settings in the array.
   return allSettings.filter(settings => !settings.matches)[0] || allSettings[0];
 };
