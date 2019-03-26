853bd642181109168651e713134129820960b5e5
Luis Herranz <luisherranz@gmail.com>
2019-03-26 17:01:39 +0100

    refactor(file-settings): declare functions outside other functions

--- packages/file-settings/src/normalizeSettings.ts
+++ packages/file-settings/src/normalizeSettings.ts
@@ -21,27 +21,27 @@ const defaultPackage = {
   active: true
 };
 
+// This function merges the imported settings with
+// the default settings.
+function mergeSettings(s: ImportedMulti): NormalizedMulti;
+function mergeSettings(s: ImportedMono): NormalizedMono;
+function mergeSettings({
+  packages,
+  ...settings
+}: ImportedMulti | ImportedMono): NormalizedMulti | NormalizedMono {
+  return merge(
+    {
+      ...defaultSettings,
+      packages: packages.map(pkg =>
+        merge(defaultPackage, typeof pkg === "string" ? { name: pkg } : pkg)
+      )
+    },
+    settings
+  );
+}
+
 // This function normalizes the imported settings.
 function normalizeSettings(settings: ImportedSettings): Settings {
-  // This function merges the imported settings with
-  // the default settings.
-  function mergeSettings(s: ImportedMulti): NormalizedMulti;
-  function mergeSettings(s: ImportedMono): NormalizedMono;
-  function mergeSettings({
-    packages,
-    ...settings
-  }: ImportedMulti | ImportedMono): NormalizedMulti | NormalizedMono {
-    return merge(
-      {
-        ...defaultSettings,
-        packages: packages.map(pkg =>
-          merge(defaultPackage, typeof pkg === "string" ? { name: pkg } : pkg)
-        )
-      },
-      settings
-    );
-  }
-
   // TODO
   // Default settings and validator from packages
   // should be imported and used with each package.
--- packages/file-settings/src/validateSettings.ts
+++ packages/file-settings/src/validateSettings.ts
@@ -1,21 +1,21 @@
 import { uniq, uniqBy } from "ramda";
 import { ImportedSettings, Package } from "./types";
 
+// This function validates the packages.
+const validatePackages = (packages: (string | Package)[]): void => {
+  // Check that every package has a unique name.
+  const uniquePackages = uniq(
+    packages
+      .map(pkg => (typeof pkg === "string" ? pkg : pkg.name))
+      .filter(pkg => pkg)
+  );
+  if (uniquePackages.length < packages.length) {
+    throw new Error("All the packages must have a unique name.");
+  }
+};
+
 // This function is used to validate the imported settings.
 export default (settings: ImportedSettings): void => {
-  // This function validates the packages.
-  const validatePackages = (packages: (string | Package)[]): void => {
-    // Check that every package has a unique name.
-    const uniquePackages = uniq(
-      packages
-        .map(pkg => (typeof pkg === "string" ? pkg : pkg.name))
-        .filter(pkg => pkg)
-    );
-    if (uniquePackages.length < packages.length) {
-      throw new Error("All the packages must have a unique name.");
-    }
-  };
-
   if (!Array.isArray(settings)) {
     // Validate packages in mono settings.
     validatePackages(settings.packages);
