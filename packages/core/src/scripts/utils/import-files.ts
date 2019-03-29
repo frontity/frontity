40e50dc14539777c54ec97bc68540ee9b75914c8
Luis Herranz <luisherranz@gmail.com>
2019-03-29 14:11:17 +0100

    chore(dev-script): create server imports file

--- packages/core/src/scripts/utils/import-files.ts
+++ packages/core/src/scripts/utils/import-files.ts
@@ -1,21 +1,34 @@
 import { writeFile } from "fs-extra";
-
-const flat = (arr: string[][]): string[] => [].concat(...arr);
+import { flatten, uniq } from "lodash";
 
 const variable = (pkg: string): string => {
   return pkg.replace(/(@|\/|-|\.)/g, "");
 };
 
-const serverTemplate = (packages: string[]): string => {
-  const imports = packages.map(
-    pkg => `import * as ${variable(pkg)} from "${pkg}"`
+const generateFile = async ({
+  filename,
+  packages
+}: {
+  filename: string;
+  packages: string[];
+}): Promise<void> => {
+  let template = "";
+  packages.forEach(
+    pkg => (template += `import * as ${variable(pkg)} from "${pkg}";\n`)
   );
-  return imports.join("\n");
+  template += "\nexport {\n";
+  packages.forEach(pkg => (template += `  ${variable(pkg)},\n`));
+  template += "};";
+  await writeFile(`build/bundling/${filename}.js`, template, "utf8");
 };
 
-export const generateServerFile = async ({ packages }) => {
-  const pkgs = flat(Object.values(packages));
-  const template = serverTemplate(pkgs);
-  console.log(template);
-  await writeFile("build/bundling/server-imports.js", template, "utf8");
+export const generateServerFile = async ({
+  packages
+}: {
+  packages: {
+    [key: string]: string[];
+  };
+}): Promise<void> => {
+  const pkgs = uniq(flatten(Object.values(packages)));
+  await generateFile({ packages: pkgs, filename: "server" });
 };
