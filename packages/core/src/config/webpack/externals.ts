c87cff228ce7ee7fd18a331496dcd6a5e6386c62
Luis Herranz <luisherranz@gmail.com>
2019-04-04 17:36:11 +0200

    build(webpack): replace lodash and ramda for their esmodule versions

--- packages/core/src/config/webpack/externals.ts
+++ packages/core/src/config/webpack/externals.ts
@@ -4,7 +4,12 @@ import { Target } from "../../types";
 export default ({ target }: { target: Target }): Configuration["externals"] => {
   const config: Configuration["externals"] = {};
   if (target === "server") {
+    // Avoid dynamic imports with Koa require.
     config["any-promise"] = "promise-monofill";
+    // Force ramda to be the esModule version to support tree-shaking.
+    config["ramda"] = "ramda/es";
+    // Force lodash to be the esModule version to support tree-shaking.
+    config["lodash"] = "lodash-es";
   }
   return config;
 };
