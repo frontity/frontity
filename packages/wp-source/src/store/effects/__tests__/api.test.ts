dafaaa3cfb90d050c8e413f91ccf90b3f547c1e5
David Arenas <darensr@gmail.com>
2019-04-09 12:41:22 +0200

    test(effects): add tests to api.get

---
+++ packages/wp-source/src/store/effects/__tests__/api.test.ts
@@ -0,0 +1,57 @@
+import api from "../api";
+import fetch from "node-fetch";
+
+jest.mock("node-fetch");
+const mockedFetch = fetch as jest.Mocked<typeof fetch>;
+
+describe("api", () => {
+  test("get from WP.org without params", () => {
+    const endpoint = "posts";
+    const siteUrl = "https://test.frontity.org";
+    api.get({ endpoint, siteUrl });
+    const [[fetchCall]] = mockedFetch.mock.calls.slice(-1);
+    expect(fetchCall).toBe("https://test.frontity.org/wp-json/wp/v2/posts");
+  });
+
+  test("get from WP.org without params 2", () => {
+    const endpoint = "posts/12";
+    const siteUrl = "https://test.frontity.org";
+    api.get({ endpoint, siteUrl });
+    const [[fetchCall]] = mockedFetch.mock.calls.slice(-1);
+    expect(fetchCall).toBe("https://test.frontity.org/wp-json/wp/v2/posts/12");
+  });
+
+  test("get from WP.org with params", () => {
+    const endpoint = "posts";
+    const siteUrl = "https://test.frontity.org";
+    const params = { _embed: true, include: "12,13,14" };
+    api.get({ endpoint, params, siteUrl });
+    const [[fetchCall]] = mockedFetch.mock.calls.slice(-1);
+    expect(fetchCall).toBe(
+      "https://test.frontity.org/wp-json/wp/v2/posts?_embed=true&include=12,13,14"
+    );
+  });
+
+  test("get from WP.com without params", () => {
+    const endpoint = "posts";
+    const siteUrl = "https://test.frontity.org";
+    const isWpCom = true;
+    api.get({ endpoint, siteUrl, isWpCom });
+    const [[fetchCall]] = mockedFetch.mock.calls.slice(-1);
+    expect(fetchCall).toBe(
+      "https://public-api.wordpress.com/wp/v2/sites/test.frontity.org/posts"
+    );
+  });
+
+  test("get from WP.com with params", () => {
+    const endpoint = "posts";
+    const siteUrl = "https://test.frontity.org";
+    const isWpCom = true;
+    const params = { _embed: true, include: "12,13,14" };
+    api.get({ endpoint, params, siteUrl, isWpCom });
+    const [[fetchCall]] = mockedFetch.mock.calls.slice(-1);
+    expect(fetchCall).toBe(
+      "https://public-api.wordpress.com/wp/v2/sites/test.frontity.org/posts?_embed=true&include=12,13,14"
+    );
+  });
+});
