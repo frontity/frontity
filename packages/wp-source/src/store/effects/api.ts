e425629428cd50f9c89bd007c165f395a513f10e
David Arenas <darensr@gmail.com>
2019-04-09 12:40:25 +0200

    feat(effects): implement api.get

--- packages/wp-source/src/store/effects/api.ts
+++ packages/wp-source/src/store/effects/api.ts
@@ -1,3 +1,31 @@
-export default {
-  get: () => {}
+import { Api } from "../types";
+import fetch from "node-fetch";
+
+const wpComBase = "https://public-api.wordpress.com/wp/v2/sites/";
+
+const api: Api = {
+  get: ({ endpoint, params, siteUrl, isWpCom }) => {
+    // Build the base URL depending on whether it is WP.com or WP.org
+    const baseUrl = isWpCom
+      ? `${wpComBase}${siteUrl.replace(/^https?:\/\//, "")}/`
+      : `${siteUrl}/wp-json`;
+
+    // Add the REST path depending on whether it should start
+    // with "/wp/v2" or not
+    const requestUrl = isWpCom || endpoint.startsWith("/")
+      ? `${baseUrl}${endpoint}`
+      : `${baseUrl}/wp/v2/${endpoint}`;
+
+    // Add query parameters
+    const query = params
+      ? `?${Object.entries(params)
+          .map(([key, value]) => `${key}=${value}`)
+          .join("&")}`
+      : "";
+
+    // Send request and return promise
+    return fetch(`${requestUrl}${query}`);
+  }
 };
+
+export default api;
