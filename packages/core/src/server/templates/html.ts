d7d02c50b084f28ef7c80c2c7d5750beab48b090
Luis Herranz <luisherranz@gmail.com>
2019-04-22 18:18:20 +0200

    chore(html-template): fix htmlAttributes in the wrong place

--- packages/core/src/server/templates/html.ts
+++ packages/core/src/server/templates/html.ts
@@ -1,8 +1,8 @@
 import { Template } from "../../types";
 
 const template: Template = ({ html, frontity, head }) => `<!doctype html>
-    <html>
-      <head ${head.htmlAttributes}>
+    <html ${head.htmlAttributes}>
+      <head>
         <meta charset="utf-8">
         <meta name="generator" content="Frontity">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
