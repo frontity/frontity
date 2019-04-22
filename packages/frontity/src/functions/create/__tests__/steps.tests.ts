a67291c2c40e8423f3d880939f463ffec0e89646
Eduardo Campaña <asrielo@gmail.com>
2019-04-22 18:43:15 +0200

    chore(create): adds tests for `ensureProjectDir` and `createPackageJson`

--- packages/frontity/src/functions/create/__tests__/steps.tests.ts
+++ packages/frontity/src/functions/create/__tests__/steps.tests.ts
@@ -1,4 +1,18 @@
-import { normalizeOptions } from "../steps";
+import {
+  normalizeOptions,
+  ensureProjectDir,
+  createPackageJson
+} from "../steps";
+import * as fsExtra from "fs-extra";
+import * as phin from "phin";
+
+jest.mock("fs-extra");
+jest.mock("phin");
+
+const mockedFsExtra = fsExtra as jest.Mocked<typeof fsExtra>;
+const mockedPhin = phin as jest.Mocked<typeof phin>;
+const mockedChDir = jest.fn();
+process.chdir = mockedChDir;
 
 describe("normalizeOptions", () => {
   test("returns merged options", () => {
@@ -23,6 +37,7 @@ describe("normalizeOptions", () => {
     const result = normalizeOptions(defaultOptions, passedOptions);
     expect(result).toEqual(expectedOptions);
   });
+
   test("throws if `options.name` is not valid", () => {
     const defaultOptions = {
       path: "/default/path/to/project",
@@ -39,9 +54,80 @@ describe("normalizeOptions", () => {
   });
 });
 
-describe("ensureProjectDir", () => {});
+describe("ensureProjectDir", () => {
+  beforeEach(() => {
+    mockedChDir.mockReset();
+    mockedFsExtra.ensureDir.mockReset();
+    mockedFsExtra.readdir.mockReset();
+  });
 
-describe("createPackageJson", () => {});
+  test("works when passing a non existent path", async () => {
+    const path = "/path/to/project";
+    mockedFsExtra.readdir.mockResolvedValue([]);
+    await ensureProjectDir({ path });
+    expect(mockedFsExtra.ensureDir.mock.calls).toMatchSnapshot();
+    expect(mockedChDir.mock.calls).toMatchSnapshot();
+    expect(mockedFsExtra.readdir.mock.calls).toMatchSnapshot();
+  });
+
+  test("works when passing an existent path with an empty repo", async () => {
+    const path = "/path/to/project";
+    mockedFsExtra.readdir.mockResolvedValue([
+      "README.md",
+      ".git",
+      ".gitignore",
+      "LICENSE"
+    ]);
+    await ensureProjectDir({ path });
+    expect(mockedFsExtra.ensureDir.mock.calls).toMatchSnapshot();
+    expect(mockedChDir.mock.calls).toMatchSnapshot();
+    expect(mockedFsExtra.readdir.mock.calls).toMatchSnapshot();
+  });
+
+  test("throws when passing an non-empty path", async () => {
+    const path = "/path/to/project";
+    mockedFsExtra.readdir.mockResolvedValue(["file-that-should-not-exist"]);
+    await expect(ensureProjectDir({ path })).rejects.toThrow(
+      "The directory passed to `create` function is not empty"
+    );
+  });
+});
+
+describe("createPackageJson", () => {
+  beforeEach(() => {
+    mockedChDir.mockReset();
+    mockedPhin.default.mockReset();
+    mockedPhin.default.mockResolvedValueOnce({
+      body: { "dist-tags": { latest: "1.0.0" } }
+    } as any);
+    mockedPhin.default.mockResolvedValueOnce({
+      body: { "dist-tags": { latest: "0.1.1" } }
+    } as any);
+    mockedFsExtra.writeFile.mockReset();
+  });
+
+  test('works with a theme like "@frontity/mars-theme"', async () => {
+    const options = {
+      name: "random-name",
+      packages: ["frontity", "@frontity/file-settings"],
+      theme: "@frontity/mars-theme"
+    };
+    await createPackageJson(options);
+    expect(mockedPhin.default.mock.calls).toMatchSnapshot();
+    expect(mockedFsExtra.writeFile.mock.calls).toMatchSnapshot();
+  });
+
+  test('works with a theme like "mars-theme"', async () => {
+    const options = {
+      name: "random-name",
+      packages: ["frontity", "@frontity/file-settings"],
+      theme: "random-theme"
+    };
+    await createPackageJson(options);
+    expect(mockedPhin.default.mock.calls).toMatchSnapshot();
+    expect(mockedFsExtra.writeFile.mock.calls).toMatchSnapshot();
+  });
+});
 
 describe("createFrontitySettings", () => {});
 
