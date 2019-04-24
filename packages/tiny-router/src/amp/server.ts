90e7204111b6a9e73d77c20f882445f9c9eedfe0
David Arenas <darensr@gmail.com>
2019-04-24 15:45:23 +0200

    chore(tiny-router): initial commit

---
+++ packages/tiny-router/src/amp/server.ts
@@ -0,0 +1,11 @@
+import * as Store from "../store";
+
+// define the package's namespace
+const router = {
+  Store
+};
+
+// export the namespace
+export default {
+  router
+};
---
+++ packages/tiny-router/src/store/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import * as store from "..";
+
+describe("tiny-router", () => {
+  test.todo("add tests");
+});
