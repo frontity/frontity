e2b1daf8e2f2aa2282b5f2050116fdae02d506d3
Luis Herranz <luisherranz@gmail.com>
2019-04-26 15:23:00 +0200

    test(types): add tests for the settings and package types

--- packages/types/__tests__/settings.ts
+++ packages/types/__tests__/settings.ts
@@ -1,4 +1,4 @@
-import { Settings, Package } from "../types";
+import Settings from "../settings";
 
 // Settings
 {
@@ -69,49 +69,6 @@ import { Settings, Package } from "../types";
           },
           namespace2: {
             otherSetting: "other setting of namespace 2"
-          }
-        }
-      }
-    ]
-  };
-}
-
-// Package
-{
-  // 1. Custom package extending from Package.
-  interface Package1 extends Package {
-    name: "package-1";
-  }
-
-  const package1: Settings<Package1> = {
-    packages: [
-      {
-        name: "package-1"
-      }
-    ]
-  };
-
-  // 2. Two different packages extending from Package.
-  interface Package2 extends Package {
-    name: "package-2";
-    settings: {
-      namespace1: {
-        someSetting: string;
-        optionalSetting?: boolean;
-      };
-    };
-  }
-
-  const package2: Settings<Package1 | Package2> = {
-    packages: [
-      {
-        name: "package-1"
-      },
-      {
-        name: "package-2",
-        settings: {
-          namespace1: {
-            someSetting: "some setting"
           }
         }
       }
