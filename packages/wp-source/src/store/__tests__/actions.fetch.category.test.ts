f5e460f66340d40aa8673fa05177c0ab48ef3d3d
David Arenas <darensr@gmail.com>
2019-04-29 17:20:18 +0200

    chore(tests): add more tests

--- packages/wp-source/src/store/__tests__/actions.fetch.category.test.ts
+++ packages/wp-source/src/store/__tests__/actions.fetch.category.test.ts
@@ -2,12 +2,13 @@ import { createOvermindMock } from "overmind";
 import { namespaced } from "overmind/config";
 import { Response, Headers } from "cross-fetch";
 import { state, actions, effects } from "..";
+import { OnInitialize } from "../../types";
 
 import patterns from "./mocks/patterns";
 import categoryJson from "./mocks/category.json";
 import postsFromCategoryJson from "./mocks/postsFromCategory.json";
 
-const mockResponse = (body, headers?) =>
+const mockResponse = (body, headers) =>
   new Response(
     JSON.stringify(body),
     headers && {
@@ -16,7 +17,7 @@ const mockResponse = (body, headers?) =>
   );
 
 describe("fetch", () => {
-  test("get category", async () => {
+  test("category for the first time", async () => {
     const { resolver } = effects;
     patterns.forEach(({ pattern, handler }) => {
       resolver.add(pattern, handler);
@@ -45,6 +46,52 @@ describe("fetch", () => {
     await store.actions.source.fetch({ name: "/category/nature/" });
     expect(api.get.mock.calls).toMatchSnapshot();
     expect(store.state.source.dataMap).toMatchSnapshot();
+  });
+
+  test("category that exists in state", async () => {
+    const { resolver } = effects;
+    patterns.forEach(({ pattern, handler }) => {
+      resolver.add(pattern, handler);
+    });
+    // Mock get responses
+    const api = {
+      get: jest
+        .fn()
+        .mockResolvedValueOnce(
+          mockResponse(categoryJson, {
+            "X-WP-Total": 1,
+            "X-WP-TotalPages": 1
+          })
+        )
+        .mockResolvedValueOnce(
+          mockResponse(postsFromCategoryJson, {
+            "X-WP-Total": 10,
+            "X-WP-TotalPages": 1
+          })
+        )
+    };
+    const onInitialize: OnInitialize = ctx => {
+      ctx.state.source.dataMap["/category/nature/"] = {
+        id: 7,
+        isArchive: true,
+        isCategory: true,
+        isFetching: false,
+        isReady: true,
+        isTaxonomy: true,
+        pages: [],
+        taxonomy: "category",
+        total: 10,
+        totalPages: 1
+      };
+    };
+    const config = namespaced({
+      source: { actions, state, effects, onInitialize }
+    });
+    const store = createOvermindMock(config, {
+      source: { resolver, api }
+    });
+    await store.actions.source.fetch({ name: "/category/nature/" });
+    expect(api.get).toHaveBeenCalledTimes(2);
   });
 
   test("try to get a category that doesn't exist", async () => {
--- packages/wp-source/src/store/__tests__/actions.fetch.postArchive.test.ts
+++ packages/wp-source/src/store/__tests__/actions.fetch.postArchive.test.ts
@@ -5,6 +5,15 @@ import { state, actions, effects } from "..";
 
 import patterns from "./mocks/patterns";
 import postArchiveJson from "./mocks/postArchive.json";
+import postArchiveP2Json from "./mocks/postArchiveP2.json";
+
+const mockResponse = (body, headers) =>
+  new Response(
+    JSON.stringify(body),
+    headers && {
+      headers: new Headers(headers)
+    }
+  );
 
 describe("fetch", () => {
   test("get post archive", async () => {
@@ -13,15 +22,11 @@ describe("fetch", () => {
       resolver.add(pattern, handler);
     });
     const api = {
-      get: jest.fn(() =>
-        Promise.resolve(
-          new Response(JSON.stringify(postArchiveJson), {
-            headers: new Headers({
-              "X-WP-Total": "47",
-              "X-WP-TotalPages": "5"
-            })
-          })
-        )
+      get: jest.fn().mockResolvedValueOnce(
+        mockResponse(postArchiveJson, {
+          "X-WP-Total": "64",
+          "X-WP-TotalPages": "7"
+        })
       )
     };
     const config = namespaced({ source: { actions, state, effects } });
@@ -40,15 +45,11 @@ describe("fetch", () => {
       resolver.add(pattern, handler);
     });
     const api = {
-      get: jest.fn(() =>
-        Promise.resolve(
-          new Response(JSON.stringify(postArchiveJson), {
-            headers: new Headers({
-              "X-WP-Total": "47",
-              "X-WP-TotalPages": "5"
-            })
-          })
-        )
+      get: jest.fn().mockResolvedValueOnce(
+        mockResponse(postArchiveJson, {
+          "X-WP-Total": "64",
+          "X-WP-TotalPages": "7"
+        })
       )
     };
     const config = namespaced({ source: { actions, state, effects } });
@@ -58,5 +59,34 @@ describe("fetch", () => {
     await store.actions.source.fetch({ name: "/" });
     await store.actions.source.fetch({ name: "/" });
     expect(api.get).toBeCalledTimes(1);
+  });
+
+  test("get two pages of post archive", async () => {
+    const { resolver } = effects;
+    patterns.forEach(({ pattern, handler }) => {
+      resolver.add(pattern, handler);
+    });
+    const api = {
+      get: jest.fn().mockResolvedValueOnce(
+        mockResponse(postArchiveJson, {
+          "X-WP-Total": "64",
+          "X-WP-TotalPages": "7"
+        })
+      ).mockResolvedValueOnce(
+        mockResponse(postArchiveP2Json, {
+          "X-WP-Total": "64",
+          "X-WP-TotalPages": "7"
+        })
+      )
+    };
+    const config = namespaced({ source: { actions, state, effects } });
+    const store = createOvermindMock(config, {
+      source: { resolver, api }
+    });
+    await store.actions.source.fetch({ name: "/" });
+    await store.actions.source.fetch({ name: "/", page: 2 });
+    expect(api.get.mock.calls).toMatchSnapshot();
+    expect(api.get).toBeCalledTimes(2);
+    expect(store.state.source.dataMap).toMatchSnapshot();
   });
 });
