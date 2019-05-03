90254bed9f6e95e9e70e51d9098cd9ab9c7ab8a0
David <darensr@gmail.com>
2019-05-03 18:29:00 +0200

    chore(fetch): fix and style code

--- packages/wp-source/src/store/actions.ts
+++ packages/wp-source/src/store/actions.ts
@@ -1,35 +1,33 @@
 import { Action } from "../types";
 import { normalizePath } from "./helpers";
 
-type Path = string;
-type Params = {
+type PathOrObj = string | {
   path: string;
   page?: number;
   isPopulating?: boolean;
 };
 
-export const fetch: Action<Path | Params> = async (ctx, pathOrParams) => {
+export const fetch: Action<PathOrObj> = async (ctx, pathOrObj) => {
   const state = ctx.state.source;
   const effects = ctx.effects.source;
 
   let { path, page = 1, isPopulating = false } =
-    typeof pathOrParams === "object" ? pathOrParams : { path: pathOrParams };
+    typeof pathOrObj === "object" ? pathOrObj : { path: pathOrObj };
 
-  // transform links to paths
+  // transform whole links to paths
   path = normalizePath(path);
 
   // Get current data object
   const data = state.dataMap[path];
 
   // return if the data that it's about to be fetched already exists
-  if (data && !page) return;
   if (data && page && data.isArchive && data.pages[page]) return;
 
   // init data if needed
   if (!data) state.dataMap[path] = {};
 
+  // get and execute the corresponding handler based on path
   try {
-    // get and execute the corresponding handler based on path
     const { handler, params } = effects.resolver.match(ctx, path);
     await handler(ctx, { path, params, page, isPopulating });
     state.dataMap[path].isReady = true;
