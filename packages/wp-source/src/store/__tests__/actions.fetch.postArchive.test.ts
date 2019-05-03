3363ef5e4ed3b122d44c11646aa0a72b1f7fc886
David <darensr@gmail.com>
2019-05-03 23:19:06 +0200

    chore(tests): fix fetch.postArchive tests

--- packages/wp-source/src/store/__tests__/actions.fetch.postArchive.test.ts
+++ packages/wp-source/src/store/__tests__/actions.fetch.postArchive.test.ts
@@ -17,7 +17,7 @@ const mockResponse = (body, headers) =>
 
 describe("fetch", () => {
   test("get post archive", async () => {
-    const { resolver } = effects;
+    const { resolver, populate } = effects;
     patterns.forEach(({ pattern, handler }) => {
       resolver.add(pattern, handler);
     });
@@ -31,16 +31,27 @@ describe("fetch", () => {
     };
     const config = namespaced({ source: { actions, state, effects } });
     const store = createOvermindMock(config, {
-      source: { resolver, api }
+      source: { resolver, api, populate }
     });
     await store.actions.source.fetch("/");
     expect(api.get.mock.calls).toMatchSnapshot();
     expect(api.get).toBeCalledTimes(1);
     expect(store.state.source.dataMap).toMatchSnapshot();
+    
+    ["post", "category", "tag", "author", "attachment"].forEach(name => {
+      const state = store.state.source;
+      expect(
+        Object.values(state[name]).map(({ id, slug, link }) => ({
+          id,
+          slug,
+          link
+        }))
+      ).toMatchSnapshot();
+    });
   });
 
   test("post archive twice gets data once", async () => {
-    const { resolver } = effects;
+    const { resolver, populate } = effects;
     patterns.forEach(({ pattern, handler }) => {
       resolver.add(pattern, handler);
     });
@@ -54,7 +65,7 @@ describe("fetch", () => {
     };
     const config = namespaced({ source: { actions, state, effects } });
     const store = createOvermindMock(config, {
-      source: { resolver, api }
+      source: { resolver, api, populate }
     });
     await store.actions.source.fetch("/");
     await store.actions.source.fetch("/");
@@ -62,26 +73,29 @@ describe("fetch", () => {
   });
 
   test("get two pages of post archive", async () => {
-    const { resolver } = effects;
+    const { resolver, populate } = effects;
     patterns.forEach(({ pattern, handler }) => {
       resolver.add(pattern, handler);
     });
     const api = {
-      get: jest.fn().mockResolvedValueOnce(
-        mockResponse(postArchiveJson, {
-          "X-WP-Total": "64",
-          "X-WP-TotalPages": "7"
-        })
-      ).mockResolvedValueOnce(
-        mockResponse(postArchiveP2Json, {
-          "X-WP-Total": "64",
-          "X-WP-TotalPages": "7"
-        })
-      )
+      get: jest
+        .fn()
+        .mockResolvedValueOnce(
+          mockResponse(postArchiveJson, {
+            "X-WP-Total": "64",
+            "X-WP-TotalPages": "7"
+          })
+        )
+        .mockResolvedValueOnce(
+          mockResponse(postArchiveP2Json, {
+            "X-WP-Total": "64",
+            "X-WP-TotalPages": "7"
+          })
+        )
     };
     const config = namespaced({ source: { actions, state, effects } });
     const store = createOvermindMock(config, {
-      source: { resolver, api }
+      source: { resolver, api, populate }
     });
     await store.actions.source.fetch("/");
     await store.actions.source.fetch({ path: "/", page: 2 });
