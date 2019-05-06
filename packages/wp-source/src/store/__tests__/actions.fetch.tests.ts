b034c1948c3ff416de4bb782956dcf0a8f88bd2f
David <darensr@gmail.com>
2019-05-06 20:35:32 +0200

    chore(tests): add test definitions for fetch action

---
+++ packages/wp-source/src/store/__tests__/actions.fetch.tests.ts
@@ -0,0 +1,8 @@
+import { fetch } from "../actions";
+
+describe("fetch action", () => {
+  test.todo("should fetch if data doesn't exist");
+  test.todo("should fetch if data is an archive and there's no page");
+  test.todo("does nothing if data exists and isn't an archive");
+  test.todo("does nothing if data is an archive and page exists");
+})
