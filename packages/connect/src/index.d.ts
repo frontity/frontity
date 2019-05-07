ba538f9cbd40ddd4092d48a78803a0a7ba7730ce
Luis Herranz <luisherranz@gmail.com>
2019-05-07 12:54:38 +0200

    build(connect): add missing main and types file

---
+++ packages/connect/src/index.d.ts
@@ -0,0 +1,37 @@
+declare module "@frontity/connect" {
+  function observable<Observable extends object>(obj?: Observable): Observable;
+  function isObservable(obj: object): boolean;
+  function raw<Observable extends object>(obj: Observable): Observable;
+
+  interface Scheduler {
+    add: Function;
+    delete: Function;
+  }
+
+  interface ObserveOptions {
+    scheduler?: Scheduler | Function;
+    debugger?: Function;
+    lazy?: boolean;
+  }
+
+  function observe<Reaction extends Function>(
+    func: Reaction,
+    options?: ObserveOptions
+  ): Reaction;
+  function unobserve(func: Function): void;
+
+  // Resolves the derived state for things like Action and Derived.
+  type ResolveState<State> = {
+    [P in keyof State]: State[P] extends (state: object) => any
+      ? ReturnType<State[P]>
+      : ResolveState<State[P]>
+  };
+
+  function createStore<S extends object, A extends object>({
+    state,
+    actions
+  }: {
+    state: S;
+    actions: A;
+  }): { state: S; actions: A };
+}
