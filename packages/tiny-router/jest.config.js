ac9b23d720f02ed78c412a0bfb9e4a37a27e4542
Luis Herranz <luisherranz@gmail.com>
2019-05-09 19:47:01 +0200

    test(tiny-router): allow ts-jest to transpile es modules

--- packages/tiny-router/jest.config.js
+++ packages/tiny-router/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  transform: { "^.+\\.tsx?$": "ts-jest" },
+  transform: { "^.+\\.(t|j)sx?$": "ts-jest" },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testPathIgnorePatterns: ["/mocks/"]
