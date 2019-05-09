0558e79c44c26ff413340d59d126f58eb5552f9e
David Arenas <darensr@gmail.com>
2019-05-09 23:13:25 +0200

    test(tiny-router): finish tests

--- packages/tiny-router/src/__tests__/index.test.ts
+++ packages/tiny-router/src/__tests__/index.test.ts
@@ -2,56 +2,83 @@ import { createStore } from "@frontity/connect";
 import { init, set } from "../actions";
 import tinyRouter from "..";
 
-const config = {
-  name: "@frontity/tiny-router",
-  state: {
-    router: tinyRouter.state.router,
-    frontity: {
-      url: "https://test.frontity.io"
-    }
-  },
-  actions: {
-    router: {
-      init,
-      set
-    }
-  }
-};
-
-let store: any;
+let store;
 beforeEach(() => {
+  const config = {
+    name: "@frontity/tiny-router",
+    state: {
+      router: { ...tinyRouter.state.router },
+      frontity: {
+        url: "https://test.frontity.io"
+      }
+    },
+    actions: {
+      router: {
+        init,
+        set
+      }
+    }
+  };
   store = createStore(config);
 });
 
 describe("state", () => {
   test("url without page should work", () => {
-    store.actions.router.set("/some-post");
-    expect(store.state.router.url).toBe("https://test.frontity.io/some-post");
+    store.actions.router.set("/some-post/");
+    expect(store.state.router.url).toBe("https://test.frontity.io/some-post/");
   });
+
   test("url with page = 1 should work", () => {
-    store.actions.router.set({ path: "/category/nature", page: 1 });
+    store.actions.router.set({ path: "/category/nature/", page: 1 });
     expect(store.state.router.url).toBe(
-      "https://test.frontity.io/category/nature"
+      "https://test.frontity.io/category/nature/"
     );
   });
+
   test("url with page > 1 should work", () => {
-    store.actions.router.set({ path: "/category/nature", page: 2 });
+    store.actions.router.set({ path: "/category/nature/", page: 2 });
     expect(store.state.router.url).toBe(
-      "https://test.frontity.io/category/nature/page/2"
+      "https://test.frontity.io/category/nature/page/2/"
     );
   });
 });
 
 describe("actions", () => {
   test("set() should work just with a path", () => {
-    store.actions.router.set("/some-post");
-    const snapshot = store.getSnapshot();
-    expect(snapshot).toMatchSnapshot();
+    store.actions.router.set("/some-post/");
+    expect(store.state.router).toMatchObject({
+      "page": null,
+      "path": "/some-post/",
+    });
   });
+
   test("set() should work with path and page", () => {
-    store.actions.router.set({ path: "/category/some-category", page: 2 });
-    const snapshot = store.getSnapshot();
-    expect(snapshot).toMatchSnapshot();
+    store.actions.router.set({ path: "/category/some-category/", page: 2 });
+    expect(store.state.router).toMatchObject({
+      "page": 2,
+      "path": "/category/some-category/",
+    });
   });
-  test.todo("init() should add event listener to handle popstate events");
+
+  test("init() should add event listener to handle popstate events", () => {
+    store.actions.router.init();
+
+    // check that first state is correct
+    expect(window.history.state).toMatchObject({
+      "page": null,
+      "path": "/",
+    });
+
+    // check reactions to "popstate" events
+    window.dispatchEvent(
+      new PopStateEvent("popstate", {
+        state: {
+          path: "/tag/japan",
+          page: 3
+        }
+      })
+    );
+    expect(store.state.router.path).toBe("/tag/japan");
+    expect(store.state.router.page).toBe(3);
+  });
 });
