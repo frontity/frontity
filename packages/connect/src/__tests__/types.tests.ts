c593ffc21b3af634bf7ce2b76b6ff9cadd59b926
Luis Herranz <luisherranz@gmail.com>
2019-05-11 11:50:12 +0200

    test(connect): add tests for createConnect

--- packages/connect/src/__tests__/types.tests.ts
+++ packages/connect/src/__tests__/types.tests.ts
@@ -1,1 +1,77 @@
+import { createStore, Action, Derived } from "..";
+
+interface Config {
+  state: {
+    prop1: number;
+    nested1: {
+      prop2: number;
+      prop3: Derived<Config, number>;
+      prop4: Derived<Config, number, number>;
+      prop5: number;
+    };
+  };
+  actions: {
+    action1: Action<Config>;
+    nested1: {
+      action2: Action<Config>;
+    };
+    nested2: {
+      nested3: {
+        action3: Action<Config>;
+        action4: Action<Config>;
+        action5: Action<Config, number>;
+      };
+    };
+  };
+}
+
+const config: Config = {
+  state: {
+    prop1: 1,
+    nested1: {
+      prop2: 2,
+      prop3: state => state.prop1 + state.nested1.prop2,
+      prop4: state => num => state.nested1.prop3 + num,
+      prop5: 0
+    }
+  },
+  actions: {
+    action1: state => {
+      state.prop1 = 2;
+    },
+    nested1: {
+      action2: state => {
+        state.prop1 = 3;
+      }
+    },
+    nested2: {
+      nested3: {
+        action3: state => {
+          state.nested1.prop5 = state.nested1.prop3;
+        },
+        action4: state => {
+          state.nested1.prop5 = state.nested1.prop4(2);
+        },
+        action5: state => num => {
+          state.nested1.prop5 = state.nested1.prop4(num);
+        }
+      }
+    }
+  }
+};
+
+const store = createStore(config);
+
+const prop1: number = store.state.prop1;
+const prop2: number = store.state.nested1.prop2;
+const prop3: number = store.state.nested1.prop3;
+const prop4: number = store.state.nested1.prop4(1);
+const prop5: number = store.state.nested1.prop5;
+
+const action1: () => void = store.actions.action1;
+const action2: () => void = store.actions.nested1.action2;
+const action3: () => void = store.actions.nested2.nested3.action3;
+const action4: () => void = store.actions.nested2.nested3.action4;
+const action5: (num: number) => void = store.actions.nested2.nested3.action5;
+
 test("Types are fine!", () => {});
--- packages/connect/src/index.d.ts
+++ packages/connect/src/index.d.ts
@@ -1,72 +1,78 @@
-declare module "@frontity/connect" {
-  import { ComponentType, ComponentProps } from "react";
+import { ComponentType, ComponentProps } from "react";
 
-  interface Scheduler {
-    add: Function;
-    delete: Function;
-  }
-
-  interface ObserveOptions {
-    scheduler?: Scheduler | Function;
-    debugger?: Function;
-    lazy?: boolean;
-  }
-
-  interface Store {
-    state?: object;
-    actions?: object;
-  }
-
-  type ResolveState<State> = {
-    [P in keyof State]: State[P] extends (state: object) => any
-      ? ReturnType<State[P]>
-      : ResolveState<State[P]>
-  };
-
-  type ResolveActions<Actions> = {
-    [P in keyof Actions]: Actions[P] extends (
-      ...a: any
-    ) => (arg: infer Arg) => void
-      ? (arg: Arg) => void
-      : Actions[P] extends (state: object) => any
-      ? () => void
-      : ResolveActions<Actions[P]>
-  };
-
-  type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
-  type FilterInjectedProps<T extends Store> = Omit<T, "state" | "actions">;
-
-  export function observable<Observable extends object>(
-    obj?: Observable
-  ): Observable;
-
-  export function isObservable(obj: object): boolean;
-
-  export function raw<Observable extends object>(obj: Observable): Observable;
-
-  export function observe<Reaction extends Function>(
-    func: Reaction,
-    options?: ObserveOptions
-  ): Reaction;
-
-  export function unobserve(func: Function): void;
-
-  export function createStore<St extends Store>(
-    store: St
-  ): Omit<St, "state" | "actions"> & {
-    state: ResolveState<St["state"]>;
-    actions: ResolveActions<St["actions"]>;
-    getSnapshot: () => St["state"];
-  };
-
-  function connect<Comp extends ComponentType<any>>(
-    comp: Comp
-  ): ComponentType<FilterInjectedProps<ComponentProps<Comp>>>;
-
-  export const Provider: React.ProviderExoticComponent<
-    React.ProviderProps<any>
-  >;
-
-  export default connect;
+interface Scheduler {
+  add: Function;
+  delete: Function;
 }
+
+interface ObserveOptions {
+  scheduler?: Scheduler | Function;
+  debugger?: Function;
+  lazy?: boolean;
+}
+
+interface Store {
+  state?: object;
+  actions?: object;
+}
+
+type ResolveState<State> = {
+  [P in keyof State]: State[P] extends (state: object) => any
+    ? ReturnType<State[P]>
+    : ResolveState<State[P]>
+};
+
+type ResolveActions<Actions> = {
+  [P in keyof Actions]: Actions[P] extends (
+    ...a: any
+  ) => (arg: infer Arg) => void
+    ? (arg: Arg) => void
+    : Actions[P] extends (state: object) => any
+    ? () => void
+    : ResolveActions<Actions[P]>
+};
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+type FilterInjectedProps<T extends Store> = Omit<T, "state" | "actions">;
+
+export type Action<Pkg extends Store, Input = null> = [Input] extends [null]
+  ? (state: ResolveState<Pkg["state"]>) => void
+  : (state: ResolveState<Pkg["state"]>) => (input: Input) => void;
+
+export type Derived<Pkg extends Store, InputOrOutput, Output = null> = [
+  Output
+] extends [null]
+  ? (state: ResolveState<Pkg["state"]>) => InputOrOutput
+  : (state: ResolveState<Pkg["state"]>) => (input: InputOrOutput) => Output;
+
+export function observable<Observable extends object>(
+  obj?: Observable
+): Observable;
+
+export function isObservable(obj: object): boolean;
+
+export function raw<Observable extends object>(obj: Observable): Observable;
+
+export function observe<Reaction extends Function>(
+  func: Reaction,
+  options?: ObserveOptions
+): Reaction;
+
+export function unobserve(func: Function): void;
+
+export function createStore<St extends Store>(
+  store: St
+): Omit<St, "state" | "actions"> & {
+  state: ResolveState<St["state"]>;
+  actions: ResolveActions<St["actions"]>;
+  getSnapshot: () => St["state"];
+};
+
+function connect<Comp extends ComponentType<any>>(
+  comp: Comp
+): ComponentType<FilterInjectedProps<ComponentProps<Comp>>>;
+
+export const Provider: React.ProviderExoticComponent<React.ProviderProps<any>>;
+
+export default connect;
