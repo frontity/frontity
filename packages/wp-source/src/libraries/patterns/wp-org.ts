587704f885df7b167cbdcf3cb67d312cbe7aeb44
David Arenas <darensr@gmail.com>
2019-05-20 16:02:06 +0200

    chore(wp-source): fix default wp.org patterns for permalinks

--- packages/wp-source/src/libraries/patterns/wp-org.ts
+++ packages/wp-source/src/libraries/patterns/wp-org.ts
@@ -6,7 +6,8 @@ import {
   date,
   post,
   attachment,
-  page
+  page,
+  postType
 } from "../handlers";
 
 export default [
@@ -34,20 +35,37 @@ export default [
     pattern: "/:year(\\d+)/:month(\\d+)?/:day(\\d+)?",
     handler: date
   },
+  // plain (NOT SUPPORTED YET)
   {
-    pattern: "/:year(\\d+)/:slug",
+    pattern: "/:year(\\d+)/:month(\\d+)/:day(\\d+)/:slug", // day & name
     handler: post
   },
   {
-    pattern: "/:year(\\d+)/:postSlug/:slug",
+    pattern: "/:year(\\d+)/:month(\\d+)/:slug", // month & name
+    handler: post
+  },
+  {
+    pattern: "/archives/:id", // numeric
+    handler: post
+  },
+  {
+    pattern: "/:year(\\d+)/:month(\\d+)/:day(\\d+)/:postSlug/:slug", // day & name
     handler: attachment
   },
   {
-    pattern: "/:slug",
-    handler: page
+    pattern: "/:year(\\d+)/:month(\\d+)/:postSlug/:slug", // month & name
+    handler: attachment
   },
   {
-    pattern: "/(.*)/:slug", // subpages
-    handler: page
+    pattern: "/archives/:postId/:slug", // numeric
+    handler: attachment
+  },
+  {
+    pattern: "/:slug", // post or page or attachment
+    handler: postType
+  },
+  {
+    pattern: "/(.*)/:slug", // page or attachment
+    handler: postType
   }
 ];
