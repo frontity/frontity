bf518ecc01a49ef7a5cc659b4fa7958283d6718c
David Arenas <darensr@gmail.com>
2019-05-30 19:19:17 +0200

    test(wp-source): add tests to fetch

--- packages/wp-source/src/__tests__/actions.tests.ts
+++ packages/wp-source/src/__tests__/actions.tests.ts
@@ -1,9 +1,58 @@
 import { createStore } from "@frontity/connect";
+import wpSource from "../";
 import actions from "../actions";
 
+jest.mock("../");
+
+let handler: jest.Mock;
+const initStore = (data = {}) => {
+  handler = jest.fn(async (source, { route }) => {
+    await (source.data[route] = {
+      type: "example",
+      id: 1,
+      isPostType: true,
+      isFetching: true
+    });
+  });
+  const config = wpSource();
+  // replace data by the one passed as argument
+  config.state.source.data = data;
+  // replace the mocked fetch by the real one we want to test
+  config.actions.source.fetch = actions.fetch;
+  // modify "resolver.match" implementation
+  config.libraries.source.resolver.match = jest
+    .fn()
+    .mockReturnValue({ handler, params: {} });
+  return createStore(config);
+};
+
 describe("fetch", () => {
-  test.todo("should work if data doesn't exist");
-  test.todo("should work if data is an archive and there's no page");
-  test.todo("does nothing if data exists and isn't an archive");
-  test.todo("does nothing if data is an archive and page exists");
+  test("should work if data doesn't exist", async () => {
+    const store = initStore();
+    await store.actions.source.fetch("/some/route/");
+    expect(handler).toBeCalledTimes(1);
+    expect(store.state.source.data).toMatchSnapshot();
+  });
+
+  test("does nothing if data exists", async () => {
+    const store = initStore({
+      "/some/route/": {
+        type: "example",
+        id: 1,
+        isPostType: true,
+        isFetching: false,
+        isReady: true
+      }
+    });
+    await store.actions.source.fetch("/some/route/");
+    expect(handler).not.toBeCalled();
+    expect(store.state.source.data).toMatchSnapshot();
+  });
+
+  test("return 404 is fetch fails", async () => {
+    const store = initStore();
+    handler.mockRejectedValue("Some error");
+    await store.actions.source.fetch("/some/route/");
+    expect(store.state.source.data).toMatchSnapshot();
+  });
 });
