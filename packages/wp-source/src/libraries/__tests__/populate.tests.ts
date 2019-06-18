293e151739fdba23b1afafb879c52593426f8e9c
David Arenas <darensr@gmail.com>
2019-06-18 16:45:09 +0200

    test(populate): add tests for populate

--- packages/wp-source/src/libraries/__tests__/populate.tests.ts
+++ packages/wp-source/src/libraries/__tests__/populate.tests.ts
@@ -1,5 +1,47 @@
+import { createStore } from "@frontity/connect";
+import { Response } from "cross-fetch";
+import wpSource from "../../";
 import populate from "../populate";
+import postsCat7 from "../handlers/__tests__/mocks/posts-cat-7.json";
+import postsCat7Subdir from "../handlers/__tests__/mocks/posts-cat-7-subdir.json";
 
-describe("populate action", () => {
-  test.todo("need tests");
-})
+jest.mock("../");
+
+const initStore = () => {
+  const config = wpSource();
+  return createStore(config);
+};
+
+describe("populate", () => {
+  test("adds posts and embedded into state", async () => {
+    const { state } = initStore();
+    const response = new Response(JSON.stringify(postsCat7));
+    const result = await populate({ state, response });
+
+    expect(result).toMatchSnapshot();
+    expect(state.source).toMatchSnapshot();
+  });
+
+  test("removes WP API path from links", async () => {
+    const { state } = initStore();
+    state.source.api = "https://test.frontity.io/subdirectory/wp-json";
+
+    const response = new Response(JSON.stringify(postsCat7Subdir));
+    const result = await populate({ state, response });
+
+    expect(result).toMatchSnapshot();
+    expect(state.source).toMatchSnapshot();
+  });
+
+  test("transforms links if subdirectory is specified", async () => {
+    const { state } = initStore();
+    state.source.api = "https://test.frontity.io/subdirectory/wp-json";
+
+    const response = new Response(JSON.stringify(postsCat7Subdir));
+    const subdirectory = "/blog/";
+    const result = await populate({ state, response, subdirectory });
+
+    expect(result).toMatchSnapshot();
+    expect(state.source).toMatchSnapshot();
+  });
+});
