ccd181ace62214d616adb2035e317e8d7581bf63
luisherranz <luisherranz@gmail.com>
2019-06-27 11:18:18 +0200

    feat(frontity-cli): add command to get environment information

--- packages/frontity/src/actions/index.ts
+++ packages/frontity/src/actions/index.ts
@@ -4,3 +4,4 @@ export { default as dev } from "./dev";
 export { default as build } from "./build";
 export { default as serve } from "./serve";
 export { default as subscribe } from "./subscribe";
+export { default as info } from "./info";
