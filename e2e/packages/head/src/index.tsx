9b0bda667b199ab295891158bd7513fe0ca3aa42
Luis Herranz <luisherranz@gmail.com>
2019-09-09 17:41:48 +0200

    tests(e2e): add first cypress tests for Head component

--- e2e/packages/head/src/index.tsx
+++ e2e/packages/head/src/index.tsx
@@ -5,9 +5,17 @@ import Package from "../types";
 const Root: React.FC = () => (
   <>
     <Head>
+      <html lang="en" />
       <title>The Title</title>
+      <meta name="description" content="The Description" />
+      <script type="text/javascript">{`window.scriptTest = "pass"`}</script>
+      <link rel="canonical" href="http://mysite.com/example" />
+      <body className="new-class" />
+      <noscript>
+        {'<link rel="stylesheet" type="text/css" href="foo.css" />'}
+      </noscript>
+      <style type="text/css">{`body { background-color: blue; }`}</style>
     </Head>
-    <div>Body Content</div>
   </>
 );
 
