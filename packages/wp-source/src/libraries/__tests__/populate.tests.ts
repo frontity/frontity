d3fbe8faedc2d35a3b30cf89795bb2ae4846ca76
David Arenas <darensr@gmail.com>
2019-09-10 11:24:15 +0200

    test(populate): use new mocks to test populate

--- packages/wp-source/src/libraries/__tests__/populate.tests.ts
+++ packages/wp-source/src/libraries/__tests__/populate.tests.ts
@@ -4,9 +4,9 @@ import clone from "clone-deep";
 import wpSource from "../../";
 import WpSource from "../../../types";
 import populate from "../populate";
-import postsCat7 from "../handlers/__tests__/mocks/posts-cat-7.json";
-import postsCat7Subdir from "../handlers/__tests__/mocks/posts-cat-7-subdir.json";
-import movies from "../handlers/__tests__/mocks/movies.json";
+import posts from "../handlers/__tests__/mocks/post-archive/posts.json";
+import postsSubdir from "../handlers/__tests__/mocks/post-archive/posts-subdir.json";
+import cpts from "../handlers/__tests__/mocks/cpt-archive/cpts.json";
 
 jest.mock("../");
 
@@ -24,7 +24,7 @@ const mockResponse = (body): Response =>
 describe("populate", () => {
   test("adds posts and embedded into state", async () => {
     const { state } = initStore();
-    const response = mockResponse(postsCat7);
+    const response = mockResponse(posts);
     const result = await populate({ state, response });
 
     expect(result).toMatchSnapshot();
@@ -35,7 +35,7 @@ describe("populate", () => {
     const { state } = initStore();
     state.source.api = "https://test.frontity.io/subdirectory/wp-json";
 
-    const response = mockResponse(postsCat7Subdir);
+    const response = mockResponse(postsSubdir);
     const result = await populate({ state, response });
 
     expect(result).toMatchSnapshot();
@@ -46,7 +46,7 @@ describe("populate", () => {
     const { state } = initStore();
     state.source.api = "https://test.frontity.io/subdirectory/wp-json";
 
-    const response = mockResponse(postsCat7Subdir);
+    const response = mockResponse(postsSubdir);
     const subdirectory = "/blog/";
     const result = await populate({ state, response, subdirectory });
 
@@ -56,7 +56,7 @@ describe("populate", () => {
 
   test("add new custom post types & taxonomies to the state", async () => {
     const { state } = initStore();
-    const response = mockResponse(movies);
+    const response = mockResponse(cpts);
     const result = await populate({ state, response });
 
     expect(result).toMatchSnapshot();
