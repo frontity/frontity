f7f0d6b9d68e315909dc8c1fad6bfb7c0cafaa2d
Luis Herranz <luisherranz@gmail.com>
2019-09-23 17:41:53 +0200

    chore(empty-mars-theme): remove state.frontity
    I don't think it's needed...

--- examples/empty-theme-example/frontity.settings.js
+++ examples/empty-theme-example/frontity.settings.js
@@ -1,11 +1,4 @@
 export default {
   name: "empty-theme-example",
-  state: {
-    frontity: {
-      url: "",
-      title: "Test Frontity Blog",
-      description: "Useful content for Frontity development"
-    }
-  },
   packages: ["@frontity/empty-theme"]
 };
