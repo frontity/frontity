006314cf676fb28a97291c3bacb6e0e43c5f9112
David Arenas <darensr@gmail.com>
2019-09-24 20:15:54 +0200

    test(e2e): add tests for image lazy-loading with useInView

--- e2e/integration/image.spec.js
+++ e2e/integration/image.spec.js
@@ -1,10 +1,8 @@
-describe("Image (with native lazy-load)", () => {
+describe("Image lazy-loading (with native lazy-load)", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000?name=image", {
-      onBeforeLoad(win) {
-        win.scrollTo(0, 0);
-      }
-    });
+    cy.viewport(360, 640);
+    cy.visit("http://localhost:3000?name=image");
+    cy.scrollTo("top");
   });
 
   it("should show an image with loading=auto if it doesn't have height", () => {
@@ -20,36 +18,51 @@ describe("Image (with native lazy-load)", () => {
   });
 });
 
-// describe("Image (with Intersection Observer)", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000?name=image", {
-//       onBeforeLoad(win) {
-//         // Remove the "loading" prop from the HTMLImageElement prototype
-//         Object.defineProperty(win.HTMLImageElement.prototype, "loading", {});
-//         delete win.HTMLImageElement.prototype.loading;
-//       }
-//     });
-//   });
+describe("Image lazy-loading (with Intersection Observer)", () => {
+  beforeEach(() => {
+    cy.viewport(360, 640);
+    cy.visit("http://localhost:3000?name=image", {
+      onBeforeLoad(win) {
+        // Remove the "loading" prop from the HTMLImageElement prototype
+        Object.defineProperty(win.HTMLImageElement.prototype, "loading", {});
+        delete win.HTMLImageElement.prototype.loading;
+      }
+    });
+  });
 
-//   it("should show an image with loading=auto if it doesn't have height", () => {
-//     cy.get("img:not([height])")
-//       .should("be.visible")
-//       .should("have.attr", "loading", "auto");
-//   });
+  it("should work scrolling from top to bottom", () => {
+    cy.scrollTo("topLeft");
+    cy.get("img:not(.top.left)").should("not.be.visible");
+    cy.get("img.bottom.left")
+      .scrollIntoView({ duration: 300 })
+      .should("be.visible");
+    cy.get("img.right").should("not.be.visible");
+  });
 
-//   it("should show an image with loading=lazy if it has a height", () => {
-//     cy.get("img[height]")
-//       .should("be.visible")
-//       .should("have.attr", "loading", "lazy");
-//   });
-// });
+  it("should work scrolling from bottom to top", () => {
+    cy.scrollTo("bottomLeft");
+    cy.get("img:not(.bottom.left)").should("not.be.visible");
+    cy.get("img.top.left")
+      .scrollIntoView({ duration: 300 })
+      .should("be.visible");
+    cy.get("img.right").should("not.be.visible");
+  });
 
-// describe("Image (without lazy-load)", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000?name=image");
-//   });
+  it("should work scrolling from left to right", () => {
+    cy.scrollTo("topLeft");
+    cy.get("img:not(.bottom.left)").should("not.be.visible");
+    cy.get("img.top.right")
+      .scrollIntoView({ duration: 300 })
+      .should("be.visible");
+    cy.get("img.bottom").should("not.be.visible");
+  });
 
-//   it("should be showing an image", () => {
-//     cy.get(".test-image-1").should("be.visible");
-//   });
-// });
+  it("should work scrolling from right to left", () => {
+    cy.scrollTo("bottomRight");
+    cy.get("img:not(.bottom.right)").should("not.be.visible");
+    cy.get("img.bottom.left")
+      .scrollIntoView({ duration: 300 })
+      .should("be.visible");
+    cy.get("img.top").should("not.be.visible");
+  });
+});
--- e2e/packages/image/src/index.tsx
+++ e2e/packages/image/src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { styled, css } from "frontity";
+import { css, styled } from "frontity";
 import Image from "@frontity/components/image";
 import Package from "../types";
 
-import icelandTest from "../static/images/Iceland-test.jpg";
-import icelandTest300x182 from "../static/images/Iceland-test-300x182.jpg";
-import icelandTest768x465 from "../static/images/Iceland-test-768x465.jpg";
-import icelandTest1024x620 from "../static/images/Iceland-test-1024x620.jpg";
-import icelandTest1200x726 from "../static/images/Iceland-test-1200x726.jpg";
+import iceland from "../static/images/Iceland-test.jpg";
+import iceland300x182 from "../static/images/Iceland-test-300x182.jpg";
+import iceland768x465 from "../static/images/Iceland-test-768x465.jpg";
+import iceland1024x620 from "../static/images/Iceland-test-1024x620.jpg";
+import iceland1200x726 from "../static/images/Iceland-test-1200x726.jpg";
 
 import japan from "../static/images/japan.jpg";
 import japan300x200 from "../static/images/japan-300x200.jpg";
@@ -15,70 +15,115 @@ import japan768x512 from "../static/images/japan-768x512.jpg";
 import japan1024x683 from "../static/images/japan-1024x683.jpg";
 import japan1200x800 from "../static/images/japan-1200x800.jpg";
 
-const Root: React.FC = () => (
-  <>
-    <Image
-      className="first"
-      alt="iceland"
-      src={icelandTest}
-      srcSet={[
-        `${icelandTest300x182} 300w`,
-        `${icelandTest768x465} 768w`,
-        `${icelandTest1024x620} 1024w`,
-        `${icelandTest1200x726} 1200w`
-      ].join(", ")}
-      sizes="100vw"
-    />
-    <Image
-      className="second"
-      css={css`margin-top 3000px;`}
-      height={200}
-      alt=""
-      src="https://test.frontity.io/wp-content/uploads/2016/11/japan.jpg"
-      srcSet={[
-        "https://test.frontity.io/wp-content/uploads/2016/11/japan-300x200.jpg   300w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/japan-768x512.jpg   768w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/japan-1024x683.jpg 1024w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/japan-1200x800.jpg 1200w"
-      ].join(", ")}
-      sizes="100vw"
-    />
-    <Image
-      className="third"
-      height={200}
-      css={css`margin-top 3000px;`}
-      alt="canyon"
-      src="https://test.frontity.io/wp-content/uploads/2016/11/canyon.jpg"
-      srcSet={[
-        "https://test.frontity.io/wp-content/uploads/2016/11/canyon-300x200.jpg   300w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/canyon-768x512.jpg   768w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/canyon-1024x683.jpg 1024w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/canyon-1200x800.jpg 1200w"
-      ].join(", ")}
-      sizes="100vw"
-    />
-    <Image
-      className="fourth"
-      css={css`margin-top 3000px;`}
-      height={200}
-      alt="sunrise-phu-quoc-island-ocean"
-      src="https://test.frontity.io/wp-content/uploads/2016/11/sunrise-phu-quoc-island-ocean-300x198.jpg"
-      srcSet={[
-        "https://test.frontity.io/wp-content/uploads/2016/11/sunrise-phu-quoc-island-ocean-300x198.jpg   300w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/sunrise-phu-quoc-island-ocean-768x506.jpg   768w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/sunrise-phu-quoc-island-ocean-1024x675.jpg 1024w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/sunrise-phu-quoc-island-ocean-1200x791.jpg 1200w",
-        "https://test.frontity.io/wp-content/uploads/2016/11/sunrise-phu-quoc-island-ocean.jpg          1280w"
-      ].join(", ")}
-      sizes="100vw"
-    />
-  </>
+import canyon from "../static/images/canyon.jpg";
+import canyon300x200 from "../static/images/canyon-300x200.jpg";
+import canyon768x512 from "../static/images/canyon-768x512.jpg";
+import canyon1024x683 from "../static/images/canyon-1024x683.jpg";
+import canyon1200x800 from "../static/images/canyon-1200x800.jpg";
+
+import phuQuoc from "../static/images/sunrise-phu-quoc-island-ocean.jpg";
+import phuQuoc300x198 from "../static/images/sunrise-phu-quoc-island-ocean-300x198.jpg";
+import phuQuoc768x506 from "../static/images/sunrise-phu-quoc-island-ocean-768x506.jpg";
+import phuQuoc1024x675 from "../static/images/sunrise-phu-quoc-island-ocean-1024x675.jpg";
+import phuQuoc1200x791 from "../static/images/sunrise-phu-quoc-island-ocean-1200x791.jpg";
+
+const ImgIceland = ({ className }) => (
+  <Image
+    className={className}
+    alt="iceland"
+    src={iceland}
+    srcSet={[
+      `${iceland300x182} 300w`,
+      `${iceland768x465} 768w`,
+      `${iceland1024x620} 1024w`,
+      `${iceland1200x726} 1200w`
+    ].join(", ")}
+    sizes="100vw"
+  />
 );
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 100px;
+const ImgJapan = ({ className }) => (
+  <Image
+    className={className}
+    height={200}
+    alt="japan"
+    src={japan}
+    srcSet={[
+      `${japan300x200} 300w`,
+      `${japan768x512} 768w`,
+      `${japan1024x683} 1024w`,
+      `${japan1200x800} 1200w`
+    ].join(", ")}
+    sizes="100vw"
+  />
+);
+
+const ImgCanyon = ({ className }) => (
+  <Image
+    className={className}
+    height={200}
+    alt="canyon"
+    src={canyon}
+    srcSet={[
+      `${canyon300x200} 300w`,
+      `${canyon768x512} 768w`,
+      `${canyon1024x683} 1024w`,
+      `${canyon1200x800} 1200w`
+    ].join(", ")}
+    sizes="100vw"
+  />
+);
+
+const ImgPhuQuoc = ({ className }) => (
+  <Image
+    className={className}
+    height={200}
+    alt="phu-quoc"
+    src={phuQuoc}
+    srcSet={[
+      `${phuQuoc300x198} 300w`,
+      `${phuQuoc768x506} 768w`,
+      `${phuQuoc1024x675} 1024w`,
+      `${phuQuoc1200x791} 1200w`
+    ].join(", ")}
+    sizes="100vw"
+  />
+);
+
+const Root: React.FC = () => (
+  <Container>
+    <Column>
+      <ImgIceland className="top left" />
+      <VSpace />
+      <ImgJapan className="bottom left" />
+    </Column>
+    <HSpace />
+    <Column>
+      <ImgCanyon className="top right" />
+      <VSpace />
+      <ImgPhuQuoc className="bottom right" />
+    </Column>
+  </Container>
+);
+
+const Container = styled.div`
+  width: 2000px;
+  position: relative;
+`;
+
+const Column = styled.div`
+  display: inline-block;
+  vertical-align: top;
+`;
+
+const HSpace = styled.div`
+  display: inline-block;
+  width: 1000px;
+  vertical-align: top;
+`;
+
+const VSpace = styled.div`
+  height: 1000px;
 `;
 
 const ImagePackage: Package = {
