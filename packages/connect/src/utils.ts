6467a2848155f724f8eda15227003dd3acac0ca4
Michal Czaplinski <mmczaplinski@gmail.com>
2019-10-07 22:23:22 -0500

    chore(connect): reformat utils with prettier

--- packages/connect/src/utils.ts
+++ packages/connect/src/utils.ts
@@ -1,16 +1,18 @@
 export function isPlainObject(obj: any): boolean {
-  if (typeof obj !== 'object' || obj === null) return false
+  if (typeof obj !== "object" || obj === null) return false;
 
-  let proto = obj
+  let proto = obj;
   while (Object.getPrototypeOf(proto) !== null) {
-    proto = Object.getPrototypeOf(proto)
+    proto = Object.getPrototypeOf(proto);
   }
 
-  return Object.getPrototypeOf(obj) === proto
+  return Object.getPrototypeOf(obj) === proto;
 }
 
-export function isArray(arg: any) { return Array.isArray(arg) }
+export function isArray(arg: any) {
+  return Array.isArray(arg);
+}
 
 export function isFunction(arg: any) {
-  return arg instanceof Function
+  return arg instanceof Function;
 }
