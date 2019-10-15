d0232e107ce8602ea1916d6e43c9ad4aad245405
Luis Herranz <luisherranz@gmail.com>
2019-10-15 13:11:32 +0200

    test(e2e): fix url of e2e project

--- e2e/integration/fonts.spec.js
+++ e2e/integration/fonts.spec.js
@@ -1,6 +1,6 @@
 describe("Fonts", () => {
   it("should not load any fonts", () => {
-    cy.visit("http://localhost:3000/empty?name=fonts");
+    cy.visit("http://localhost:3001/empty?name=fonts");
     cy.get("[data-test-id='div-with-font']").should(
       "have.css",
       "font-family",
--- e2e/integration/global.spec.js
+++ e2e/integration/global.spec.js
@@ -1,13 +1,13 @@
 describe("Global", () => {
   it("should have a blue background, but not a red color", () => {
-    cy.visit("http://localhost:3000/color-red?name=global");
-    cy.visit("http://localhost:3000/background-blue?name=global");
+    cy.visit("http://localhost:3001/color-red?name=global");
+    cy.visit("http://localhost:3001/background-blue?name=global");
     cy.get("body").should("have.css", "background-color", "rgb(0, 0, 255)");
     cy.get("body").should("not.have.css", "color", "rgb(255, 0, 0)");
   });
   it("should have a red color, but not a blue background", () => {
-    cy.visit("http://localhost:3000/background-blue?name=global");
-    cy.visit("http://localhost:3000/color-red?name=global");
+    cy.visit("http://localhost:3001/background-blue?name=global");
+    cy.visit("http://localhost:3001/color-red?name=global");
     cy.get("body").should("have.css", "color", "rgb(255, 0, 0)");
     cy.get("body").should("not.have.css", "background-color", "rgb(0, 0, 255)");
   });
