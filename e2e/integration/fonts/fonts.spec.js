78b381471acf93346feecabe16919fbda7e89ebe
Luis Herranz <luisherranz@gmail.com>
2019-10-25 17:24:01 +0200

    test(fonts): fix e2e tests for fonts
    I removed eot and svg because there is no way to test that in production

--- e2e/integration/fonts/fonts.spec.js
+++ e2e/integration/fonts/fonts.spec.js
@@ -4,41 +4,39 @@ describe("Fonts", () => {
     cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
   });
 
-  it("should load the eot-1 font", () => {
-    cy.visit("http://localhost:3001/eot-1?name=fonts");
-    // Eot doesn't work in Chrome so we just check if the data has been added.
-    cy.get("style").contains("base64");
-  });
-
-  it("should load the eot-2 font", () => {
-    cy.visit("http://localhost:3001/eot-2?name=fonts");
-    // Eot doesn't work in Chrome so we just check if the data has been added.
-    cy.get("style").contains("base64");
-  });
-
   it("should load the woff-1 font", () => {
     cy.visit("http://localhost:3001/woff-1?name=fonts");
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
     cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
   });
 
   it("should load the woff-2 font", () => {
     cy.visit("http://localhost:3001/woff-2?name=fonts");
     cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
   });
 
   it("should load the ttf font", () => {
     cy.visit("http://localhost:3001/ttf?name=fonts");
     cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
-  });
-
-  it("should load the svg font", () => {
-    cy.visit("http://localhost:3001/svg?name=fonts");
-    // SVG fonts don't work in Chrome so we just check if the data has been added.
-    cy.get("style").contains('url("/static/images/aclonica-v10-latin-regular');
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
   });
 
   it("should load the all fonts", () => {
     cy.visit("http://localhost:3001/?name=fonts");
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
+    cy.get("[data-test-id='toggle-button']").click();
     cy.get("[data-test-id='div-with-font']").toMatchImageSnapshot();
   });
 });
--- e2e/packages/fonts/src/index.tsx
+++ e2e/packages/fonts/src/index.tsx
@@ -12,128 +12,114 @@ const Div = styled.div`
   width: 300px;
 `;
 
-const All: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: url(${aclonicaEot});
-          src: local("Aclonica Regular"), local("Aclonica-Regular"),
-            url(${aclonicaEot + "?#iefix"}) format("embedded-opentype"),
-            url(${aclonicaWoff2}) format("woff2"),
-            url(${aclonicaWoff}) format("woff"),
-            url(${aclonicaTtf}) format("truetype"),
-            url(${aclonicaSvg + "#Aclonica"}) format("svg");
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with all fonts!</Div>
-  </>
-);
+const Button = styled.button`
+  margin: 20px 0;
+`;
 
-const Eot1: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: url(${aclonicaEot});
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with eot1!</Div>
-  </>
-);
+const All: React.FC = () => {
+  const [global, setGlobal] = React.useState(false);
+  return (
+    <>
+      {global && (
+        <Global
+          styles={css`
+            @font-face {
+              font-family: "Aclonica";
+              font-style: normal;
+              font-weight: 400;
+              src: url(${aclonicaEot});
+              src: local("Aclonica Regular"), local("Aclonica-Regular"),
+                url(${aclonicaEot + "?#iefix"}) format("embedded-opentype"),
+                url(${aclonicaWoff2}) format("woff2"),
+                url(${aclonicaWoff}) format("woff"),
+                url(${aclonicaTtf}) format("truetype"),
+                url(${aclonicaSvg + "#Aclonica"}) format("svg");
+            }
+          `}
+        />
+      )}
+      <Div data-test-id="div-with-font">Aclonica Font with all fonts!</Div>
+      <Button data-test-id="toggle-button" onClick={() => setGlobal(!global)}>
+        Toggle Global
+      </Button>
+    </>
+  );
+};
 
-const Eot2: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: local("Aclonica Regular"), local("Aclonica-Regular"),
-            url(${aclonicaEot + "?#iefix"}) format("embedded-opentype");
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with eot2!</Div>
-  </>
-);
+const Woff1: React.FC = () => {
+  const [global, setGlobal] = React.useState(false);
+  return (
+    <>
+      {global && (
+        <Global
+          styles={css`
+            @font-face {
+              font-family: "Aclonica";
+              font-style: normal;
+              font-weight: 400;
+              src: local("Aclonica Regular"), local("Aclonica-Regular"),
+                url(${aclonicaWoff}) format("woff");
+            }
+          `}
+        />
+      )}
+      <Div data-test-id="div-with-font">Aclonica Font with woff1!</Div>
+      <Button data-test-id="toggle-button" onClick={() => setGlobal(!global)}>
+        Toggle Global
+      </Button>
+    </>
+  );
+};
 
-const Woff1: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: local("Aclonica Regular"), local("Aclonica-Regular"),
-            url(${aclonicaWoff}) format("woff");
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with woff1!</Div>
-  </>
-);
+const Woff2: React.FC = () => {
+  const [global, setGlobal] = React.useState(false);
+  return (
+    <>
+      {global && (
+        <Global
+          styles={css`
+            @font-face {
+              font-family: "Aclonica";
+              font-style: normal;
+              font-weight: 400;
+              src: local("Aclonica Regular"), local("Aclonica-Regular"),
+                url(${aclonicaWoff2}) format("woff2");
+            }
+          `}
+        />
+      )}
+      <Div data-test-id="div-with-font">Aclonica Font with woff2!</Div>
+      <Button data-test-id="toggle-button" onClick={() => setGlobal(!global)}>
+        Toggle Global
+      </Button>
+    </>
+  );
+};
 
-const Woff2: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: local("Aclonica Regular"), local("Aclonica-Regular"),
-            url(${aclonicaWoff2}) format("woff2");
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with woff2!</Div>
-  </>
-);
-
-const Ttf: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: local("Aclonica Regular"), local("Aclonica-Regular"),
-            url(${aclonicaTtf}) format("truetype");
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with ttf!</Div>
-  </>
-);
-
-const Svg: React.FC = () => (
-  <>
-    <Global
-      styles={css`
-        @font-face {
-          font-family: "Aclonica";
-          font-style: normal;
-          font-weight: 400;
-          src: local("Aclonica Regular"), local("Aclonica-Regular"),
-            url("${aclonicaSvg + "#Aclonica"}") format("svg");
-        }
-      `}
-    />
-    <Div data-test-id="div-with-font">Aclonica Font with svg!</Div>
-  </>
-);
+const Ttf: React.FC = () => {
+  const [global, setGlobal] = React.useState(false);
+  return (
+    <>
+      {global && (
+        <Global
+          styles={css`
+            @font-face {
+              font-family: "Aclonica";
+              font-style: normal;
+              font-weight: 400;
+              src: local("Aclonica Regular"), local("Aclonica-Regular"),
+                url(${aclonicaTtf}) format("truetype");
+            }
+          `}
+        />
+      )}
+      <Div data-test-id="div-with-font">Aclonica Font with ttf!</Div>
+      <Button data-test-id="toggle-button" onClick={() => setGlobal(!global)}>
+        Toggle Global
+      </Button>
+    </>
+  );
+};
 
 const Empty: React.FC = () => (
   <Div data-test-id="div-with-font">Fonts not loaded</Div>
@@ -142,12 +128,9 @@ const Empty: React.FC = () => (
 const Font: React.FC = connect(({ state }) => {
   const { pathname } = new URL(state.router.link, "http://localhost:3001");
   if (pathname === "/") return <All />;
-  if (pathname === "/eot-1") return <Eot1 />;
-  if (pathname === "/eot-2") return <Eot2 />;
   if (pathname === "/woff-1") return <Woff1 />;
   if (pathname === "/woff-2") return <Woff2 />;
   if (pathname === "/ttf") return <Ttf />;
-  if (pathname === "/svg") return <Svg />;
   if (pathname === "/empty") return <Empty />;
 });
 
