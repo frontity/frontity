471c938d06902d71c1fb3a036a1f3ce6971ab50c
Luis Herranz <luisherranz@gmail.com>
2019-10-25 17:24:32 +0200

    test(global): add conditionals to Global e2e tests

--- e2e/integration/global.spec.js
+++ e2e/integration/global.spec.js
@@ -2,13 +2,22 @@ describe("Global", () => {
   it("should have a blue background, but not a red color", () => {
     cy.visit("http://localhost:3001/color-red?name=global");
     cy.visit("http://localhost:3001/background-blue?name=global");
-    cy.get("body").should("have.css", "background-color", "rgb(0, 0, 255)");
+    cy.get("body").should("not.have.css", "background-color", "rgb(0, 0, 255)");
     cy.get("body").should("not.have.css", "color", "rgb(255, 0, 0)");
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("body").should("have.css", "background-color", "rgb(0, 0, 255)");
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("body").should("not.have.css", "background-color", "rgb(0, 0, 255)");
   });
+
   it("should have a red color, but not a blue background", () => {
     cy.visit("http://localhost:3001/background-blue?name=global");
     cy.visit("http://localhost:3001/color-red?name=global");
-    cy.get("body").should("have.css", "color", "rgb(255, 0, 0)");
     cy.get("body").should("not.have.css", "background-color", "rgb(0, 0, 255)");
+    cy.get("body").should("not.have.css", "color", "rgb(255, 0, 0)");
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("body").should("have.css", "color", "rgb(255, 0, 0)");
+    cy.get("[data-test-id='toggle-button']").click();
+    cy.get("body").should("not.have.css", "color", "rgb(255, 0, 0)");
   });
 });
--- e2e/packages/global/src/index.tsx
+++ e2e/packages/global/src/index.tsx
@@ -2,31 +2,47 @@ import React from "react";
 import { Global, css, connect, URL } from "frontity";
 import Package from "../types";
 
-const BackgroundBlue = () => (
-  <>
-    <Global
-      styles={css`
-        body {
-          background-color: blue;
-        }
-      `}
-    />
-    <div data-test-id="div-with-text">BackgroundBlue</div>
-  </>
-);
+const BackgroundBlue = () => {
+  const [global, setGlobal] = React.useState(false);
+  return (
+    <>
+      {global && (
+        <Global
+          styles={css`
+            body {
+              background-color: blue;
+            }
+          `}
+        />
+      )}
+      <div data-test-id="div-with-text">BackgroundBlue</div>
+      <button data-test-id="toggle-button" onClick={() => setGlobal(!global)}>
+        Toggle Global
+      </button>
+    </>
+  );
+};
 
-const ColorRed = () => (
-  <>
-    <Global
-      styles={css`
-        body {
-          color: red;
-        }
-      `}
-    />
-    <div data-test-id="div-with-text">ColorRed</div>
-  </>
-);
+const ColorRed = () => {
+  const [global, setGlobal] = React.useState(false);
+  return (
+    <>
+      {global && (
+        <Global
+          styles={css`
+            body {
+              color: red;
+            }
+          `}
+        />
+      )}
+      <div data-test-id="div-with-text">ColorRed</div>
+      <button data-test-id="toggle-button" onClick={() => setGlobal(!global)}>
+        Toggle Global
+      </button>
+    </>
+  );
+};
 
 const Root = connect(({ state }) => {
   const { pathname } = new URL(state.router.link, "http://localhost:3000");
