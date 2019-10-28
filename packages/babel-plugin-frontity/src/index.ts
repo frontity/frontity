94347449e9bde81c426a43969a5390864dae1260
Luis Herranz <luisherranz@Luiss-iMac.local>
2019-10-28 10:48:48 +0100

    refactor(babel-plugin): add proper types

--- packages/babel-plugin-frontity/src/index.ts
+++ packages/babel-plugin-frontity/src/index.ts
@@ -1,8 +1,21 @@
-const defaultImports = {
+import * as BabelTypes from "@babel/types";
+import { Visitor, NodePath } from "@babel/traverse";
+
+interface Babel {
+  types: typeof BabelTypes;
+}
+
+type GetElementType<T extends Array<any>> = T extends (infer U)[] ? U : never;
+
+type Imports = {
+  [key: string]: string;
+};
+
+const defaultImports: Imports = {
   styled: "@emotion/styled"
 };
 
-const namedImports = {
+const namedImports: Imports = {
   css: "@emotion/core",
   Global: "@emotion/core",
   keyframes: "@emotion/core"
@@ -10,24 +23,32 @@ const namedImports = {
 
 const allNames = [...Object.keys(defaultImports), ...Object.keys(namedImports)];
 
-export default babel => {
+function getMemberImports(
+  specifier: GetElementType<
+    NodePath<BabelTypes.ImportDeclaration>["node"]["specifiers"]
+  >
+): specifier is BabelTypes.ImportSpecifier {
+  return specifier.type === "ImportSpecifier";
+}
+
+export default (babel: Babel): { name: string; visitor: Visitor } => {
   const types = babel.types;
   return {
     name: "frontity",
     visitor: {
       Program(programPath) {
         programPath.traverse({
-          ImportDeclaration: function(path) {
-            const transforms = [];
+          ImportDeclaration: function(
+            path: NodePath<BabelTypes.ImportDeclaration>
+          ) {
+            const transforms: BabelTypes.ImportDeclaration[] = [];
 
             const source = path.node.source.value;
 
             if (source === "frontity") {
-              const memberImports = path.node.specifiers.filter(function(
-                specifier
-              ) {
-                return specifier.type === "ImportSpecifier";
-              });
+              const memberImports = path.node.specifiers.filter(
+                getMemberImports
+              );
               const hasImports = memberImports
                 .map(memberImport => memberImport.imported.name)
                 .filter(name => allNames.includes(name))
