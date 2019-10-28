3aed942afe6333e5a520be1e5dc36f86b21f5041
Luis Herranz <luisherranz@Luiss-iMac.local>
2019-10-28 11:11:04 +0100

    refactor(babel-plugin): add proper coments

--- packages/babel-plugin-frontity/src/index.ts
+++ packages/babel-plugin-frontity/src/index.ts
@@ -5,25 +5,36 @@ interface Babel {
   types: typeof BabelTypes;
 }
 
-type GetElementType<T extends Array<any>> = T extends (infer U)[] ? U : never;
+type GetElementType<T extends Array<BabelTypes.Node>> = T extends (infer U)[]
+  ? U
+  : never;
 
 type Imports = {
   [key: string]: string;
 };
 
+// Named imports that end up as default imports.
 const defaultImports: Imports = {
+  // "import { styled } from 'frontity'" ==> "import styled from '@emotion/styled'"
   styled: "@emotion/styled"
 };
 
+// Named imports that end up as named imports.
 const namedImports: Imports = {
+  // "import { css } from 'frontity'" ==> "import { css } from '@emotion/core'"
   css: "@emotion/core",
+  // "import { Global } from 'frontity'" ==> "import { Global } from '@emotion/core'"
   Global: "@emotion/core",
+  // "import { keyframes } from 'frontity'" ==> "import { keyframes } from '@emotion/core'"
   keyframes: "@emotion/core"
 };
 
+// Import names that should be looking for in our "frontity" imports.
 const allNames = [...Object.keys(defaultImports), ...Object.keys(namedImports)];
 
-function getMemberImports(
+// Checks if the import is a ImportSpecifier. We are only interested on named imports
+// because "frontity" doesn't have any default import (ImportDefaultSpecifier).
+function isImportSpecifier(
   specifier: GetElementType<
     NodePath<BabelTypes.ImportDeclaration>["node"]["specifiers"]
   >
@@ -31,24 +42,28 @@ function getMemberImports(
   return specifier.type === "ImportSpecifier";
 }
 
+// The main plugin.
 export default (babel: Babel): { name: string; visitor: Visitor } => {
   const types = babel.types;
   return {
     name: "frontity",
     visitor: {
+      // Use Program instead of ImportDeclaration because the Emotion plugin is also using
+      // Program and this needs to be faster to be able to make the changes before emotion does.
       Program(programPath) {
         programPath.traverse({
           ImportDeclaration: function(
             path: NodePath<BabelTypes.ImportDeclaration>
           ) {
+            // Array to store all the our import transforms.
             const transforms: BabelTypes.ImportDeclaration[] = [];
 
-            const source = path.node.source.value;
-
-            if (source === "frontity") {
+            if (path.node.source.value === "frontity") {
+              // 1. Get only the named imports.
               const memberImports = path.node.specifiers.filter(
-                getMemberImports
+                isImportSpecifier
               );
+              // 2. Check if any of them is one of the ones we have to substitute.
               const hasImports = memberImports
                 .map(memberImport => memberImport.imported.name)
                 .filter(name => allNames.includes(name))
@@ -59,6 +74,7 @@ export default (babel: Babel): { name: string; visitor: Visitor } => {
                   const memberName = memberImport.imported.name;
 
                   if (defaultImports[memberName]) {
+                    // 3. For default imports, it needs to pass types.importDefaultSpecifier.
                     transforms.push(
                       types.importDeclaration(
                         [
@@ -70,6 +86,7 @@ export default (babel: Babel): { name: string; visitor: Visitor } => {
                       )
                     );
                   } else if (namedImports[memberName]) {
+                    // 4. For named imports, the member import is enough.
                     transforms.push(
                       types.importDeclaration(
                         [memberImport],
@@ -77,6 +94,7 @@ export default (babel: Babel): { name: string; visitor: Visitor } => {
                       )
                     );
                   } else {
+                    // 5. The rest of the imports are just preserved under "frontity".
                     transforms.push(
                       types.importDeclaration(
                         [memberImport],
@@ -88,6 +106,7 @@ export default (babel: Babel): { name: string; visitor: Visitor } => {
               }
             }
 
+            // 6. Replace the original import with all the new imports.
             if (transforms.length > 0) {
               path.replaceWithMultiple(transforms);
             }
