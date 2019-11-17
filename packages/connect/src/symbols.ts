a2fa490c9258d635a16a2c64023c6b3435234568
Luis Herranz <luisherranz@gmail.com>
2019-11-17 23:48:29 +0800

    chore(connect): first work on Connect class

--- packages/connect/src/symbols.ts
+++ packages/connect/src/symbols.ts
@@ -6,3 +6,4 @@ export const OBSERVABLE_STATE = Symbol("OBSERVABLE_STATE");
 export const MUTABLE_STATE = Symbol("MUTABLE_STATE");
 export const OBSERVABLE_ACTIONS = Symbol("OBSERVABLE_ACTIONS");
 export const EXECUTABLE_ACTIONS = Symbol("EXECUTABLE_ACTIONS");
+export const STORE = Symbol("STORE");
