913d64ab927e61218cb7e4a6dc6084afdd422f94
Luis Herranz <luisherranz@gmail.com>
2019-11-27 09:54:31 +0100

    fix(loadable): fix secondary chunks

--- packages/core/src/server/utils/__tests__/stats.tests.ts
+++ packages/core/src/server/utils/__tests__/stats.tests.ts
@@ -2,32 +2,122 @@ import { hasEntryPoint, getBothScriptTags } from "../stats";
 
 describe("hasEntryPoint", () => {
   test("should return true if the entry points exists", () => {
-    const stats = { assetsByChunkName: { "site-1": "site-1.js" } };
-    expect(hasEntryPoint({ site: "site-1", stats })).toBe(true);
+    const stats = { assetsByChunkName: { "chunk-1": "chunk-1.js" } };
+    expect(hasEntryPoint({ site: "chunk-1", stats })).toBe(true);
   });
   test("should return false if the entry points doesn't exist", () => {
     const stats = { assetsByChunkName: {} };
-    expect(hasEntryPoint({ site: "site-1", stats })).toBe(false);
+    expect(hasEntryPoint({ site: "chunk-1", stats })).toBe(false);
   });
 });
 
 describe("getBothScriptTags", () => {
-  test("should return both es5 and module tags", () => {
+  test("should return both es5 and module tags in development", () => {
     const moduleStats = {
       assetsByChunkName: {
-        "site-1": "site-1.module.js",
-        "site-2": "site-2.module.js"
+        "chunk-1": "chunk-1.module.js",
+        "chunk-2": "chunk-2.module.js"
       }
     };
     const es5Stats = {
       assetsByChunkName: {
-        "site-1": "site-1.es5.js",
-        "site-2": "site-2.es5.js"
+        "chunk-1": "chunk-1.es5.js",
+        "chunk-2": "chunk-2.es5.js"
       }
     };
     const extractor = {
       publicPath: "/public-path/",
-      getMainAssets: () => [{ chunk: "site-1" }, { chunk: "site-2" }],
+      getMainAssets: () => [
+        { filename: "chunk-1.module.js" },
+        { filename: "chunk-2.module.js" }
+      ],
+      getRequiredChunksScriptTag: (arg: {}) =>
+        !!arg && "<script>REQ-CHUNK-SCRIPT-TAG</script>"
+    };
+    expect(
+      getBothScriptTags({ moduleStats, es5Stats, extractor })
+    ).toMatchSnapshot();
+  });
+
+  test("should return both es5 and module tags in production", () => {
+    const moduleStats = {
+      assetsByChunkName: {
+        "chunk-1": "chunk-1.module.123.js",
+        "chunk-2": "chunk-2.module.123.js"
+      }
+    };
+    const es5Stats = {
+      assetsByChunkName: {
+        "chunk-1": "chunk-1.es5.456.js",
+        "chunk-2": "chunk-2.es5.456.js"
+      }
+    };
+    const extractor = {
+      publicPath: "/public-path/",
+      getMainAssets: () => [
+        { filename: "chunk-1.module.123.js" },
+        { filename: "chunk-2.module.123.js" }
+      ],
+      getRequiredChunksScriptTag: (arg: {}) =>
+        !!arg && "<script>REQ-CHUNK-SCRIPT-TAG</script>"
+    };
+    expect(
+      getBothScriptTags({ moduleStats, es5Stats, extractor })
+    ).toMatchSnapshot();
+  });
+
+  test("should work with secondary chunks in development", () => {
+    const moduleStats = {
+      assetsByChunkName: {
+        "chunk-1": "chunk-1.module.js",
+        "chunk-2": "chunk-2.module.js",
+        "chunk-1~chunk-2": "chunk-1~chunk-2.module.js"
+      }
+    };
+    const es5Stats = {
+      assetsByChunkName: {
+        "chunk-1": "chunk-1.es5.js",
+        "chunk-2": "chunk-2.es5.js",
+        "chunk-1~chunk-2": "chunk-1~chunk-2.es5.js"
+      }
+    };
+    const extractor = {
+      publicPath: "/public-path/",
+      getMainAssets: () => [
+        { filename: "chunk-1.module.js" },
+        { filename: "chunk-2.module.js" },
+        { filename: "chunk-1~chunk-2.module.js" }
+      ],
+      getRequiredChunksScriptTag: (arg: {}) =>
+        !!arg && "<script>REQ-CHUNK-SCRIPT-TAG</script>"
+    };
+    expect(
+      getBothScriptTags({ moduleStats, es5Stats, extractor })
+    ).toMatchSnapshot();
+  });
+
+  test("should work with secondary chunks in production", () => {
+    const moduleStats = {
+      assetsByChunkName: {
+        "chunk-1": "chunk-1.module.123.js",
+        "chunk-2": "chunk-2.module.123.js",
+        "chunk-1~chunk-2": "chunk-1~chunk-2.module.123.js"
+      }
+    };
+    const es5Stats = {
+      assetsByChunkName: {
+        "chunk-1": "chunk-1.es5.456.js",
+        "chunk-2": "chunk-2.es5.456.js",
+        "chunk-1~chunk-2": "chunk-1~chunk-2.es5.456.js"
+      }
+    };
+    const extractor = {
+      publicPath: "/public-path/",
+      getMainAssets: () => [
+        { filename: "chunk-1.module.123.js" },
+        { filename: "chunk-2.module.123.js" },
+        { filename: "chunk-1~chunk-2.module.123.js" }
+      ],
       getRequiredChunksScriptTag: (arg: {}) =>
         !!arg && "<script>REQ-CHUNK-SCRIPT-TAG</script>"
     };
