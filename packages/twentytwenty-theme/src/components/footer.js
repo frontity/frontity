81794b333f7e7a50497bad1b4b9f42484de0d5b3
Luis Herranz <luisherranz@gmail.com>
2019-12-12 19:52:30 +0100

    Acknowledge both WordPress and Frontity

--- packages/twentytwenty-theme/src/components/footer.js
+++ packages/twentytwenty-theme/src/components/footer.js
@@ -35,7 +35,7 @@ const Footer = ({ state }) => {
             &copy; {currentYear}{" "}
             <Link link={state.frontity.url}>{state.frontity.title}</Link>
           </Copyright>
-          <PoweredBy>Powered by Frontity</PoweredBy>
+          <PoweredBy>Powered by WordPress & Frontity</PoweredBy>
         </Credits>
         <BackToTop />
       </SiteFooterInner>
