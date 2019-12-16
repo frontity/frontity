ac2bb8ab568f90208dbfc58a59a36121c5724980
David Arenas <darensr@gmail.com>
2019-12-16 16:51:43 +0100

    Move `name` to the FrontityError's prototype

--- packages/error/index.tsx
+++ packages/error/index.tsx
@@ -1,9 +1,10 @@
 export class FrontityError extends Error {
-  name = "FrontityError";
-
   constructor(message: string) {
     super(
       `${message}\n\nIf you have any doubts, join our community at https://community.frontity.org/.\n`
     );
   }
 }
+
+// Add the `name` property to the class prototype.
+FrontityError.prototype.name = "FrontityError";
