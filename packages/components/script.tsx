d855db3e07f7508519f98f19ade00093eeb1c554
Uche Jude <iamuchejude@gmail.com>
2020-01-16 00:00:03 +0100

    fix(html2react): script component re-render

--- packages/components/script.tsx
+++ packages/components/script.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import { connect } from 'frontity';
-import { Connect, Package } from 'frontity/types';
+import React, { useRef, useEffect } from "react";
+import { connect } from "frontity";
+import { Connect, Package } from "frontity/types";
 
-type ScriptType = React.FC<Connect<Package, object>>;
+type ScriptType = React.FC<Connect<Package, { src: string }>>;
 
-const Script: ScriptType  = ({ state, actions, libraries, ...props }) => {
-  if (state.frontity.rendering === "csr") {
-    const script = window.document.createElement('script');
-    script.src = props.src;
-    script.async = true;
-    script.charset = 'utf-8';
+const generateId = () => Math.trunc(Math.random() * 9999999).toString();
 
+const Script: ScriptType = ({ state, ...props }) => {
+  const scriptId = useRef(generateId());
+  const { src } = props;
 
-    window.document.body.appendChild(script)
-  }
-  
-  return <script async {...props} />
+  useEffect(() => {
+    if (state.frontity.rendering === "csr") {
+      const script = window.document.createElement("script");
+
+      script.src = src;
+      script.id = scriptId.current;
+      script.async = true;
+      script.charset = "utf-8";
+
+      window.document.body.appendChild(script);
+    }
+
+    return () => {
+      const script = document.getElementById(scriptId.current);
+      window.document.body.removeChild(script);
+    };
+  }, []);
+
+  return <script async src={src} charSet="utf-8" />;
 };
 
 export default connect(Script);
