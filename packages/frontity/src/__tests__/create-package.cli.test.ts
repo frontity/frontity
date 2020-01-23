713d45453ebd81383f13e592b2a1ecc463fcabf5
Michal Czaplinski <mmczaplinski@gmail.com>
2020-01-23 17:54:22 -0500

    CLI: Fix tests

--- packages/frontity/src/__tests__/create-package.cli.test.ts
+++ packages/frontity/src/__tests__/create-package.cli.test.ts
@@ -1,24 +1,32 @@
 import createPackage from "../cli/create-package";
 import inquirer from "inquirer";
 import * as utils from "../utils";
+import * as createCmd from "../commands/create-package";
+import { EventPromised } from "../utils/eventPromised";
 
 jest.mock("inquirer");
+jest.mock("../commands/create-package");
 
 const mockedInquirer = inquirer as jest.Mocked<typeof inquirer>;
 const mockedUtils = utils as any;
 const mockedExit = jest.spyOn(process, "exit");
-
-beforeEach(() => {
-  mockedInquirer.prompt.mockRestore();
-  mockedUtils.isFrontityProjectRoot = jest.fn();
-  mockedUtils.errorLogger = jest.fn();
-  mockedExit.mockRestore();
-  mockedExit.mockImplementation(() => {
-    throw new Error();
-  });
-});
+const mockedCreate = createCmd as jest.Mocked<typeof createCmd>;
 
 describe("create-package", () => {
+  beforeEach(() => {
+    mockedInquirer.prompt.mockRestore();
+    mockedUtils.isFrontityProjectRoot = jest.fn();
+    mockedUtils.errorLogger = jest.fn();
+    mockedCreate.default.mockReset();
+    mockedCreate.default.mockReturnValue(
+      new EventPromised(resolve => resolve())
+    );
+    mockedExit.mockRestore();
+    mockedExit.mockImplementation(() => {
+      throw new Error();
+    });
+  });
+
   test("should not ask for passed options", async done => {
     const name = "example-theme";
     const namespace = "theme";
