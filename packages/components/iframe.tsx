635d83c5b9c2bd2a1a7b7641d3bbdd949863ec9a
Uche Jude <iamuchejude@gmail.com>
2020-01-28 15:36:39 +0100

    Component: initial implementation of iframe component

--- packages/components/iframe.tsx
+++ packages/components/iframe.tsx
@@ -1,23 +1,165 @@
 import React from "react";
-import { connect } from "frontity";
+import { connect, Head } from "frontity";
 import { Connect, Package } from "frontity/types";
+import { useInView } from "react-intersection-observer";
+
+// TODO: Review and Refactor;
+
+const noJsStyles = `
+  :non(noscript) > .frontity-lazy-iframe {
+    display: none;
+  }
+`;
+
+// No Proxy === No Frontity
+const noProxyScript = `
+  if (!("Proxy" in window) || !("IntersectionObserver" in window)) {
+    document.addEventListener("DOMContentLoaded", () => {
+      let iframes = document.querySelectorAll("img.frontity-lazy-iframe");
+
+      iframes.forEach(iframe => {
+         iframe.setAttribute("src", iframe.getAttribute("data-src"));
+
+        iframe.removeAttribute("data-src");
+        iframe.removeAttribute("style");
+      });
+    });
+  }
+`;
 
 interface Props {
-  src?: string;
-  title?: string;
-  width?: string;
-  height?: string;
+  src: string;
+  title: string;
+  width?: number;
+  height?: number;
   id?: string;
+  className?: string;
+  rootMargin?: string;
+  loading?: "auto" | "lazy" | "eager";
 }
 
 type Component = React.FC<Connect<Package, Props>>;
 
-// WIP
-const Iframe: Component = ({ state, ...props }) => {
-  const { src, title, width, height, id } = props;
+interface Attributes extends Props {
+  "data-src"?: string;
+  style?: { visibility: "hidden" };
+}
+
+type NoScriptIframe = React.FC<Attributes>;
+
+interface ChangeAttributes {
+  (attrs: Attributes): Attributes;
+}
+
+const changeAttributes: ChangeAttributes = attrs => {
+  const attributes = { ...attrs };
+
+  attributes.src = attributes["data-src"];
+
+  delete attributes["data-src"];
+  delete attributes["style"];
+
+  return attributes;
+};
+
+const NoScriptIframe: NoScriptIframe = props => {
+  const attributes = { ...props };
+
+  console.log("Condition 1");
 
   return (
-    <iframe src={src} title={title} width={width} height={height} id={id} />
+    <noscript>
+      <iframe {...attributes} />
+    </noscript>
+  );
+};
+
+// WIP
+const Iframe: Component = ({ state, ...props }) => {
+  const {
+    src,
+    title,
+    width,
+    height,
+    id,
+    className,
+    loading = "lazy",
+    rootMargin
+  } = props;
+
+  const lazyAttributes: Attributes = {
+    "data-src": src,
+    className: "frontity-lazy-iframe".concat(className ? `${className}` : ""),
+    loading,
+    style: { visibility: "hidden" },
+    height,
+    width,
+    title,
+    id
+  };
+  const eagerAttributes = changeAttributes(lazyAttributes);
+
+  if (loading === "eager") {
+    console.log("Condition 2");
+    return <img {...eagerAttributes} />;
+  }
+
+  if (typeof window !== undefined) {
+    if (
+      typeof IntersectionObserver !== "undefined" &&
+      !("loading" in HTMLIFrameElement.prototype)
+    ) {
+      const [ref, inView] = useInView({
+        rootMargin: rootMargin,
+        triggerOnce: true
+      });
+
+      console.log("Condition 3");
+
+      return (
+        <>
+          <NoScriptIframe {...eagerAttributes} />
+          <iframe ref={ref} {...(inView ? eagerAttributes : lazyAttributes)} />
+        </>
+      );
+    }
+
+    console.log("Condition 4");
+
+    return (
+      <>
+        <NoScriptIframe {...eagerAttributes} />
+        <iframe
+          {...(state.frontity.rendering === "csr"
+            ? eagerAttributes
+            : lazyAttributes)}
+          suppressHydrationWarning
+        />
+      </>
+    );
+  }
+
+  console.log("Condition 5");
+
+  return (
+    <>
+      <Head
+        script={[
+          {
+            id: "frontity-no-proxy-iframe",
+            type: "text/javascript",
+            innerHTML: noProxyScript
+          }
+        ]}
+        noscript={[
+          {
+            innerHTML: `<style id="frontity-no-js-iframes" type="text/css">${noJsStyles}</style>`
+          }
+        ]}
+      />
+      <NoScriptIframe {...eagerAttributes} />
+      <img {...lazyAttributes} />
+    </>
   );
 };
 
--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Global, css, connect, styled, Head } from "frontity";
+import Iframe from "@frontity/components/iframe";
 import Header from "./header";
 import List from "./list";
 import Post from "./post";
@@ -41,6 +42,31 @@ const Theme = ({ state }) => {
           (data.isPostType && <Post />) ||
           (data.is404 && <Page404 />)}
       </Main>
+
+      <Iframe
+        src="https://frontity.org"
+        title="Frontity"
+        width="500"
+        height="400"
+      />
+      <Iframe
+        src="https://olaolu.dev"
+        title="Olaolu"
+        width="500"
+        height="400"
+      />
+      <Iframe
+        src="https://codewonders.dev"
+        title="Codewonders"
+        width="500"
+        height="400"
+      />
+      <Iframe
+        src="https://gideonoladimeji.com"
+        title="Gideon"
+        width="500"
+        height="400"
+      />
     </>
   );
 };
