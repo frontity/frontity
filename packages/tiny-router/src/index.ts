b8c4f8bbb40867575d667a6860dc95010591939a
Michal Czaplinski <mmczaplinski@gmail.com>
2020-02-07 18:24:39 -0500

    tiny-router: Add correct source param to state

--- packages/tiny-router/src/index.ts
+++ packages/tiny-router/src/index.ts
@@ -1,5 +1,6 @@
 import TinyRouter from "../types";
 import { set, init, beforeSSR } from "./actions";
+import WPSource from "@frontity/wp-source/src";
 
 const tinyRouter: TinyRouter = {
   name: "@frontity/tiny-router",
@@ -7,7 +8,8 @@ const tinyRouter: TinyRouter = {
     router: {
       link: "/",
       autoFetch: true
-    }
+    },
+    source: WPSource().state.source // TODO: notr sure if that's correct
   },
   actions: {
     router: {
