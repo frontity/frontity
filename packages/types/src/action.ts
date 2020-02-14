d77aca49517f8d359b67a2cebf61c32e8c0e6696
Luis Herranz <luisherranz@gmail.com>
2020-02-14 08:35:39 +0000

    Export context
    I think that we can leave ServerAction as both async and requiring a
    Context for the moment.

--- packages/types/src/action.ts
+++ packages/types/src/action.ts
@@ -23,34 +23,36 @@ export type Action<Pkg extends Package, Input = null> = [Input] extends [null]
     }) => (args: Input) => void;
 
 /**
- *  Type for asynchronous actions.
- *  The `Input` corresponds to the second (optional) curried parameter passed to the action.abs
+ * Type for asynchronous actions.
+ * The `Input` corresponds to the second (optional) curried parameter passed
+ * to the action.abs.
  *
- *  @example
- *  ```
- *  import { AsyncAction } from '@frontity/types';
- *  interface Input = {
- *    input: string;
- *  }
+ * @example
+ * ```
+ * import { AsyncAction } from '@frontity/types';
+ * interface Input = {
+ *   input: string;
+ * }
  *
- *  const actions: { myCustomAction: AsyncAction<MyPackage, Input> } = {
- *    myCustomAction: ({ state, libraries }) => async ({ input }) => {
- *      const result = await somePromise();
- *      console.log(input);
- *     }
- *  }
- *  ```
+ * const actions: { myCustomAction: AsyncAction<MyPackage, Input> } = {
+ *   myCustomAction: ({ state, libraries }) => async ({ input }) => {
+ *     const result = await somePromise();
+ *     console.log(input);
+ *    }
+ * }
+ * ```
  *
- *  Or simply:
- *  @example
- *  ```
- *  import { AsyncAction } from '@frontity/types';
+ * Or simply:
+ * @example
+ * ```
+ * import { AsyncAction } from '@frontity/types';
  *
- *  const actions: { myCustomAction: AsyncAction<MyPackage>} = {
- *    myCustomAction: async ({ state, libraries }) => {
- *      const result = await somePromise();
- *     }
- *  }
+ * const actions: { myCustomAction: AsyncAction<MyPackage>} = {
+ *   myCustomAction: async ({ state, libraries }) => {
+ *     const result = await somePromise();
+ *    }
+ * }
+ * ```
  *
  */
 export type AsyncAction<Pkg extends Package, Input = null> = [Input] extends [
@@ -75,12 +77,9 @@ export type AsyncAction<Pkg extends Package, Input = null> = [Input] extends [
       libraries: Pkg["libraries"];
     }) => (args: Input) => Promise<void>;
 
-type Context = {
-  ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>;
-};
+export type Context = Koa.ParameterizedContext<
+  Koa.DefaultState,
+  Koa.DefaultContext
+>;
 
-export type ServerAction<Pkg> =
-  | Action<Pkg>
-  | Action<Pkg, Context>
-  | AsyncAction<Pkg>
-  | AsyncAction<Pkg, Context>;
+export type ServerAction<Pkg> = AsyncAction<Pkg, { ctx: Context }>;
