cb59b96c61893831e82b810bb990253827593290
Luis Herranz <luisherranz@gmail.com>
2020-02-14 08:29:29 +0000

    Refactor test file
    Move each action to its own describe and add mocks for source state and
    actions

--- packages/tiny-router/src/__tests__/index.test.ts
+++ packages/tiny-router/src/__tests__/index.test.ts
@@ -1,10 +1,11 @@
 import { createStore, observe } from "frontity";
-import { init, set } from "../actions";
 import tinyRouter from "..";
 import TinyRouter from "../../types";
 
 let config: TinyRouter;
 let normalize: jest.Mock;
+let fetch: jest.Mock;
+let get: jest.Mock;
 
 beforeEach(() => {
   normalize = jest.fn();
@@ -16,12 +17,17 @@ beforeEach(() => {
         platform: "server",
         initialLink: "/initial/link/"
       },
-      router: { ...tinyRouter.state.router }
+      router: { ...tinyRouter.state.router },
+      source: {
+        get
+      }
     },
     actions: {
       router: {
-        init,
-        set
+        ...tinyRouter.actions.router
+      },
+      source: {
+        fetch
       }
     },
     libraries: {
@@ -33,68 +39,72 @@ beforeEach(() => {
 });
 
 describe("actions", () => {
-  test("set() should work just with links", () => {
-    const store = createStore(config);
+  describe("set", () => {
+    test("should work just with links", () => {
+      const store = createStore(config);
 
-    const link = "/some-post/";
-    const normalized = "/some-post/";
+      const link = "/some-post/";
+      const normalized = "/some-post/";
 
-    normalize.mockReturnValue(normalized);
+      normalize.mockReturnValue(normalized);
 
-    store.actions.router.set(link);
-    expect(normalize).toHaveBeenCalledWith(link);
-    expect(store.state.router.link).toBe(normalized);
-  });
-
-  test("set() should work with full URLs", () => {
-    const store = createStore(config);
-
-    const link = "https://blog.example/some-post/page/3/?some=query";
-    const normalized = "/some-post/page/3/?some=query";
-
-    normalize.mockReturnValue(normalized);
-
-    store.actions.router.set(link);
-    expect(normalize).toHaveBeenCalledWith(link);
-    expect(store.state.router.link).toBe(normalized);
-  });
-
-  test("init() should populate the initial link", () => {
-    const store = createStore(config);
-
-    normalize.mockReturnValue("/initial/link/");
-
-    store.actions.router.init();
-
-    // check that first state is correct
-    expect(normalize).toHaveBeenCalledTimes(1);
-    expect(normalize).toHaveBeenCalledWith("/initial/link/");
-    expect(store.state.router.link).toBe("/initial/link/");
-  });
-
-  test("init() should add event listener to handle popstate events", () => {
-    config.state.frontity.platform = "client";
-    const store = createStore(config);
-    store.actions.router.init();
-
-    normalize.mockReturnValueOnce("/tag/japan/page/3/");
-
-    // check that first state is correct
-    expect(window.history.state).toMatchObject({ link: "/" });
-
-    // check reactions to "popstate" events
-    let currentLink = store.state.router.link;
-    observe(() => {
-      currentLink = store.state.router.link;
+      store.actions.router.set(link);
+      expect(normalize).toHaveBeenCalledWith(link);
+      expect(store.state.router.link).toBe(normalized);
     });
 
-    window.dispatchEvent(
-      new PopStateEvent("popstate", {
-        state: { link: "/tag/japan/page/3/" }
-      })
-    );
+    test("set() should work with full URLs", () => {
+      const store = createStore(config);
 
-    expect(currentLink).toBe("/tag/japan/page/3/");
-    expect(store.state.router.link).toBe("/tag/japan/page/3/");
+      const link = "https://blog.example/some-post/page/3/?some=query";
+      const normalized = "/some-post/page/3/?some=query";
+
+      normalize.mockReturnValue(normalized);
+
+      store.actions.router.set(link);
+      expect(normalize).toHaveBeenCalledWith(link);
+      expect(store.state.router.link).toBe(normalized);
+    });
+  });
+
+  describe("init", () => {
+    test("should populate the initial link", () => {
+      const store = createStore(config);
+
+      normalize.mockReturnValue("/initial/link/");
+
+      store.actions.router.init();
+
+      // check that first state is correct
+      expect(normalize).toHaveBeenCalledTimes(1);
+      expect(normalize).toHaveBeenCalledWith("/initial/link/");
+      expect(store.state.router.link).toBe("/initial/link/");
+    });
+
+    test("should add event listener to handle popstate events", () => {
+      config.state.frontity.platform = "client";
+      const store = createStore(config);
+      store.actions.router.init();
+
+      normalize.mockReturnValueOnce("/tag/japan/page/3/");
+
+      // check that first state is correct
+      expect(window.history.state).toMatchObject({ link: "/" });
+
+      // check reactions to "popstate" events
+      let currentLink = store.state.router.link;
+      observe(() => {
+        currentLink = store.state.router.link;
+      });
+
+      window.dispatchEvent(
+        new PopStateEvent("popstate", {
+          state: { link: "/tag/japan/page/3/" }
+        })
+      );
+
+      expect(currentLink).toBe("/tag/japan/page/3/");
+      expect(store.state.router.link).toBe("/tag/japan/page/3/");
+    });
   });
 });
