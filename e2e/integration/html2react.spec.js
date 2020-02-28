eeedb4091fb7f33b02836928031485ce1673fe34
David Arenas <darensr@gmail.com>
2020-02-28 16:03:43 +0100

    Html2React: add e2e specs

---
+++ e2e/integration/html2react.spec.js
@@ -0,0 +1,18 @@
+describe("Html2React", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3001?name=html2react");
+  });
+
+  it("should pass state to processors", () => {
+    cy.get("p").should("have.css", "color", "rgb(0, 0, 255)");
+  });
+
+  it("should re-render when the state is updated", () => {
+    cy.get("button#change-color").click();
+    cy.get("p").should("have.css", "color", "rgb(255, 0, 0)");
+  });
+
+  it("should work with old processors", () => {
+    cy.get("span#old-processors").should("have.text", "Yes");
+  });
+});
