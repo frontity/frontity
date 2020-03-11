46e7f348eb04457346135f90fbde4a473ac53b44
Luis Herranz <luisherranz@gmail.com>
2020-03-11 12:36:52 +0100

    Don't declare the getTotal types twice

--- packages/wp-source/src/libraries/response-utils.ts
+++ packages/wp-source/src/libraries/response-utils.ts
@@ -1,13 +1,14 @@
-export const getTotal = (
-  response: Response,
-  valueIfHeaderMissing: number
-): number =>
-  parseInt(response.headers.get("X-WP-Total")) || valueIfHeaderMissing || 0;
+import WpSource from "../../types";
 
-export const getTotalPages = (
-  response: Response,
-  valueIfHeaderMissing: number
-): number =>
+export const getTotal: WpSource["libraries"]["source"]["getTotal"] = (
+  response,
+  valueIfHeaderMissing
+) => parseInt(response.headers.get("X-WP-Total")) || valueIfHeaderMissing || 0;
+
+export const getTotalPages: WpSource["libraries"]["source"]["getTotal"] = (
+  response,
+  valueIfHeaderMissing
+) =>
   parseInt(response.headers.get("X-WP-TotalPages")) ||
   valueIfHeaderMissing ||
   0;
