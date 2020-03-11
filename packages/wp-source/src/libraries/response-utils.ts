edf4e0abe58a1bd63b8d49219cad822f07dac638
Luis Herranz <luisherranz@gmail.com>
2020-03-11 09:00:19 +0100

    Add fallbacks to 0 for getTotal libraries
    So they cannot be undefined.

--- packages/wp-source/src/libraries/response-utils.ts
+++ packages/wp-source/src/libraries/response-utils.ts
@@ -1,7 +1,7 @@
 export const getTotal = (response: Response, _default: number): number =>
-  parseInt(response.headers.get("X-WP-Total")) || _default;
+  parseInt(response.headers.get("X-WP-Total")) || _default || 0;
 
 export const getTotalPages = (response: Response, _default: number): number =>
-  parseInt(response.headers.get("X-WP-TotalPages")) || _default;
+  parseInt(response.headers.get("X-WP-TotalPages")) || _default || 0;
 
 export default { getTotal, getTotalPages };
