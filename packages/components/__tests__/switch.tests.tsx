56cbb356829ba3fc9dc96e81e4610cc201b2447c
Luis Herranz <luisherranz@gmail.com>
2020-04-01 10:40:23 +0200

    Switch: tests for passing down props

--- packages/components/__tests__/switch.tests.tsx
+++ packages/components/__tests__/switch.tests.tsx
@@ -3,8 +3,10 @@ import { create } from "react-test-renderer";
 import Switch from "../switch";
 
 // Test components
-const TestArchive: React.FC<{ when?: boolean }> = () => {
-  return <h1>Test Archive Component</h1>;
+const TestArchive: React.FC<{ when?: boolean; someProp?: string }> = ({
+  someProp,
+}) => {
+  return <h1>Test Archive Component{someProp}</h1>;
 };
 
 const TestBlog: React.FC<{ when?: boolean }> = () => {
@@ -12,12 +14,12 @@ const TestBlog: React.FC<{ when?: boolean }> = () => {
 };
 
 // Default test component
-const Default: React.FC = () => {
-  return <h1>Default Component</h1>;
+const Default: React.FC<{ someProp?: string }> = ({ someProp }) => {
+  return <h1>Default Component{someProp}</h1>;
 };
 
 describe("Switch", () => {
-  test("render default component if no match was found", () => {
+  test("should render the default component if no match was found", () => {
     const SwitchComponent = create(
       <Switch>
         <TestArchive when={"archive" !== "archive"} />
@@ -46,6 +48,26 @@ describe("Switch", () => {
       <Switch>
         <TestArchive when={"archive" !== "archive"} />
         <TestBlog when={"blog" !== "blog"} />
+      </Switch>
+    );
+
+    expect(SwitchComponent.toJSON()).toMatchSnapshot();
+  });
+
+  test("should pass down any prop that the matching component receives", () => {
+    const SwitchComponent = create(
+      <Switch>
+        <TestArchive when={true} someProp="prop value" />
+      </Switch>
+    );
+
+    expect(SwitchComponent.toJSON()).toMatchSnapshot();
+  });
+
+  test("should pass down any prop of the default component", () => {
+    const SwitchComponent = create(
+      <Switch>
+        <Default someProp="prop value" />
       </Switch>
     );
 
