7e4e51c1aa58dfe253bfd4f26aa015523ef6a110
David Arenas <darensr@gmail.com>
2020-04-17 10:59:52 +0200

    Implement TypeScript tests

--- packages/html2react/__test__/types.test.ts
+++ packages/html2react/__test__/types.test.ts
@@ -1,75 +1,96 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { Processor, Element, Text, Component } from "../types";
-import IFrame from "@frontity/components/iframe";
+import { Processor, Node, Element, Text, Comment } from "../types";
 
 // Simple functions to check types.
 export const expectType = <T>(value: T) => {
   value;
 };
 
-// Default processor should
-const defaultProcessor: Processor<Element> = {
-  test: () => true,
-  processor: ({ node }) => {
-    expectType<"element" | "text">(node.type);
-    node.props.onMouseDown;
-    node.props;
-    return {
-      component: IFrame,
-      whatever: "props",
-    };
-  },
-};
+// 1. Processor by default accepts Element, Text or Comment.
+const proc1: Processor = {
+  test: ({ node }) => {
+    expectType<"element" | "text" | "comment">(node.type);
 
-const processorWithComment: Processor<{
-  type: "comment";
-  content: "string";
-}> = {
-  test: () => true,
-  processor: ({ node }) => {
-    expectType<"comment">(node.type);
-    expectType<string>(node.content);
-    return {
-      type: "element",
-      component: IFrame,
-    };
-  },
-};
+    if (node.type === "element") expectType<Element>(node);
+    if (node.type === "text") expectType<Text>(node);
+    if (node.type === "comment") expectType<Comment>(node);
 
-const processorWithText: Processor<{
-  type: "text";
-  content: "string";
-}> = {
-  test: () => true,
-  processor: ({ node }) => {
-    expectType<"text">(node.type);
-    expectType<string>(node.content);
-    return {
-      type: "element",
-      component: IFrame,
-    };
+    return true;
   },
-};
-
-const processorWithElementDef: Processor<{
-  type: "element";
-  component: Component;
-  props: Element["props"] & {
-    myOwnProp: string;
-  };
-}> = {
-  test: () => true,
   processor: ({ node }) => {
-    expectType<"element">(node.type);
-    expectType<Component>(node.component);
-    expectType<string>(node.props.src);
-    expectType<string>(node.props.title);
-    expectType<string>(node.props.myOwnProp);
+    expectType<"element" | "text" | "comment">(node.type);
+
+    if (node.type === "element") expectType<Element>(node);
+    if (node.type === "text") expectType<Text>(node);
+    if (node.type === "comment") expectType<Comment>(node);
+
     return node;
   },
 };
 
+// 2. Processor with default Element should receive correct types.
+const proc2: Processor<Element> = {
+  test: ({ node }) => {
+    expectType<Element>(node);
+    expectType<string | React.ComponentType>(node.component);
+    expectType<Element>(node.parent);
+    expectType<Node[]>(node.children);
+
+    // Test CSS prop.
+    expectType<Element["props"]["css"]>(node.props.css);
+
+    // Test some HTML props.
+    expectType<string>(node.props.id);
+    expectType<string>(node.props.className);
+    expectType<string>(node.props.src);
+    expectType<string>(node.props.srcSet);
+
+    return true;
+  },
+  processor: ({ node }) => {
+    expectType<Element>(node);
+    expectType<string | React.ComponentType>(node.component);
+    expectType<Element>(node.parent);
+    expectType<Node[]>(node.children);
+
+    // Test CSS prop.
+    expectType<Element["props"]["css"]>(node.props.css);
+
+    // Test some HTML props.
+    expectType<string>(node.props.id);
+    expectType<string>(node.props.className);
+    expectType<string>(node.props.src);
+    expectType<string>(node.props.srcSet);
+
+    return node;
+  },
+};
+
+// 3. Element can be extended with custom props.
+interface CustomElement extends Element {
+  props: Element["props"] & {
+    hasCustomProp: true;
+  };
+}
+
+const proc3: Processor<CustomElement> = {
+  test: ({ node }) => {
+    expectType<Element>(node);
+    expectType<CustomElement>(node);
+    expectType<true>(node.props.hasCustomProp);
+
+    return true;
+  },
+  processor: ({ node }) => {
+    expectType<Element>(node);
+    expectType<CustomElement>(node);
+    expectType<true>(node.props.hasCustomProp);
+
+    return node;
+  },
+};
+
+// 4. Element can be extended with custom relatives.
 interface MyElementParent extends Element {
   props: Element["props"] & {
     isParent: true;
@@ -77,19 +98,40 @@ interface MyElementParent extends Element {
 }
 
 interface MyElement extends Element {
-  type: "element";
-  component: string;
   props: Element["props"] & {
     isChild: true;
   };
   parent: MyElementParent;
+  children: [Element, Text, Comment];
 }
 
-const processorWithElementAndParent: Processor<MyElement> = {
-  test: () => true,
-  processor: ({ node }) => {
-    expectType<"element">(node.type);
+const proc4: Processor<MyElement> = {
+  test: ({ node }) => {
+    // Test node.
+    expectType<MyElement>(node);
+    expectType<true>(node.props.isChild);
+    // Test parent.
+    expectType<MyElementParent>(node.parent);
     expectType<true>(node.parent.props.isParent);
+    // Test children.
+    expectType<Element>(node.children[0]);
+    expectType<Text>(node.children[1]);
+    expectType<Comment>(node.children[2]);
+
+    return true;
+  },
+  processor: ({ node }) => {
+    // Test node.
+    expectType<MyElement>(node);
+    expectType<true>(node.props.isChild);
+    // Test parent.
+    expectType<MyElementParent>(node.parent);
+    expectType<true>(node.parent.props.isParent);
+    // Test children.
+    expectType<Element>(node.children[0]);
+    expectType<Text>(node.children[1]);
+    expectType<Comment>(node.children[2]);
+
     return node;
   },
 };
