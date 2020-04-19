20232c6562ec490ef86c9315ef0e03c71f0c588b
David Arenas <darensr@gmail.com>
2020-04-19 14:41:50 +0200

    Write gtm spec file

--- e2e/integration/google-tag-manager.spec.js
+++ e2e/integration/google-tag-manager.spec.js
@@ -1,17 +1,55 @@
 describe("Google Tag Manager", () => {
-  beforeEach(() => {
+  const pageviewHome = {
+    event: "virtualPageview",
+    pageview: {
+      page: "/?name=google-tag-manager",
+      title: "Homepage Title",
+    },
+  };
+
+  const pageviewSomePost = {
+    event: "virtualPageview",
+    pageview: {
+      page: "/some-post/",
+      title: "Some Post Title",
+    },
+  };
+
+  const someEvent = {
+    event: "virtualEvent",
+    virtualEvent: { some: "content" },
+  };
+
+  beforeAll(() => {
     cy.visit("http://localhost:3001?site=google-tag-manager");
   });
 
-  it("should load Google Tag Manager library", () => {});
+  it("should load Google Tag Manager library", () => {
+    cy.find(
+      `script[src="https://www.googletagmanager.com/gtm.js?id=UA-XXXXXX-X"]`
+    );
+  });
 
-  it("should sent a first pageview", () => {});
+  it("should have sent the first pageview", () => {
+    cy.window().its("dataLayer").its(0).should("deep.equal", pageviewHome);
+  });
 
   it("should sent a pageview if the page changes", () => {
-    cy.get("button#change-link");
+    cy.get("button#change-link").click();
+    cy.window().its("dataLayer").its(1).should("deep.equal", pageviewSomePost);
+  });
+
+  it("should sent pageviews when going back or forward", () => {
+    cy.go("back");
+    cy.window().its("dataLayer").its(2).should("deep.equal", pageviewHome);
+
+    cy.go("forward");
+    cy.window().its("dataLayer").its(3).should("deep.equal", pageviewSomePost);
   });
 
   it("should send events", () => {
-    cy.get("button#send-event");
+    cy.get("button#send-event").click();
+    cy.get("button#change-link").click();
+    cy.window().its("dataLayer").its(4).should("deep.equal", someEvent);
   });
 });
