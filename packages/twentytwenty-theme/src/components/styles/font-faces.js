f5f37ea79f78ec8fc3a06e51c29ea353e24a9141
Luis Herranz <luisherranz@gmail.com>
2020-04-28 16:22:51 +0200

    TwentyTwenty: Refactor font-faces

--- packages/twentytwenty-theme/src/components/styles/font-faces.js
+++ packages/twentytwenty-theme/src/components/styles/font-faces.js
@@ -10,18 +10,14 @@ import InterMediumLatin from "../../fonts/inter/Inter-Medium-LATIN.woff2";
 import InterBoldLatin from "../../fonts/inter/Inter-Bold-LATIN.woff2";
 import InterSemiBoldLatin from "../../fonts/inter/Inter-SemiBold-LATIN.woff2";
 
+const fonts = {
+  "us-ascii": [InterMediumUS, InterSemiBoldUS, InterBoldUS],
+  latin: [InterMediumLatin, InterSemiBoldLatin, InterBoldLatin],
+  all: [InterMedium, InterSemiBold, InterBold],
+};
+
 const FontFace = ({ state }) => {
-  let fonts = null;
-  switch (state.theme.fontSets) {
-    case "us-ascii":
-      fonts = [InterMediumUS, InterSemiBoldUS, InterBoldUS];
-      break;
-    case "latin":
-      fonts = [InterMediumLatin, InterSemiBoldLatin, InterBoldLatin];
-      break;
-    default:
-      fonts = [InterMedium, InterSemiBold, InterBold];
-  }
+  const font = fonts[state.theme.fontSets] || fonts["all"];
 
   return (
     <Global
@@ -31,7 +27,7 @@ const FontFace = ({ state }) => {
           font-style: normal;
           font-weight: 500;
           font-display: "swap";
-          src: url(${fonts[0]}) format("woff2");
+          src: url(${font[0]}) format("woff2");
         }
 
         @font-face {
@@ -39,7 +35,7 @@ const FontFace = ({ state }) => {
           font-style: normal;
           font-weight: 600;
           font-display: "swap";
-          src: url(${fonts[1]}) format("woff2");
+          src: url(${font[1]}) format("woff2");
         }
 
         @font-face {
@@ -47,7 +43,7 @@ const FontFace = ({ state }) => {
           font-style: normal;
           font-weight: 700;
           font-display: "swap";
-          src: url(${fonts[2]}) format("woff2");
+          src: url(${font[2]}) format("woff2");
         }
       `}
     />
