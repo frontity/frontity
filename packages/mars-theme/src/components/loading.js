4884aa1b1de341187ef9b7f3ced04dc11ac9b897
w3bdesign <hijack_xxx@yahoo.com>
2020-05-02 23:18:44 +0200

    Implement changes according to luisherranz
    Also deleted Spinner.js

--- packages/mars-theme/src/components/loading.js
+++ packages/mars-theme/src/components/loading.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { styled } from "frontity";
-import Spinner from "./spinner";
+import { styled, keyframes, css } from "frontity";
+
+const scale = keyframes`
+  0% {transform: scaley(1.0)}
+  50% {transform: scaley(0.4)}
+  100% {transform: scaley(1.0)}
+`;
 
 const Loading = () => (
   <Container>
-    <Spinner/>
+    <div>
+      <div css={bar(1)} />
+      <div css={bar(2)} />
+      <div css={bar(3)} />
+      <div css={bar(4)} />
+      <div css={bar(5)} />
+    </div>
   </Container>
 );
 
 export default Loading;
+
+const bar = index => css`
+  background-color: rgba(12, 17, 43, 0.3);
+  width: 4px;
+  height: 24px;
+  margin: 3px;
+  border-radius: 0;
+  display: inline-block;
+  animation: ${scale} 1s ${index * 0.1}s infinite
+    cubic-bezier(0.2, 0.68, 0.18, 1.08);
+  animation-fill-mode: both;
+`;
 
 const Container = styled.div`
   width: 800px;
