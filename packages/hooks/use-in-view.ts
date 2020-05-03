28943f63288530ce1604ed682e4c5783fcb145b2
Eduardo Campaña <asrielo@gmail.com>
2020-05-03 22:27:47 +0900

    reimplement use-in-view as a wrapper of react-intersection-observer

--- packages/hooks/use-in-view.ts
+++ packages/hooks/use-in-view.ts
@@ -1,88 +1,17 @@
-import { useState, useEffect, useRef, Dispatch, SetStateAction } from "react";
-import { warn } from "frontity";
+import { useInView, IntersectionOptions } from "react-intersection-observer";
 
-interface IntersectionObserverCallback {
-  (entries: IntersectionObserverEntry[]);
-}
-interface IntersectionObserverCallbackCreator {
-  (options: { onlyOnce: boolean }): IntersectionObserverCallback;
+interface UseInViewResponse {
+  ref: (node?: Element | null) => void;
+  inView: boolean;
+  supported: boolean;
 }
 
-interface Options {
-  rootMargin?: string;
-  onlyOnce?: boolean;
-}
+export default (options?: IntersectionOptions): UseInViewResponse => {
+  if (typeof window.IntersectionObserver === "undefined") {
+    return { ref: undefined, inView: true, supported: false };
+  }
 
-interface UseInView {
-  (options?: Options): [boolean, React.MutableRefObject<undefined>];
-}
+  const [ref, inView] = useInView(options);
 
-// This is an array with all the `setIntersected` functions
-// created by `useInView`, so we can find the corresponding
-// one to each entry on the IntersectionObserver callback.
-const setFunctions: [
-  Dispatch<SetStateAction<boolean>>,
-  React.MutableRefObject<undefined>
-][] = [];
-
-// This observer will be used during the whole life of the process.
-let observer: IntersectionObserver;
-
-// This callback can be called with more than one entry,
-// so we need to filter them and call the corresponding
-// `setIntersected` function for each changed entry.
-const createCallback: IntersectionObserverCallbackCreator = (options) => {
-  return (entries) => {
-    entries.forEach((entry) => {
-      const setFunction = setFunctions.find(
-        (set) => set[1].current === entry.target
-      );
-
-      // Check if we found the ref
-      if (setFunction) {
-        // This is the `setIntersected` function.
-        setFunction[0](entry.isIntersecting);
-      }
-
-      if (entry.isIntersecting && options.onlyOnce) {
-        observer.unobserve(entry.target);
-        setFunctions.splice(setFunctions.indexOf(setFunction), 1);
-      }
-    });
-  };
+  return { ref, inView, supported: true };
 };
-
-const useInView: UseInView = ({ rootMargin, onlyOnce } = {}) => {
-  const [isIntersecting, setIntersecting] = useState(false);
-  const ref = useRef();
-
-  useEffect(() => {
-    warn(
-      "The @frontity/hooks package is deprecated and will be removed in the future version of frontity.\nThe Frontity team recommends that you use https://github.com/thebuilder/react-intersection-observer instead."
-    );
-  }, []);
-
-  if (!observer)
-    observer = new IntersectionObserver(createCallback({ onlyOnce }), {
-      rootMargin,
-    });
-
-  useEffect(() => {
-    if (ref.current) {
-      setFunctions.push([setIntersecting, ref]);
-      observer.observe(ref.current);
-    }
-
-    return () => {
-      observer.unobserve(ref.current);
-      setFunctions.splice(
-        setFunctions.findIndex((set) => set[1].current === ref.current),
-        1
-      );
-    };
-  }, []);
-
-  return [isIntersecting, ref];
-};
-
-export default useInView;
