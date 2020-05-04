d2da4c332caa0f96ce19b6ff76104a1dff7544c3
Luis Herranz <luisherranz@gmail.com>
2020-05-04 15:00:09 +0200

    Connect: Use RES view inside connect()

--- packages/connect/src/connect.js
+++ packages/connect/src/connect.js
@@ -1,165 +1,45 @@
-import {
-  Component,
-  createContext,
-  createElement,
-  memo,
-  useMemo,
-  useState,
-  useEffect,
-  useContext,
-  useRef,
-} from "react";
-import { observe, unobserve, raw, isObservable } from ".";
+import { createContext, useContext, createElement } from "react";
+import { view } from "@risingstack/react-easy-state";
 
-const COMPONENT = Symbol("owner component");
-
+/**
+ * A React context that stores the store of Frontity Connect.
+ */
 const context = createContext();
 
+/**
+ * The Provider of the store context, to be used in the root of the React
+ * application.
+ */
 export const Provider = context.Provider;
 
-const hasHooks = typeof useState === "function";
+/**
+ * Connects a React component with the Frontity connect store.
+ *
+ * @param Component The React component to be connected
+ * @returns The same Component, but with the store included in the props.
+ */
+export const connect = (Component) => {
+  const isStatelessComp = !(
+    Component.prototype && Component.prototype.isReactComponent
+  );
 
-const mapStateToStores = (state) => {
-  // find store properties and map them to their none observable raw value
-  // to do not trigger none static this.setState calls
-  // from the static getDerivedStateFromProps lifecycle method
-  const component = state[COMPONENT];
-  return Object.keys(component)
-    .map((key) => component[key])
-    .filter(isObservable)
-    .map(raw);
-};
-
-export function connect(Comp) {
-  const isStatelessComp = !(Comp.prototype && Comp.prototype.isReactComponent);
-
-  let ReactiveComp;
-
-  if (isStatelessComp && hasHooks) {
-    // use a hook based reactive wrapper when we can
-    ReactiveComp = memo((props) => {
-      // set a flag to know when the component is unmounted.
-      const _isMounted = useRef(true);
-
-      // use a dummy setState to update the component
-      const [, setState] = useState();
-
-      // get frontity from the context;
-      const frontity = useContext(context);
-
-      // create a memoized reactive wrapper of the original component (render)
-      // at the very first run of the component function
-      const render = useMemo(
-        () =>
-          observe(Comp, {
-            scheduler: () => _isMounted.current && setState({}),
-            lazy: true,
-          }),
-        []
-      );
-
-      // cleanup the reactive connections after the very last render of the component
-      useEffect(() => {
-        return () => {
-          _isMounted.current = false;
-          unobserve(render);
-        };
-      }, []);
-
-      // run the reactive render instead of the original one
-      return render({ ...props, ...frontity });
+  if (isStatelessComp) {
+    return view((props) => {
+      const store = useContext(context);
+      return Component({ ...props, ...store });
     });
   } else {
-    const BaseComp = isStatelessComp ? Component : Comp;
-    // a HOC which overwrites render, shouldComponentUpdate and componentWillUnmount
-    // it decides when to run the new reactive methods and when to proxy to the original methods
-    class ReactiveClassComp extends BaseComp {
-      constructor(props, context) {
-        super(props);
-
-        this.store = context;
-
-        this.state = this.state || {};
-        this.state[COMPONENT] = this;
-
-        this._isMounted = true;
-
-        // create a reactive render for the component
-        this.render = observe(this.render, {
-          scheduler: () => this._isMounted && this.setState({}),
-          lazy: true,
-        });
-      }
+    class ConnectedComponent extends Component {
+      static contextType = context;
 
       render() {
-        return isStatelessComp
-          ? Comp({ ...this.props, ...this.store }, this.context)
-          : createElement(BaseComp, {
-              ...this.props,
-              ...this.store,
-            });
-      }
-
-      // react should trigger updates on prop changes, while Connect handles store changes
-      shouldComponentUpdate(nextProps, nextState) {
-        const { props, state } = this;
-
-        // respect the case when the user defines a shouldComponentUpdate
-        if (super.shouldComponentUpdate) {
-          return super.shouldComponentUpdate(nextProps, nextState);
-        }
-
-        // return true if it is a reactive render or state changes
-        if (state !== nextState) {
-          return true;
-        }
-
-        // the component should update if any of its props shallowly changed value
-        const keys = Object.keys(props);
-        const nextKeys = Object.keys(nextProps);
-        return (
-          nextKeys.length !== keys.length ||
-          nextKeys.some((key) => props[key] !== nextProps[key])
-        );
-      }
-
-      // add a custom deriveStoresFromProps lifecyle method
-      static getDerivedStateFromProps(props, state) {
-        if (super.deriveStoresFromProps) {
-          // inject all local stores and let the user mutate them directly
-          const stores = mapStateToStores(state);
-          super.deriveStoresFromProps(props, ...stores);
-        }
-        // respect user defined getDerivedStateFromProps
-        if (super.getDerivedStateFromProps) {
-          return super.getDerivedStateFromProps(props, state);
-        }
-        return null;
-      }
-
-      componentWillUnmount() {
-        // call user defined componentWillUnmount
-        if (super.componentWillUnmount) {
-          super.componentWillUnmount();
-        }
-        // clean up memory used
-        unobserve(this.render);
-        // don't use setState if it's unmounted.
-        this._isMounted = false;
+        return createElement(Component, {
+          ...this.props,
+          ...this.context,
+        });
       }
     }
-    ReactiveClassComp.contextType = context;
-    ReactiveComp = ReactiveClassComp;
-  }
 
-  ReactiveComp.displayName = Comp.displayName || Comp.name;
-  // static props are inherited by class components,
-  // but have to be copied for function components
-  if (isStatelessComp) {
-    for (let key of Object.keys(Comp)) {
-      ReactiveComp[key] = Comp[key];
-    }
+    return view(ConnectedComponent);
   }
-
-  return ReactiveComp;
-}
+};
