362681f39fd8a7c6523fa46806c44c39e037bb0d
Michal Czaplinski <mmczaplinski@gmail.com>
2020-05-11 20:29:15 -0500

    hooks: Add more tests cases and update the implementation of useFill()

--- packages/hooks/use-fills.test.tsx
+++ packages/hooks/use-fills.test.tsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { create } from "react-test-renderer";
-import { createStore } from "frontity";
+import { createStore, connect } from "frontity";
 import { Provider } from "@frontity/connect";
 import useFills from "./use-fills";
 
 let store;
 
-const FillComponent = () => <div>Hi, Im Phil, Im a Fill</div>;
+// spy on the console.warn calls
+const warn = jest.spyOn(global.console, "warn");
+
+const FillComponent = () => <div id="test-fill">Hi, Im Phil, Im a Fill</div>;
 
 beforeEach(() => {
+  warn.mockClear();
+
   store = createStore({
     state: {
       fills: {
         "test fill": {
           slot: "slot 1",
           library: "FillComponent",
-          priority: 10,
           props: {
             number: 42,
           },
@@ -31,18 +35,118 @@ beforeEach(() => {
   });
 });
 
-describe("test", () => {
-  it("should work", () => {
-    const Comp = () => {
-      const fills = useFills("test fill");
+describe("useFills", () => {
+  it("Should work in the most basic case", () => {
+    const Comp = connect(() => {
+      const fills = useFills("slot 1");
 
-      return fills.map(({ Fill, props }) => <Fill key={props.id} {...props} />);
-    };
+      return (
+        <>
+          {fills.map(({ Fill, props, key }) => (
+            <Fill key={key} {...props} />
+          ))}
+        </>
+      );
+    });
+
     const app = create(
       <Provider value={store}>
         <Comp />
       </Provider>
     );
-    expect(app).toMatchSnapshot();
+
+    expect(app.toJSON().props).toEqual({ id: "test-fill" });
+    expect(app.toJSON().children).toEqual(["Hi, Im Phil, Im a Fill"]);
+
+    expect(app.toJSON()).toMatchSnapshot();
+  });
+
+  it("Should work when the slot does not exist and should show a warning", () => {
+    const Comp = connect(() => {
+      const fills = useFills("slot that does not exist");
+
+      expect(fills).toEqual([]);
+
+      return (
+        <>
+          {fills.map(({ Fill, props, key }) => (
+            <Fill key={key} {...props} />
+          ))}
+        </>
+      );
+    });
+
+    const app = create(
+      <Provider value={store}>
+        <Comp />
+      </Provider>
+    );
+
+    expect(warn.mock.calls[0][0]).toMatch(
+      "Could not find any slot with name slot that does not exist. Is this what you want?"
+    );
+
+    expect(app.toJSON()).toEqual(null);
+    expect(app.toJSON()).toMatchSnapshot();
+  });
+
+  it("Should remind to specify the slot name if called without arguments", () => {
+    const warn = jest.spyOn(global.console, "warn");
+
+    const Comp = connect(() => {
+      // This is just to trick typescript to allow us to call
+      // useFills without any arguments
+      const useFills2: any = useFills;
+      const fills = useFills2();
+
+      expect(fills).toEqual([]);
+
+      return (
+        <>
+          {fills.map(({ Fill, props, key }) => (
+            <Fill key={key} {...props} />
+          ))}
+        </>
+      );
+    });
+
+    const app = create(
+      <Provider value={store}>
+        <Comp />
+      </Provider>
+    );
+
+    expect(warn.mock.calls[0][0]).toMatch(
+      "You should pass the name of the slot that you would like to fill!"
+    );
+
+    expect(app.toJSON()).toEqual(null);
+    expect(app.toJSON()).toMatchSnapshot();
+  });
+
+  it("Should work when the library does not exist", () => {
+    delete store.state.fills["test fill"].library;
+
+    const Comp = connect(() => {
+      const fills = useFills("slot 1");
+      expect(fills).toEqual([]);
+
+      return (
+        <>
+          {fills.map(({ Fill, props, key }) => (
+            <Fill key={key} {...props} />
+          ))}
+        </>
+      );
+    });
+
+    const app = create(
+      <Provider value={store}>
+        <Comp />
+      </Provider>
+    );
+
+    expect(app.toJSON()).toEqual(null);
+    expect(app.toJSON()).toMatchSnapshot();
   });
 });
--- packages/hooks/use-fills.tsx
+++ packages/hooks/use-fills.tsx
@@ -1,39 +1,71 @@
-import { useConnect } from "frontity";
+import { useConnect, warn } from "frontity";
 
 interface Fills {
-  state: {
-    fills: {
-      [key: string]: {
-        slot: string;
-        library: string;
-        priority: number;
-        props: {
-          [key: string]: any;
-        };
-      };
+  [key: string]: {
+    slot: string;
+    library: string;
+    priority?: number;
+    props?: {
+      [key: string]: any;
     };
-  };
-  libraries: {
-    [key: string]: any;
   };
 }
 
-const useFills = (name) => {
-  const { state, libraries } = useConnect<Fills>();
+interface FillsPackage {
+  state: {
+    fills: Fills;
+  };
+  libraries: {
+    fills: {
+      [key: string]: React.ComponentType;
+    };
+  };
+}
 
-  Object.values(state.fills)
-    // Match only the fills for this name.
-    .find((fill) => fill.name === name)
-    // Sort fills by priority.
-    .sort((a, b) => a.priority > b.priority)
-    // Add real component to the array.
-    .map((fill) => {
-      const Fill = libraries.fills[fill.library];
-      return {
-        ...fill,
-        Fill,
-      };
-    });
+const useFills = (name: string) => {
+  const { state, libraries } = useConnect<FillsPackage>();
+
+  if (!name) {
+    warn("You should pass the name of the slot that you would like to fill!");
+    return [];
+  }
+
+  if (!Object.values(state.fills).find((slot) => slot.slot === name)) {
+    warn(`Could not find any slot with name ${name}. Is this what you want?`);
+    return [];
+  }
+
+  return (
+    Object.entries(state.fills)
+
+      // Nest the `key` of the fill
+      .map(([key, value]) => ({ key, ...value }))
+
+      // Match only the fills for this name.
+      .filter(({ slot }) => slot === name)
+
+      // Sort by priority
+      .sort((a, b) => a.priority - b.priority)
+
+      // Add real component to the array.
+      .map((fill) => {
+        const { fills } = libraries;
+        const { library } = fill;
+
+        // If we cannot find the fill component in `libraries` OR
+        // if we cannot find the reference to the component in `state.fills.library` return a "placeholder". This is more clear than .reduce()
+        if (!fills || !library) return "not found";
+
+        const Fill = libraries.fills[fill?.library];
+
+        return {
+          ...fill,
+          Fill,
+        };
+      })
+      // Remove the "placeholders" from the array
+      .filter((item) => item !== "not found")
+  );
 };
 
 export default useFills;
