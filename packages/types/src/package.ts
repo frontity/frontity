538eb6e4cb173fc6661dbe34ce726b6120f79b56
Luis Herranz <luisherranz@Luiss-MacBook-Pro.local>
2020-05-23 18:44:07 +0200

    Remove old fills type

--- packages/types/src/package.ts
+++ packages/types/src/package.ts
@@ -3,9 +3,6 @@ export interface Package {
   roots?: {
     [namespace: string]: React.ReactType;
   };
-  fills?: {
-    [namespace: string]: React.ReactType;
-  };
   state?: {
     frontity?: {
       // Automatically populated:
