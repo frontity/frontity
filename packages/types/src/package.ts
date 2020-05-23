89ea7d6761ef7847435571b8da8bce6b1fa7fae3
Luis Herranz <luisherranz@Luiss-MacBook-Pro.local>
2020-05-23 19:05:02 +0200

    Add TSDocs

--- packages/types/src/package.ts
+++ packages/types/src/package.ts
@@ -1,3 +1,32 @@
+/**
+ * The configuration object for a Fill.
+ */
+export interface Fill {
+  /**
+   * The name of the Slot that this Fill wants to be injected in.
+   */
+  slot: string;
+
+  /**
+   * The name of the React component that it should be stored in `libraries.fills`.
+   */
+  library: string;
+
+  /**
+   * The priority of this fill in relation to the other fills that also point to the same `slot`.
+   *
+   * @default 10
+   */
+  priority?: number;
+
+  /**
+   * Optional object with props that will be passed to the React component defined in `library`.
+   */
+  props?: {
+    [key: string]: any;
+  };
+}
+
 export interface Package {
   name?: string;
   roots?: {
