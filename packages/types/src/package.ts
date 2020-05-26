ab4b3da249d88b8ca319da73ab2b449400e3fea9
Luis Herranz <luisherranz@gmail.com>
2020-05-26 11:45:42 +0200

    Add TSDocs to Package types

--- packages/types/src/package.ts
+++ packages/types/src/package.ts
@@ -1,4 +1,200 @@
 /**
+ * Types of a Frontity package.
+ *
+ * The main purpose of this interface is to be extended by the specific types
+ * of a Frontity package.
+ */
+export interface Package {
+  /**
+   * The name of the Frontity package.
+   */
+  name?: string;
+
+  /**
+   * An object of React components exposed by this package. The keys are the
+   * namespaces.
+   */
+  roots?: {
+    [namespace: string]: React.ReactType;
+  };
+
+  /**
+   * The state exposed by this package.
+   */
+  state?: {
+    /**
+     * State exposed in the `frontity` namespace.
+     */
+    frontity?: {
+      /**
+       * The name of the site
+       *
+       * Populated automatically by Frontity.
+       */
+      name?: string;
+
+      /**
+       * The link that was requested in the server-side rendering.
+       *
+       * Populated automatically by Frontity.
+       */
+      initialLink?: string;
+
+      /**
+       * An array with the names of the Frontity packages that are loaded for
+       * this site.
+       *
+       * Populated automatically by Frontity.
+       */
+      packages?: string[];
+
+      /**
+       * Indicates if Frontity is being run in the client or in the server.
+       *
+       * Populated automatically by Frontity.
+       */
+      platform?: "client" | "server";
+
+      /**
+       * Indicates if Frontity is in server-side rendering or client-side
+       * rendering. The difference between this and `state.frontity.platform`
+       * is that `state.frontity.rendering` is still `"ssr"` in the first frame
+       * of the client. That is useful when working with components that are
+       * different on server-side rendering and client-side rendering, because
+       * it won't cause a mismatch warning in the React hydration.
+       *
+       * Populated automatically by Frontity.
+       */
+      rendering?: "ssr" | "csr";
+
+      /**
+       * The title of the site.
+       *
+       * Populated by the user in `frontity.settings.js`.
+       */
+      title?: string;
+
+      /**
+       * The description of the site.
+       *
+       * Populated by the user in `frontity.settings.js`.
+       */
+      description?: string;
+
+      /**
+       * The final url of the site.
+       *
+       * Populated by the user in `frontity.settings.js`.
+       */
+      url?: string;
+    };
+
+    /**
+     * The configuration objects for all the fills exposed by this package.
+     */
+    fills?: {
+      [namespace: string]: {
+        [name: string]: Fill;
+      };
+    };
+
+    /**
+     * The rest of the state exposed by this package.
+     *
+     * @todo Replace `any` with serializable objects and derived state.
+     */
+    [namespace: string]: {
+      [key: string]: any;
+    };
+  };
+
+  /**
+   * The actions exposed by this package.
+   */
+  actions?: {
+    [namespace: string]: Actions;
+  };
+
+  /**
+   * The libraries exposed by this package.
+   */
+  libraries?: {
+    /**
+     * The Fill compoments exposed by this package.
+     */
+    fills?: {
+      [namespace: string]: {
+        [name: string]: React.ReactType;
+      };
+    };
+
+    /**
+     * The rest of the libraries exposed by this package.
+     */
+    [namespace: string]: {
+      [library: string]: any;
+    };
+  };
+}
+
+export default Package;
+
+/**
+ * An individual Frontity action.
+ */
+interface Action {
+  /**
+   * Declaration of an action without additional params.
+   *
+   * @example
+   * ```typescript
+   * const someAction: ({ state, actions, libraries }) => {
+   *  // Action content.
+   * }
+   *
+   * // Calling the action:
+   * actions.myPackage.someAction();
+   * ```
+   *
+   * @param store An object containing `state`, `actions` and `libraries`.
+   *
+   * @returns void or Promise<void> if it is an async action.
+   */
+  (store: Pick<Package, "state" | "actions" | "libraries">): void | Promise<
+    void
+  >;
+
+  /**
+   * Declaration of an action with additional params.
+   *
+   * @example
+   * ```typescript
+   * const someAction: ({ state, actions, libraries }) => (...args) => {
+   *  // Action content.
+   * }
+   *
+   * // Calling the action:
+   * actions.myPackage.someAction("arg 1", "arg 2", ...);
+   * ```
+   *
+   * @param store - An object containing `state`, `actions` and `libraries`.
+   * @param args - Any number of arguments.
+   *
+   * @returns void or Promise<void> if it is an async action.
+   */
+  (store: Pick<Package, "state" | "actions" | "libraries">): (
+    ...args: any
+  ) => void | Promise<void>;
+}
+
+/**
+ * An object containing Frontity actions, or other objects containing actions.
+ */
+interface Actions {
+  [key: string]: Action | Actions;
+}
+
+/**
  * The configuration object for a Fill.
  */
 export interface Fill {
@@ -26,83 +222,3 @@ export interface Fill {
     [key: string]: any;
   };
 }
-
-export interface Package {
-  name?: string;
-  roots?: {
-    [namespace: string]: React.ReactType;
-  };
-  state?: {
-    frontity?: {
-      // Automatically populated:
-      name?: string;
-      mode?: string;
-      initialLink?: string;
-      packages?: string[];
-      platform?: "client" | "server";
-      rendering?: "ssr" | "csr";
-      // Populated by the user:
-      title?: string;
-      description?: string;
-      url?: string;
-    };
-    fills?: { [key: string]: Fill };
-    [namespace: string]: {
-      [key: string]: any;
-    };
-  };
-  actions?: {
-    [namespace: string]: {
-      [action: string]:
-        | (({
-            state,
-            actions,
-            libraries,
-          }: {
-            state: {
-              [namespace: string]: {
-                [state: string]: any;
-              };
-            };
-            actions: {
-              [key: string]: {
-                [action: string]: any;
-              };
-            };
-            libraries: {
-              [key: string]: any;
-            };
-          }) => void)
-        | (({
-            state,
-            actions,
-            libraries,
-          }: {
-            state: {
-              [namespace: string]: {
-                [state: string]: any;
-              };
-            };
-            fills: { [key: string]: Fill };
-            actions: {
-              [key: string]: {
-                [action: string]: any;
-              };
-            };
-            libraries: {
-              [key: string]: any;
-            };
-          }) => (input: any) => void);
-    };
-  };
-  libraries?: {
-    [namespace: string]: {
-      fills?: {
-        [namespace: string]: React.ReactType;
-      };
-      [library: string]: any;
-    };
-  };
-}
-
-export default Package;
