af4d9a5eab5973ba1d942c5e0b1e45b9b5d1c798
Michal Czaplinski <mmczaplinski@gmail.com>
2020-05-26 19:41:50 -0500

    e2e: robots.txt - Add test case where robots file exists

--- e2e/integration/robots.spec.js
+++ e2e/integration/robots.spec.js
@@ -1,7 +1,28 @@
 import expect from "expect";
 
+// Remove the file before each run to ensure that we start with a clean project
+// We pass the -f option so that it doesn't throw the error in case the file don't exist
+beforeEach(() => {
+  cy.exec("rm -f project/robots.txt");
+});
+
+// Also clean up after each test run so that we don't pollute the repo
+afterEach(() => {
+  cy.exec("rm -f project/robots.txt");
+});
+
 describe("Robots", () => {
-  it("should respect the robots.txt file", () => {
+  it("should respond with a default robots.txt", () => {
+    cy.request("http://localhost:3001/robots.txt").as("robots");
+
+    cy.get("@robots").should((response) => {
+      expect(response.body).toContain("User-agent: *\nAllow: /");
+    });
+  });
+
+  it("should respect the robots.txt file if present", () => {
+    cy.writeFile("project/robots.txt", "User-agent: google\nAllow: /");
+
     cy.request("http://localhost:3001/robots.txt").as("robots");
 
     cy.get("@robots").should((response) => {
