c94db01965cdc25ec164445497ff5f2bcf153d49
Luis Herranz <luisherranz@gmail.com>
2020-05-27 10:56:09 +0200

    Add e2e tests for useFills

--- e2e/packages/slot-and-fill/src/index.tsx
+++ e2e/packages/slot-and-fill/src/index.tsx
@@ -1,31 +1,83 @@
-import React from "react";
-import { useFills } from "frontity";
+import React, { FC } from "react";
+import { connect, useConnect, useFills } from "frontity";
 import SlotAndFill from "../types";
 
-const Root: React.FC = () => {
+const Root: FC = connect(() => {
+  const { actions } = useConnect<SlotAndFill>();
   const fills1 = useFills("slot 1");
   const fills2 = useFills("slot 2");
   return (
     <>
-      {fills1.map(({ Fill, key, props }) => (
-        <Fill key={key} {...props} />
-      ))}
+      <div id="useFills-slot1">
+        {fills1.length > 0 ? (
+          fills1.map(({ Fill, key, props }) => <Fill key={key} {...props} />)
+        ) : (
+          <div id="useFills-slot1-empty">useFills slot 1 empty</div>
+        )}
+      </div>
       <hr />
-      {fills2.map(({ Fill, key, props }) => (
-        <Fill key={key} {...props} />
-      ))}
+      <div id="useFills-slot2">
+        {fills2.length > 0 ? (
+          fills2.map(({ Fill, key, props }) => <Fill key={key} {...props} />)
+        ) : (
+          <div id="useFills-slot2-empty">useFills slot 2 empty</div>
+        )}
+      </div>
+      <hr />
+      <button id="addFill1" onClick={actions.slotAndFill.addFill1}>
+        Add Fill1
+      </button>
+      <button id="addFill2" onClick={actions.slotAndFill.addFill2}>
+        Add Fill2
+      </button>
     </>
   );
-};
+});
+
+const Fill1: FC<{ text: string }> = ({ text }) => <div id="Fill1">{text}</div>;
+const Fill2: FC<{ text: string }> = ({ text }) => <div id="Fill2">{text}</div>;
 
 const slotAndFill: SlotAndFill = {
   name: "slot-and-fill",
-  state: {},
-  actions: {},
+  state: {
+    fills: {
+      slotAndFill: {},
+    },
+  },
+  actions: {
+    slotAndFill: {
+      addFill1: ({ state }) => {
+        state.fills.slotAndFill.fill1 = {
+          slot: "slot 1",
+          library: "slotAndFill.Fill1",
+          props: {
+            text: "I am Fill1",
+          },
+        };
+      },
+      addFill2: ({ state }) => {
+        state.fills.slotAndFill.fill2 = {
+          slot: "slot 1",
+          library: "slotAndFill.Fill2",
+          props: {
+            text: "I am Fill2",
+          },
+          priority: 9,
+        };
+      },
+    },
+  },
   roots: {
     slotAndFill: Root,
   },
-  libraries: {},
+  libraries: {
+    fills: {
+      slotAndFill: {
+        Fill1,
+        Fill2,
+      },
+    },
+  },
 };
 
 export default slotAndFill;
