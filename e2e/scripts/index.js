c767e02d467e02cf11fb132ff0777a4be8d13c26
Michal Czaplinski <mmczaplinski@gmail.com>
2020-05-28 17:26:57 -0500

    remove wp core install

--- e2e/scripts/index.js
+++ e2e/scripts/index.js
@@ -2,36 +2,38 @@ const { spawn, exec } = require("child-process-promise");
 
 (async () => {
   try {
-    await spawn("docker-compose", ["up", "-d"]);
+    await spawn("docker-compose", ["up", "-d"], {
+      stdio: "inherit",
+    });
 
     // Wait until server is ready.
-    await spawn("npx", ["wait-on", "http-get://localhost:8080"]);
-
-    // Change permissions to the wp-content folder because volumes have root
-    // permissions by default.
-    await spawn("docker-compose", [
-      "run",
-      "wp",
-      "/bin/bash",
-      "-c",
-      "chown -R www-data:www-data /var/www/html/wp-content/",
-    ]);
-
-    await spawn("docker-compose", ["run", "wp", "core", "install"]);
-
-    // Install plugins.
-    await spawn("docker-compose", [
-      "run",
-      "--rm",
-      "wpcli",
-      "plugin",
-      "install",
-      "wordpress-seo --version=12.6",
-      "all-in-one-seo-pack",
-    ]);
+    await spawn("npx", ["wait-on", "http-get://localhost:8080"], {
+      stdio: "inherit",
+    });
 
     const { stdout } = await exec("curl localhost:8080");
     console.log(stdout);
+
+    // await spawn("docker-compose", ["run", "wp", "core", "install"], {
+    //   stdio: "inherit",
+    // });
+
+    // Install plugins.
+    await spawn(
+      "docker-compose",
+      [
+        "run",
+        "--rm",
+        "wpcli",
+        "plugin",
+        "install",
+        "wordpress-seo --version=12.6",
+        "all-in-one-seo-pack",
+      ],
+      {
+        stdio: "inherit",
+      }
+    );
   } catch (err) {
     console.error(err);
 
