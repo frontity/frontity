dc26b8bf82ee023b0d3dcef1b434344811dac01c
Michal Czaplinski <mmczaplinski@gmail.com>
2020-05-28 16:35:38 -0500

    move the docker-compose stuff into the script

--- e2e/scripts/index.js
+++ e2e/scripts/index.js
@@ -2,10 +2,10 @@ const { spawn } = require("child-process-promise");
 
 (async () => {
   try {
+    await spawn("docker-compose", ["up", "-d"]);
+
     // Wait until server is ready.
-    await spawn("npx", ["wait-on", "http-get://localhost:8080"], {
-      stdio: "inherit",
-    });
+    await spawn("npx", ["wait-on", "http-get://localhost:8080"]);
 
     // Change permissions to the wp-content folder because volumes have root
     // permissions by default.
@@ -18,22 +18,21 @@ const { spawn } = require("child-process-promise");
     ]);
 
     // Install plugins.
-    await spawn(
-      "docker-compose",
-      [
-        "run",
-        "--user",
-        "33:33",
-        "--rm",
-        "wpcli",
-        "plugin",
-        "install",
-        "wordpress-seo --version=12.6",
-        "all-in-one-seo-pack",
-      ],
-      { stdio: "inherit" }
-    );
+    await spawn("docker-compose", [
+      "run",
+      "--user",
+      "33:33",
+      "--rm",
+      "wpcli",
+      "plugin",
+      "install",
+      "wordpress-seo --version=12.6",
+      "all-in-one-seo-pack",
+    ]);
   } catch (err) {
     console.error(err);
+
+    // We need to return the exit code so that the github action returns a fail
+    process.exit(1);
   }
 })();
