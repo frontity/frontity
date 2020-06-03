fe53436812fc28a26dbafac0a1634db7d0faf908
Michal Czaplinski <mmczaplinski@gmail.com>
2020-06-03 12:49:55 -0500

    fix the start script

--- scripts/start-e2e-docker.js
+++ scripts/start-e2e-docker.js
@@ -1,4 +1,5 @@
 const { spawn } = require("child-process-promise");
+var waitOn = require("wait-on");
 
 (async () => {
   try {
@@ -6,29 +7,7 @@ const { spawn } = require("child-process-promise");
       stdio: "inherit",
     });
 
-    // Wait until server is ready.
-    await spawn("npx", ["wait-on", "http-get://localhost:8080"], {
-      stdio: "inherit",
-    });
-
-    // await spawn(
-    //   "docker-compose",
-    //   [
-    //     "exec",
-    //     "wpcli",
-    //     "wp", // this is the reference to the volume (we have wp, wpcli, and msql)
-    //     "core",
-    //     "install",
-    //     "--url=example.com",
-    //     "--title=Example",
-    //     "--admin_user=admin",
-    //     "--admin_password=password",
-    //     "--admin_email=info@example.com",
-    //   ],
-    //   {
-    //     stdio: "inherit",
-    //   }
-    // );
+    await waitOn({ resources: ["http-get://localhost:8080"], interval: 1000 });
 
     // Change permissions to the wp-content folder so that we can install plugins and load content
     await spawn(
@@ -36,28 +15,44 @@ const { spawn } = require("child-process-promise");
       [
         "exec",
         "-T", // Disable pseudo-tty allocation https://stackoverflow.com/a/57565119/2638310
-        "frontity_wp", // this is the reference to the volume (we have wp, wpcli, and msql)
+        "wp", // this is the reference to the volume (we have wp, wpcli, and msql)
         "/bin/bash",
         "-c",
         "chown -R www-data:www-data /var/www/html/wp-content/plugins/ && \
          chown -R www-data:www-data /var/www/html/wp-content/uploads && \
          chown -R www-data:www-data /var/www/html/wp-content/ && \
-         chmod 775 /var/www/html/wp-content/plugins && \
-         chmod 775 /var/www/html/wp-content/uploads && \
-         chmod 775 /var/www/html/wp-content/",
+         chmod -R 775 /var/www/html/wp-content/",
       ],
       { stdio: "inherit" }
+    );
+
+    await spawn(
+      "docker-compose",
+      [
+        "run",
+        "wpcli", // this is the reference to the service (we have wp, wpcli, and msql)
+        "wp",
+        "core",
+        "install",
+        "--url=localhost:8080",
+        "--title=Example",
+        "--admin_user=admin",
+        "--admin_password=password",
+        "--admin_email=info@example.com",
+      ],
+      {
+        stdio: "inherit",
+      }
     );
 
     // Install plugins.
     await spawn(
       "docker-compose",
       [
-        "exec",
-        "-T",
+        "run", // needs to be "run" and not "exec"
         "--user", // https://hub.docker.com/_/wordpress/ See: "Running as arbitrary user"
         "33:33",
-        "frontity_wpcli", // this is the reference to the volume (we have wp, wpcli, and msql)
+        "wpcli", // this is the reference to the volume (we have wp, wpcli, and msql)
         "wp",
         "plugin",
         "install",
