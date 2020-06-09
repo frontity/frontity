c8c40f9d6d88a2e99c6b6b370da5c931b1f11c5a
David Arenas <darensr@gmail.com>
2020-06-10 01:51:07 +0200

    Add tsdocs for Google Tag Manager

--- packages/google-tag-manager/types.ts
+++ packages/google-tag-manager/types.ts
@@ -10,19 +10,79 @@ declare global {
   }
 }
 
+/**
+ * `@frontity/google-tag-manager` package.
+ */
 interface GoogleTagManager extends Analytics {
   roots: Analytics["roots"] & {
     googleTagManager: ReactType;
   };
   state: Analytics["state"] & {
+    /**
+     * State properties for the Google Tag Manager package.
+     */
     googleTagManager: {
+      /**
+       * Container ID associated to a Google Tag Manager account.
+       */
       containerId?: string;
+      /**
+       * List of container IDs associated to a Google Tag Manager account.
+       */
       containerIds?: string[];
     };
   };
   actions: Analytics["actions"] & {
+    /**
+     * Actions from the Google analytics package.
+     */
     googleTagManager: {
+      /**
+       * Send a pageview event to all container IDs defined in
+       * either `state.googleTagManager.containerId` or
+       * `state.googleTagManager.containerIds`.
+       *
+       * The pageview event is received by Google Tag Manager with the
+       * following format:
+       *
+       * ```ts
+       * {
+       *   event: "virtualPageview",
+       *   pageview: {
+       *     link: pageview.link,
+       *     title: pageview.title,
+       *   }
+       * }
+       * ```
+       *
+       * @remarks
+       * This action is called automatically if
+       * `state.analytics.pageviews.googleTagManager` is set to `true`.
+       *
+       * @param pageview - Object of type {@link Pageview}.
+       */
       pageview: Action<GoogleTagManager, Pageview>;
+      /**
+       * Send an event to all container IDs defined in
+       * either `state.googleTagManager.containerId` or
+       * `state.googleTagManager.containerIds`.
+       *
+       * The event is received by Google Tag Manager with the following format:
+       *
+       * ```ts
+       * {
+       *   event: event.name,
+       *   payload: event.payload
+       * }
+       * ```
+       *
+       * @remarks
+       * This action is called automatically when calling
+       * `actions.analytics.event()` and
+       * `state.analytics.pageviews.googleTagManager` is set to `true`.
+       *
+       * @param event - Object of type {@link Event}.
+       */
       event: Action<GoogleTagManager, Event>;
     };
   };
