f2bd015818153523ee33c0506ab4f20997f6d1cc
Michal Czaplinski <mmczaplinski@gmail.com>
2020-06-30 17:46:41 -0500

    link-component: Add the TSDoc to the mars-theme

--- packages/mars-theme/src/components/link.js
+++ packages/mars-theme/src/components/link.js
@@ -2,9 +2,22 @@ import React from "react";
 import { connect, useConnect } from "frontity";
 import Link from "@frontity/components/link";
 
+/**
+ * The Link component.
+ *
+ * @param chldren React children.
+ *
+ * @example
+ * ```js
+ *   <MarsLink link="/posts" />
+ * ```
+ *
+ * @returns React `<a/>` element.
+ */
 const MarsLink = ({ children, ...props }) => {
   const { state, actions } = useConnect();
 
+  //eslint-disable-next-line jsdoc/require-jsdoc
   const onClick = () => {
     if (state.theme.isMobileMenuOpen) {
       actions.theme.closeMobileMenu();
