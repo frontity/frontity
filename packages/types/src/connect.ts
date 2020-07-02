ee2bbeb1bb8d61efa7ff2412822676e8212d081c
Luis Herranz <luisherranz@gmail.com>
2020-07-02 14:21:29 +0200

    TSDocs: disable on connect types
    Because I want to refactor those types in the
    https://github.com/frontity/frontity/pull/415 PR.

--- packages/types/src/connect.ts
+++ packages/types/src/connect.ts
@@ -1,3 +1,12 @@
+/* eslint-disable jsdoc/require-jsdoc */
+
+/**
+ * TSDocs disabled by `@luisherranz` in this file because he needs to figure
+ * out first which types will remain when we merge this PR
+ * https://github.com/frontity/frontity/pull/415, taking into account that
+ * `react-easy-state` and `@nx-js/observer-util` already have types themselves.
+ */
+
 import { ComponentProps, FunctionComponent, ComponentClass } from "react";
 import Package from "./package";
 import { ResolveState, ResolveActions, FilterInjectedProps } from "./utils";
