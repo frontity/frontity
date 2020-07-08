916c94ca5020bafbf1324d2a04a73078caa1bc6b
Luis Herranz <luisherranz@gmail.com>
2020-07-08 10:43:36 +0200

    Fix eslint errors

--- packages/error/index.ts
+++ packages/error/index.ts
@@ -1,14 +1,31 @@
 const suffix = "\nVisit https://community.frontity.org for help! 🙂\n";
 
+/**
+ * The options for the {@link error} function.
+ */
 interface ErrorOptions {
+  /**
+   * Indicate if the function should throw or just log the error in the console
+   * using `console.error`.
+   *
+   * @example
+   * ```js
+   * error("Something wrong happaned.", { throw: false });
+   * ```
+   *
+   * @defaultValue true
+   */
   throw?: boolean;
 }
 
 /**
+ * Throws an error. In development, it adds a message that encourage users to
+ * visit the Frontity community if they need help.
  *
+ * @example error("Something wrong happened.")
  *
- * @param message - Describes the error.
- * @param options Options
+ * @param message - The message that describes the error.
+ * @param options - The options, described in {@link ErrorOptions}.
  */
 export const error = (message: string, options: ErrorOptions = {}): void => {
   const doThrow = typeof options.throw !== "undefined" ? options.throw : true;
@@ -26,6 +43,9 @@ export const error = (message: string, options: ErrorOptions = {}): void => {
  * visit the Frontity community if they need help. It's intended to be used
  * by Frontity packages.
  *
+ * @example warn("You should do/change something.")
+ *
+ * @param message - The message that describes the warning.
  */
 export const warn = (message: string) => {
   if (process.env.NODE_ENV !== "production") {
