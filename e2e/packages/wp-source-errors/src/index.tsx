d4c7a0bc5641d31eba5c032af9a7944988fa45e0
Michal Czaplinski <mmczaplinski@gmail.com>
2020-07-09 17:18:36 -0500

    e2e: wp-source-errors: Change routing in tests to use a statusCode as a query param

--- e2e/packages/wp-source-errors/src/index.tsx
+++ e2e/packages/wp-source-errors/src/index.tsx
@@ -21,9 +21,10 @@ const wpSourceErrors: WpSourceErrors = {
   actions: {
     wpSourceErrors: {
       init: ({ state, libraries }) => {
-        const { path } = libraries.source.parse(state.frontity.initialLink);
-        if (path !== "/") {
-          state.source.api = `https://httpstat.us/${path}?rest_route=/`;
+        const { query } = libraries.source.parse(state.frontity.initialLink);
+        console.log(query);
+        if (query.statusCode) {
+          state.source.api = `https://httpstat.us/${query.statusCode}?rest_route=/`;
         } else {
           state.source.api = "https://test.frontity.org/wp-json";
         }
