a16548cdfc915cd644ce0bb558a56b6ee30b751a
Michal Czaplinski <mmczaplinski@gmail.com>
2020-07-10 18:09:16 -0500

    e2e wp: refactor the setup

--- e2e-wp/instance-1/start-e2e-docker.js
+++ e2e-wp/instance-1/start-e2e-docker.js
@@ -1,70 +1,26 @@
 #!/usr/bin/env node
-const { spawn } = require("child-process-promise");
-var waitOn = require("wait-on");
+/* eslint-disable */
+const waitOn = require("wait-on");
+const execa = require("execa");
 
 (async () => {
   try {
-    await spawn("docker-compose", ["up", "-d"], {
-      stdio: "inherit",
+    // run docker
+    await execa("docker-compose", ["up", "-d"], { stdio: "inherit" });
+
+    await waitOn({
+      resources: ["http-get://localhost:8080"],
+      interval: 1000,
     });
 
-    await waitOn({ resources: ["http-get://localhost:8080"], interval: 1000 });
-
-    // Change permissions to the wp-content folder so that we can install plugins and load content
-    await spawn(
-      "docker-compose",
-      [
-        "exec",
-        "-T", // Disable pseudo-tty allocation https://stackoverflow.com/a/57565119/2638310
-        "wp", // this is the reference to the volume (we have wp, wpcli, and msql)
-        "/bin/bash",
-        "-c",
-        "chown -R www-data:www-data /var/www/html/wp-content/plugins/ && \
-         chown -R www-data:www-data /var/www/html/wp-content/uploads && \
-         chown -R www-data:www-data /var/www/html/wp-content/ && \
-         chmod -R 775 /var/www/html/wp-content/",
-      ],
-      { stdio: "inherit" }
-    );
-
-    await spawn(
-      "docker-compose",
-      [
-        "run",
-        "--user", // https://hub.docker.com/_/wordpress/ See: "Running as arbitrary user"
-        "33:33",
-        "wpcli", // this is the reference to the service (we have wp, wpcli, and msql)
-        "wp",
-        "core",
-        "install",
-        "--url=localhost:8080",
-        "--title=Example",
-        "--admin_user=admin",
-        "--admin_password=password",
-        "--admin_email=info@example.com",
-      ],
-      {
-        stdio: "inherit",
-      }
-    );
-
     // Install plugins.
-    await spawn(
-      "docker-compose",
-      [
-        "run", // needs to be "run" and not "exec"
-        "--user", // https://hub.docker.com/_/wordpress/ See: "Running as arbitrary user"
-        "33:33",
-        "wpcli", // this is the reference to the volume (we have wp, wpcli, and msql)
-        "wp",
-        "plugin",
-        "install",
-        "all-in-one-seo-pack",
-        "--activate",
-      ],
-      {
-        stdio: "inherit",
-      }
+    // https://github.com/docker-library/wordpress/issues/417#issuecomment-514360301
+    await execa.command(
+      "docker run -i --user 33 \
+      --volumes-from instance-1_wp_1 \
+      --network container:instance-1_wp_1 \
+      wordpress:cli wp plugin install all-in-one-seo-pack --activate",
+      { stdio: "inherit" }
     );
   } catch (err) {
     console.error(err);
