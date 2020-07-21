a68a314cd1ef8c9f13cee4ba30816662b144a4c2
David Arenas <darensr@gmail.com>
2020-07-21 14:41:29 +0200

    Use mockComment func instead of json files

--- packages/wp-comments/src/__tests__/commentsHandler.test.ts
+++ packages/wp-comments/src/__tests__/commentsHandler.test.ts
@@ -4,117 +4,133 @@ import clone from "clone-deep";
 import { mergeDeepRight } from "ramda";
 import wpComments from "../";
 import { Packages } from "../../types";
-// JSON mocks
-
-import comments from "./mocks/post-60-comments.json";
-import { mockResponse } from "./mocks/helpers";
+import { mockResponse, mockComment } from "./mocks/helpers";
 
 type Api = Packages["libraries"]["source"]["api"];
 
 let store: InitializedStore<Packages>;
 let api: jest.Mocked<Api>;
+
 beforeEach(() => {
   const packages: any = mergeDeepRight(clone(wpSource()), clone(wpComments));
   store = createStore<Packages>(packages);
   store.state.source.api = "https://test.frontity.org/wp-json";
   store.actions.source.init();
   api = store.libraries.source.api as jest.Mocked<Api>;
+  api.get = jest.fn();
 });
 
 describe("commentsHandler", () => {
-  test("fetch comments from given post and add them to the state", async () => {
-    // Mock Api responses
-    // We have to use this form instead of:
-    // .mockResolvedValueOnce(mockResponse([]))
-    // because the later always returns the same instance of Response.
-    // which results in error because response.json() can only be run once
-    api.get = jest.fn((_) => Promise.resolve(mockResponse(comments)));
+  test("should populate comments correctly", async () => {
+    api.get.mockResolvedValueOnce(
+      mockResponse([
+        mockComment({ post: 60, id: 2 }),
+        mockComment({ post: 60, id: 1 }),
+      ])
+    );
 
-    // Fetch entities
     await store.actions.source.fetch("comments/60");
+
     expect(api.get).toHaveBeenCalledTimes(1);
-    expect(api.get.mock.calls[0]).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "endpoint": "comments",
-          "params": Object {
-            "_embed": false,
-            "page": 1,
-            "per_page": 100,
-            "post": 60,
-          },
-        },
-      ]
-    `);
+    expect(api.get.mock.calls[0]).toMatchSnapshot();
+    expect(store.state.source.comment).toMatchSnapshot();
+    expect(store.state.source.get("comments/60")).toMatchSnapshot();
+  });
 
-    expect(store.state.source.comment).toMatchInlineSnapshot(`
-      Object {
-        "1": Object {
-          "_links": Object {},
-          "author": 1,
-          "author_avatar_urls": Object {},
-          "author_name": "Frontibotito",
-          "author_url": "http://test.frontity.org",
-          "content": Object {
-            "rendered": "<p>Test Comment 1</p>",
-          },
-          "date": "2020-07-20T11:00:00",
-          "date_gmt": "2019-07-20T09:00:00",
-          "id": 1,
-          "link": "/2016/the-beauties-of-gullfoss/comment-page-1/#comment-1",
-          "meta": Array [],
-          "parent": 0,
-          "post": 60,
-          "status": "approved",
-          "type": "comment",
-        },
-        "2": Object {
-          "_links": Object {},
-          "author": 1,
-          "author_avatar_urls": Object {},
-          "author_name": "Frontibotito",
-          "author_url": "http://test.frontity.org",
-          "content": Object {
-            "rendered": "<p>Test Comment 2</p>",
-          },
-          "date": "2020-07-20T12:00:00",
-          "date_gmt": "2019-07-20T10:00:00",
-          "id": 2,
-          "link": "/2016/the-beauties-of-gullfoss/comment-page-1/#comment-2",
-          "meta": Array [],
-          "parent": 0,
-          "post": 60,
-          "status": "approved",
-          "type": "comment",
-        },
-      }
-    `);
+  test("should work fine if there are no comments", async () => {
+    api.get.mockResolvedValueOnce(mockResponse([]));
 
-    expect(store.state.source.get("comments/60")).toMatchInlineSnapshot(`
-      Object {
-        "areComments": true,
-        "isFetching": false,
-        "isReady": true,
-        "items": Array [
-          Object {
-            "children": Array [],
-            "id": 2,
-            "type": "comment",
-          },
-          Object {
-            "children": Array [],
-            "id": 1,
-            "type": "comment",
-          },
-        ],
-        "link": "comments/60/",
-        "page": 1,
-        "postId": 60,
-        "query": Object {},
-        "route": "comments/60/",
-        "total": 2,
-        "totalPages": 1,
-      }
-    `);
+    await store.actions.source.fetch("comments/60");
+
+    expect(api.get).toHaveBeenCalledTimes(1);
+    expect(api.get.mock.calls[0]).toMatchSnapshot();
+    expect(store.state.source.comment).toMatchSnapshot();
+    expect(store.state.source.get("comments/60")).toMatchSnapshot();
+  });
+
+  test("should request more pages if there are more than 100 comments", async () => {
+    // We use these headers to pretend that the responses contain many more
+    // comments than they actually have.
+    const responseProps = {
+      headers: {
+        "X-WP-TotalPages": "3",
+        "X-WP-Total": "201",
+      },
+    };
+
+    api.get
+      .mockResolvedValueOnce(
+        mockResponse([mockComment({ post: 60, id: 201 })], responseProps)
+      )
+      .mockResolvedValueOnce(
+        mockResponse([mockComment({ post: 60, id: 101 })], responseProps)
+      )
+      .mockResolvedValueOnce(
+        mockResponse([mockComment({ post: 60, id: 1 })], responseProps)
+      );
+
+    await store.actions.source.fetch("comments/60");
+
+    expect(api.get).toHaveBeenCalledTimes(3);
+    expect(api.get.mock.calls[0]).toMatchSnapshot();
+    expect(store.state.source.comment).toMatchSnapshot();
+    expect(store.state.source.get("comments/60")).toMatchSnapshot();
+  });
+
+  test("should handle replies correctly", async () => {
+    api.get.mockResolvedValueOnce(
+      mockResponse([
+        mockComment({ post: 60, id: 3, parent: 2 }),
+        mockComment({ post: 60, id: 2 }),
+        mockComment({ post: 60, id: 1 }),
+      ])
+    );
+
+    await store.actions.source.fetch("comments/60");
+
+    expect(api.get).toHaveBeenCalledTimes(1);
+    expect(api.get.mock.calls[0]).toMatchSnapshot();
+    expect(store.state.source.comment).toMatchSnapshot();
+    expect(store.state.source.get("comments/60")).toMatchSnapshot();
+  });
+
+  test("should handle replies correctly when more than one page", async () => {
+    // We use these headers to pretend that the responses contain many more
+    // comments than they actually have.
+    const responseProps = {
+      headers: {
+        "X-WP-TotalPages": "2",
+        "X-WP-Total": "103",
+      },
+    };
+
+    api.get
+      .mockResolvedValueOnce(
+        mockResponse(
+          [
+            mockComment({ post: 60, id: 3, parent: 2 }),
+            mockComment({ post: 60, id: 2 }),
+            mockComment({ post: 60, id: 1 }),
+          ],
+          responseProps
+        )
+      )
+      .mockResolvedValueOnce(
+        mockResponse(
+          [
+            mockComment({ post: 60, id: 6 }),
+            mockComment({ post: 60, id: 5, parent: 4 }),
+            mockComment({ post: 60, id: 4, parent: 3 }),
+          ],
+          responseProps
+        )
+      );
+
+    await store.actions.source.fetch("comments/60");
+
+    expect(api.get).toHaveBeenCalledTimes(2);
+    expect(api.get.mock.calls[0]).toMatchSnapshot();
+    expect(store.state.source.comment).toMatchSnapshot();
+    expect(store.state.source.get("comments/60")).toMatchSnapshot();
   });
 });
--- packages/wp-comments/src/__tests__/mocks/helpers.ts
+++ packages/wp-comments/src/__tests__/mocks/helpers.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
 import { ResponseInit, Response as NodeResponse } from "node-fetch";
+import { mergeDeepRight } from "ramda";
+import { WpComment } from "../../../types";
 
 /**
  * Utility to mock responses from WP REST API.
@@ -27,4 +31,44 @@ export const fromEntries = (
   const result = {};
   for (const [key, value] of entries) result[key] = value;
   return result;
+};
+
+/**
+ * Function that returns a mocked comment.
+ *
+ * @example
+ * ```
+ * mockComment({ id: 5, parent: 4 });
+ * ```
+ *
+ * @param props - Any bunch of comment properties.
+ *
+ * @returns A comment entity with merged props.
+ */
+export const mockComment = (props: Partial<WpComment>): WpComment => {
+  const base: WpComment = {
+    id: 6,
+    post: 1,
+    parent: 0,
+    author: 1,
+    author_name: "Frontibotito",
+    author_url: "http://test.frontity.org",
+    date: "2020-07-20T12:00:00",
+    date_gmt: "2020-07-20T10:00:00",
+    content: { rendered: "" },
+    link: "",
+    status: "approved",
+    type: "comment",
+    author_avatar_urls: {},
+    meta: [],
+  };
+
+  // Create comment mergin base and props.
+  const comment = mergeDeepRight(base, props);
+
+  // Change some props based on the final props.
+  comment.link = `https://test.frontity.org/2020/post-${comment.post}/comment-page-1/#comment-${comment.id}`;
+  comment.content.rendered = `<p>Test Comment ${comment.id}</p>`;
+
+  return comment;
 };
