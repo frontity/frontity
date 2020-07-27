a5bf04ec3294cd91bed074c8cb69fe488c1cc2e0
Luis Herranz <luisherranz@gmail.com>
2020-07-27 17:00:59 +0200

    EntryPoints: Fix packages that start with a number
    Like `@123/package`.

--- packages/core/src/utils/get-variable.ts
+++ packages/core/src/utils/get-variable.ts
@@ -1,9 +1,18 @@
-// Remove some characters present in the npm package name to
-// turn it into a variable name.
+/**
+ * Create a valid variable name out of a package name and a mode.
+ *
+ * @param pkg - The package name, like for example `@frontity/mars-theme`.
+ * @param mode - The mode, like for example `default` or `amp`.
+ *
+ * @returns A valid variable name that can be used to create the entry point
+ * files.
+ */
 export default (pkg: string, mode: string) => {
   return (
+    // "_" +
     pkg
       .replace(/^@/, "")
+      .replace(/^(\d)/, "_$1")
       .replace(/-/g, "_")
       .replace(/\//g, "__")
       .replace(/\./g, "___") + `_${mode}`
