8e57aa80c2d1eea3091cf9204bcb8e0f7d5581d1
David Arenas <darensr@gmail.com>
2020-07-29 14:46:13 +0200

    GoogleAdManager: add stub for e2e tests spec

---
+++ e2e/integration/google-ad-manager.spec.js
@@ -0,0 +1,21 @@
+describe("Google Ad Manager", () => {
+  it("should load the GPT library", () => {});
+  it("should render GPT ad units from fills", () => {
+    // Go to home.
+    // Get add unit from header.
+    // Get ad unit from footer.
+    // Go to post.
+    // Get add unit from header.
+    // Get add unit from content.
+    // Get ad unit from footer.
+  });
+  it("should work using GPT component directly from libraries", () => {
+    // Go to page without slots.
+    // Check GPT fills are not there.
+    // Check GPT component is there.
+  });
+  it("should add the link to the container ID if data is passed", () => {
+    // Go to post 2.
+    // Check add unit of next post (data is passed).
+  });
+});
