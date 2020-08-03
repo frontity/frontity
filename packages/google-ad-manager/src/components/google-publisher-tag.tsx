be3a84b98eaf10637c0b89199ecd526d73e27789
David Arenas <darensr@gmail.com>
2020-08-03 14:27:04 +0200

    Do not use unit to generate id and get min size

--- packages/google-ad-manager/src/components/google-publisher-tag.tsx
+++ packages/google-ad-manager/src/components/google-publisher-tag.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import GoogleAdManager from "../../types";
+import { css } from "frontity";
+import { Connect } from "frontity/types";
+import GoogleAdManager, { GooglePublisherTagProps, Size } from "../../types";
 
 /**
  * Documented at {@link GoogleAdManager.libraries.googleAdManager.GooglePublisherTag}.
@@ -7,16 +9,10 @@ import GoogleAdManager from "../../types";
  * @param props - Documented at {@link GoogleAdManager.libraries.googleAdManager.GooglePublisherTag}.
  * @returns React element.
  */
-const GooglePublisherTag: GoogleAdManager["libraries"]["googleAdManager"]["GooglePublisherTag"] = ({
-  unit,
-  size,
-  id,
-  targeting = {},
-  data,
-}) => {
-  // Generate the div ID.
-  let divId = `${id}-${unit}`;
-
+const GooglePublisherTag: React.FC<Connect<
+  GoogleAdManager,
+  GooglePublisherTagProps
+>> = ({ unit, size, id, targeting = {}, data }) => {
   /**
    * Append link if data is specified.
    *
@@ -24,14 +20,16 @@ const GooglePublisherTag: GoogleAdManager["libraries"]["googleAdManager"]["Googl
    * different pages showed at the same time, like in an infinite-scroll
    * layout, without conflicts.
    */
-  if (data) divId = `${divId}-${data.link}`;
+  if (data) id = `${id}-${data.link.replace(/[^\w]/g, "-")}`;
 
   // Set up the ad unit once the component has been mounted.
   React.useEffect(() => {
+    window.googletag = window.googletag || { cmd: [] };
+
     window.googletag.cmd.push(function () {
       // Define the slot with the give properties.
       const slot = window.googletag
-        .defineSlot(unit, size, divId)
+        .defineSlot(unit, size, id)
         .addService(window.googletag.pubads());
 
       // Set targeting values if specified.
@@ -39,13 +37,36 @@ const GooglePublisherTag: GoogleAdManager["libraries"]["googleAdManager"]["Googl
         slot.setTargeting(key, value)
       );
 
+      // Enables all GPT services.
+      window.googletag.enableServices();
+
       // Show the ad unit.
-      window.googletag.display(divId);
+      window.googletag.display(id);
     });
   }, []);
 
+  /*
+   * Get `minWidth` and `minHeight` from `size` prop to render the container
+   * with minimum dimensions.
+   */
+  let minWidth: number, minHeight: number;
+  if (typeof size[0] === "number") {
+    [minWidth, minHeight] = size as Size;
+  } else {
+    minWidth = Math.min(...(size as Size[]).map(([width]) => width));
+    minHeight = Math.min(...(size as Size[]).map(([_, height]) => height));
+  }
+
   // Render the ad container.
-  return <div id={divId} />;
+  return (
+    <div
+      id={id}
+      css={css`
+        min-height: ${minWidth};
+        min-width: ${minHeight};
+      `}
+    />
+  );
 };
 
 export default GooglePublisherTag;
