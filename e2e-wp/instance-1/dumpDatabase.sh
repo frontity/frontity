6773d4a3813a45fe0ac1dd04a9e7820c258ac73d
Michal Czaplinski <mmczaplinski@gmail.com>
2020-08-17 19:26:12 -0500

    remove all the "old" files from the e2e-wp folder

--- e2e-wp/instance-1/dumpDatabase.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-docker-compose exec -T db sh -c "exec mysqldump -u root -ppassword wordpress" > ./data/dump.sql
--- e2e-wp/support/index.js
+++
@@ -1,6 +0,0 @@
-// This file normally contains setup code that will run for EACH test case.
-//
-// However, running global beforeEach() in this file does not play well with
-// beforeEach() in individual test files if both are using cy.task()
-//
-// For this reason, the setup has to be done in the actual test files.
