34bac0ad8d8197fa76afcd175eb60edea3753cff
Luis Herranz <luisherranz@gmail.com>
2020-08-21 12:16:54 +0200

    Connect: Pass only state, actions and libraries

--- packages/connect/src/connect.js
+++ packages/connect/src/connect.js
@@ -28,7 +28,8 @@ const defaultOptions = {
 /**
  * Connects a React component with the Frontity connect store.
  *
- * @param Component The React component to be connected.
+ * @param Component - The React component to be connected.
+ * @param options TO BE LINKED WHEN WE SWITCH TO TYPESCRIPT.
  *
  * @returns The same Component, but with the store included in the props.
  */
@@ -41,28 +42,42 @@ export const connect = (Component, options) => {
 
   if (isStatelessComp) {
     return view((props) => {
-      const store = useContext(context);
+      const { state, actions, libraries } = useContext(context);
       isConnected = true;
       const rendered = Component({
         ...props,
-        ...(options.injectProps ? store : {}),
+        ...(options.injectProps ? { state, actions, libraries } : {}),
       });
       isConnected = false;
       return rendered;
     });
   } else {
-    class ConnectedComponent extends Component {
-      static contextType = context;
+    if (options.injectProps) {
+      /**
+       * A wrapper for the Component that extracts the store from the context
+       * and passes it to the Component.
+       */
+      class ConnectedComponent extends Component {
+        static contextType = context;
 
-      render() {
-        return createElement(Component, {
-          ...this.props,
-          ...(options.injectProps ? this.context : {}),
-        });
+        /**
+         * The render method of this React Class.
+         *
+         * @returns The same Component but with the store injected as props.
+         */
+        render() {
+          const { state, actions, libraries } = this.context;
+          return createElement(Component, {
+            ...this.props,
+            state,
+            actions,
+            libraries,
+          });
+        }
       }
+      return view(ConnectedComponent);
     }
-
-    return view(ConnectedComponent);
+    return view(Component);
   }
 };
 
