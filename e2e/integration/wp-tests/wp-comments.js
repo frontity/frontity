99c5cf61f34433c9a5072c724e311e451804cac8
Michal Czaplinski <mmczaplinski@gmail.com>
2020-08-21 13:16:16 -0500

    e2e: Add structure for adding new tests

--- e2e/integration/wp-tests/wp-comments.js
+++ e2e/integration/wp-tests/wp-comments.js
@@ -1,14 +1,15 @@
-describe("", () => {
+describe("wp-comments", () => {
   beforeEach(() => {
     cy.task("resetDatabase");
     cy.task("removeAllPlugins");
-    cy.visit("http://localhost:3001?name=e2e-wp-comments");
+    cy.visit("http://localhost:3001?name=wp-comments");
   });
 
   it("should work", () => {
+    cy.task("installPlugin", { name: "code-snippets" });
+
     // the path is relative to the e2e directory
-    cy.task("loadDatabase", { path: "./data/db.sql" });
-    cy.task("installPlugin", { name: "wordpress-seo" });
+    cy.task("loadDatabase", { path: "./fixtures/wp-comments.sql" });
     cy.get("[id='test']").should("have.attr", "class", "test");
   });
 });
