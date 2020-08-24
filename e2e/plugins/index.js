8ab6ec3e88bedb74d3924d100a60323482586513
Luis Herranz <luisherranz@gmail.com>
2020-08-24 11:19:54 +0200

    Reactivate Eslint and add TSDocs in plugins file

--- e2e/plugins/index.js
+++ e2e/plugins/index.js
@@ -1,9 +1,27 @@
-/* eslint-disable */
 const { initPlugin } = require("cypress-plugin-snapshots/plugin");
 const execa = require("execa");
 
+/**
+ * The plugins export.
+ *
+ * @param on - The callback from Cypress.
+ * @param config - The config object from Cypress.
+ *
+ * @returns The modified config.
+ */
 module.exports = (on, config) => {
   on("task", {
+    /**
+     * Installs a WordPress plugin.
+     *
+     * Oficial WP CLI docs: https://developer.wordpress.org/cli/commands/plugin/install/.
+     *
+     * @param options - Options object:
+     * - `name`: The name of the plugin.
+     * - `version`: The version of the plugin.
+     *
+     * @returns Null if successful. Throws an error otherwise.
+     */
     installPlugin({ name, version }) {
       return (async () => {
         try {
@@ -13,14 +31,22 @@ module.exports = (on, config) => {
             }`,
             { stdio: "inherit" }
           );
-          // Asynchronous tasks in cypress HAVE TO return a promise which resolves to a value or a null.
-          // https://docs.cypress.io/api/commands/task.html#Return-a-Promise-from-an-asynchronous-task
           return null;
         } catch (err) {
           console.log(err);
         }
       })();
     },
+
+    /**
+     * Loads a new database.
+     *
+     * @param options - Options object:
+     * - `path`: the path where the database SQL file is located. Relative to
+     * the `e2e` folder.
+     *
+     * @returns Null if successful. Throws an error otherwise.
+     */
     loadDatabase({ path }) {
       return (async () => {
         try {
@@ -28,38 +54,7 @@ module.exports = (on, config) => {
             `docker-compose exec -T db mysql -uroot -ppassword wordpress < ${path}`,
             {
               stdio: "inherit",
-              shell: true, // Because we use file redirection (the "<") in the command
-            }
-          );
-          return null;
-        } catch (err) {
-          console.log(err);
-        }
-      })();
-    },
-    resetDatabase() {
-      return (async () => {
-        try {
-          await execa.command(
-            `docker-compose exec -T db mysql -uroot -ppassword wordpress < ./data/db.sql`,
-            {
-              stdio: "inherit",
-              shell: true, // Because we use file redirection (the "<") in the command
-            }
-          );
-          return null;
-        } catch (err) {
-          console.log(err);
-        }
-      })();
-    },
-    removeAllPlugins() {
-      return (async () => {
-        try {
-          await execa.command(
-            `docker-compose exec -T wp /bin/bash -c "rm -rf /var/www/html/wp-content/plugins/*"`,
-            {
-              stdio: "inherit",
+              // Because we use file redirection (the "<") in the command.
               shell: true,
             }
           );
@@ -69,7 +64,57 @@ module.exports = (on, config) => {
         }
       })();
     },
+
+    /**
+     * Loads the default database.
+     *
+     * @returns Null if successful. Throws an error otherwise.
+     */
+    resetDatabase() {
+      return (async () => {
+        try {
+          await execa.command(
+            `docker-compose exec -T db mysql -uroot -ppassword wordpress < ./data/db.sql`,
+            {
+              stdio: "inherit",
+              // Because we use file redirection (the "<") in the command.
+              shell: true,
+            }
+          );
+          return null;
+        } catch (err) {
+          console.log(err);
+        }
+      })();
+    },
+
+    /**
+     * Removes all the WordPress plugins.
+     *
+     * @returns Null if successful. Throws an error otherwise.
+     */
+    removeAllPlugins() {
+      return (async () => {
+        try {
+          await execa.command(
+            `docker-compose exec -T wp /bin/bash -c "rm -rf /var/www/html/wp-content/plugins/*"`,
+            {
+              stdio: "inherit",
+            }
+          );
+          return null;
+        } catch (err) {
+          console.log(err);
+        }
+      })();
+    },
   });
+
+  /**
+   * The initiliazation plugin required for the `cypress-plugin-snapshot`
+   * package.
+   */
   initPlugin(on, config);
+
   return config;
 };
