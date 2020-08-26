427b59adeabcd47bcc57a2851602aa366af09f3f
Luis Herranz <luisherranz@gmail.com>
2020-08-26 10:02:24 +0200

    CLI: Add TSDocs to build, dev and serve

--- packages/frontity/src/cli/build.ts
+++ packages/frontity/src/cli/build.ts
@@ -1,15 +1,67 @@
-import build from "../commands/build";
+import buildCommand from "../commands/build";
 import { errorLogger } from "../utils";
 
-export default async ({
+/**
+ * The options for the {@link build} command.
+ */
+interface BuildOptions {
+  /**
+   * Describes the environments the final bundles will support in the client
+   * side, which can be either `es5` for old browsers with ES6 Proxy support or
+   * `module` for modern browsers with `<script type="module"></script>`
+   * support, or `both` to generate both.
+   *
+   * - Module: edge \>= 16, firefox \>= 60, chrome \>= 61, safari \>= 10.1,
+   * opera \>= 48, ios_saf \>= 10.3, and_ff \>= 60.
+   *
+   * - ES5: and_chr \>= 67, and_ff \>= 18, and_uc \>= 11.8, android \>= 67, not
+   * android \<= 4.4.4, chrome \>= 49, edge \>= 12, firefox \>= 18, ios_saf \>=
+   * 10, not op_mini all, op_mob \>= 46, opera \>= 36, "safari \>= 10, samsung
+   * \>= 5.
+   *
+   * @defaultValue "both"
+   */
+  target?: string;
+
+  /**
+   * Indicates if the bundles should be generated using the mode="development"
+   * option of Webpack.
+   *
+   * Webpack docs: https://webpack.js.org/configuration/mode/.
+   *
+   * @defaultValue false
+   */
+  development?: boolean;
+
+  /**
+   * The publicPath option of Webpack.
+   *
+   * Webpack docs: https://webpack.js.org/guides/public-path/.
+   *
+   * @example
+   * ```sh
+   * npx frontity build --public-path /custom/path
+   * npx frontity build --public-path http://cdn.domain.com/custom/path
+   * ```
+   *
+   * @defaultValue "/static/"
+   */
+  publicPath?: string;
+}
+
+/**
+ * The build CLI command, usually run with `npx frontity build`.
+ *
+ * It takes args from the CLI and checks for the presence of environment
+ * variables. Then, it runs the build command programatically.
+ *
+ * @param options - Defined in {@link BuildOptions}.
+ */
+const build = async ({
   target = process.env.FRONTITY_BUILD_TARGET || "both",
   development = !!process.env.FRONTITY_BUILD_DEVELOPMENT,
   publicPath = process.env.FRONTITY_BUILD_PUBLIC_PATH || "/static/",
-}: {
-  target?: string;
-  development?: boolean;
-  publicPath?: string;
-}) => {
+}: BuildOptions) => {
   // Check `target` parameter.
   if (target && target !== "es5" && target !== "module" && target !== "both") {
     errorLogger(
@@ -19,10 +71,12 @@ export default async ({
     );
   }
 
-  // Execute `build` command.
-  build({
+  // Execute the `build` command.
+  buildCommand({
     target: target as "es5" | "module" | "both",
     development,
     publicPath,
   });
 };
+
+export default build;
--- packages/frontity/src/cli/serve.ts
+++ packages/frontity/src/cli/serve.ts
@@ -1,18 +1,59 @@
-import serve from "../commands/serve";
+import serveCommand from "../commands/serve";
 import { errorLogger } from "../utils";
 
-export default async ({
+/**
+ * The options for the {@link serve} command.
+ */
+interface ServeOptions {
+  /**
+   * The port that should be used to open the server.
+   *
+   * @example
+   * ```sh
+   * # This will start Frontity in http://localhost:3003.
+   * npx frontity serve --port 3003
+   * ```
+   *
+   * @defaultValue "3000"
+   */
+  port?: string;
+
+  /**
+   * Indicates if the server should be started in HTTPS mode.
+   *
+   * The certificates used are stored internally in the Frontity core and are
+   * not meant to be used in production, only in local development.
+   *
+   * @example
+   * ```sh
+   * # This will start Frontity in https://localhost:3000.
+   * npx frontity serve --https
+   * ```
+   *
+   * @defaultValue false
+   */
+  https?: boolean;
+}
+
+/**
+ * The serve CLI command, usually run with `npx frontity serve`.
+ *
+ * It takes args from the CLI and checks for the presence of environment
+ * variables. Then, it runs the serve command programatically.
+ *
+ * @param options - Defined in {@link ServeOptions}.
+ */
+const serve = async ({
   port = process.env.FRONTITY_SERVE_PORT || "3000",
   https = !!process.env.FRONTITY_SERVE_HTTPS,
-}: {
-  port?: string;
-  https?: boolean;
-}) => {
+}: ServeOptions) => {
   // Check `port` parameter.
   if (port && Number.isNaN(parseInt(port, 10))) {
     errorLogger(new Error(`The port number specified is not valid: ${port}.`));
   }
 
   // Execute `serve` command.
-  serve({ port: parseInt(port, 10), https });
+  serveCommand({ port: parseInt(port, 10), https });
 };
+
+export default serve;
