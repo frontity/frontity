e56f34eedc67679e95a050330e216f763d2d4de4
Michal Czaplinski <mmczaplinski@gmail.com>
2020-08-26 18:42:04 -0500

    Add new e2e tests

--- e2e/integration/wordpress-01/wp-comments.spec.js
+++ e2e/integration/wordpress-01/wp-comments.spec.js
@@ -1,33 +1,86 @@
 describe("wp-comments", () => {
   before(() => {
-    // cy.task("installPlugin", { name: "code-snippets" });
+    cy.task("installPlugin", { name: "code-snippets" });
+  });
+
+  beforeEach(() => {
     cy.task("loadDatabase", {
       path: "./wp-data/wp-comments/code-snippets.sql",
     });
+    cy.visit("http://localhost:3001?name=wp-comments");
   });
 
-  // after(() => {
-  //   cy.task("resetDatabase");
-  //   cy.task("removeAllPlugins");
-  // });
+  after(() => {
+    cy.task("resetDatabase");
+    cy.task("removeAllPlugins");
+  });
 
-  const getComments = () =>
-    cy.window().its("frontity").its("state").its("comments").its("forms");
+  const commentForPostId = (postId) => ({
+    shouldHavePropertyWithValue: (property, value) =>
+      cy
+        .window()
+        .its("frontity")
+        .its("state")
+        .its("comments")
+        .its("forms")
+        .its(postId)
+        .should("have.nested.property", property, value),
+  });
 
   it("should work in the basic case", () => {
-    cy.visit("http://localhost:3001?name=wp-comments");
+    cy.get("#comment-ok").click();
 
-    cy.get("#send-comment").click();
+    commentForPostId(1).shouldHavePropertyWithValue("submitted.isOnHold", true);
+    commentForPostId(1).shouldHavePropertyWithValue(
+      "submitted.isApproved",
+      false
+    );
+    commentForPostId(1).shouldHavePropertyWithValue("submitted.id", 2);
+  });
 
-    getComments()
-      .its(1)
-      .should("have.nested.property", "submitted.isError", false);
-    getComments()
-      .its(1)
-      .should("have.nested.property", "submitted.isOnHold", true);
-    getComments()
-      .its(1)
-      .should("have.nested.property", "submitted.isApproved", false);
-    getComments().its(1).should("have.nested.property", "submitted.id", 2);
+  it("should return an error when sending a comment for a non-existing post ID", () => {
+    cy.get("#comment-wrong-id").click();
+
+    commentForPostId(9999).shouldHavePropertyWithValue(
+      "submitted.isError",
+      true
+    );
+    commentForPostId(9999).shouldHavePropertyWithValue(
+      "submitted.errorMessage",
+      "Sorry, you are not allowed to create this comment without a post."
+    );
+  });
+
+  it("should return when sending a comment without an email", () => {
+    cy.get("#comment-no-email").click();
+
+    commentForPostId(1).shouldHavePropertyWithValue("submitted.isError", true);
+    commentForPostId(1).shouldHavePropertyWithValue(
+      "submitted.errorMessage",
+      "Creating a comment requires valid author name and email values."
+    );
+  });
+
+  it("should warn when attempting to send the same comment when another is pending", () => {
+    cy.visit("http://localhost:3001?name=wp-comments", {
+      onBeforeLoad(win) {
+        cy.stub(win.console, "warn").as("consoleWarn");
+      },
+    });
+
+    cy.get("#comment-ok").click();
+    cy.get("#comment-ok").click();
+
+    commentForPostId(1).shouldHavePropertyWithValue("submitted.isOnHold", true);
+    commentForPostId(1).shouldHavePropertyWithValue(
+      "submitted.isApproved",
+      false
+    );
+    commentForPostId(1).shouldHavePropertyWithValue("submitted.id", 2);
+
+    cy.get("@consoleWarn").should(
+      "be.calledWith",
+      "You cannot submit a comment to the same post if another is already pending.\nVisit https://community.frontity.org for help! 🙂\n"
+    );
   });
 });
--- e2e/packages/wp-comments/src/index.tsx
+++ e2e/packages/wp-comments/src/index.tsx
@@ -4,30 +4,63 @@ import { connect } from "frontity";
 import { Connect } from "frontity/types";
 
 /**
- * This is a basic test of the wp-comments.
+ * A component that can send comments to the WordPress backend.
  *
  * @param props - Injected props by {@link connect}.
  * @returns React Element.
  */
 const Component: React.FC<Connect<Package>> = ({ actions, state }) => {
   /**
-   * Send a test comment.
+   * Send a correct test comment.
    */
-  const sendComment1 = () => {
+  const sendComment = () => {
     actions.comments.submit(1, {
       content: "Hello world!",
-      author_email: "frontibotito@frontity.org",
-      author_name: "Michal",
+      authorEmail: "frontibotito@frontity.com",
+      authorName: "Frontitbotito",
+    });
+  };
+
+  /**
+   * Send a comment with a non-existing ID.
+   */
+  const sendCommentWrongId = () => {
+    actions.comments.submit(9999, {
+      content: "Hello world!",
+      authorEmail: "frontibotito@frontity.com",
+      authorName: "Frontitbotito",
+    });
+  };
+
+  /**
+   * Send a comment without an email.
+   */
+  const sendCommentNoEmail = () => {
+    actions.comments.submit(1, {
+      content: "Hello world!",
+      authorEmail: "",
+      authorName: "Frontitbotito",
     });
   };
 
   return (
     <>
-      <button id="send-comment" onClick={sendComment1}>
-        Comment
+      <button id="comment-ok" onClick={sendComment}>
+        Send correct comment
+      </button>
+      <button id="comment-wrong-id" onClick={sendCommentWrongId}>
+        comment with a wrong ID
+      </button>
+      <button id="comment-no-email" onClick={sendCommentNoEmail}>
+        comment with no email
       </button>
 
-      <pre id="form"> {JSON.stringify(state.comments.forms[1], null, 2)}</pre>
+      <pre id="form">
+        {JSON.stringify(state.comments.forms[1]?.submitted, null, 2)}
+      </pre>
+      <pre id="error-message">
+        {state.comments.forms[1]?.submitted?.errorMessage}
+      </pre>
     </>
   );
 };
