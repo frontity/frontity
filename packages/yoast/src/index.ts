b4978175bce4922799829a4feae40919e97212da
David Arenas <darensr@gmail.com>
2020-08-27 15:43:40 +0200

    Revert "Yoast: Reimplement package to use yoast_meta field"
    This reverts commit 5d8af15b5714a8ac1a1ed56c768693dd251f4a0a.

--- packages/yoast/src/index.ts
+++
@@ -1,9 +0,0 @@
-import YoastPackage from "../types";
-import Component from "./components";
-
-const yoastPackage: YoastPackage = {
-  name: "@frontity/yoast",
-  roots: { yoast: Component },
-};
-
-export default yoastPackage;
