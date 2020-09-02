b3dfda6fc2b9e982599788af66825a52c654644a
David Arenas <darensr@gmail.com>
2020-09-02 20:04:41 +0200

    Cypress: Modify visit command to disable scripts

--- e2e/support/commands.js
+++ e2e/support/commands.js
@@ -21,3 +21,30 @@ Cypress.Commands.add("isNotInViewport", { prevSubject: true }, (subject) => {
 
   return subject;
 });
+
+/**
+ * Extends the `visit` command to add an option to disable scripts. Useful when
+ * testing SSR content.
+ *
+ * Based on the solution @pke proposed on the following comment:
+ * https://github.com/cypress-io/cypress/issues/1611#issuecomment-623896822.
+ */
+Cypress.Commands.overwrite("visit", (visit, url, options = {}) => {
+  const parentDocument = cy.state("window").parent.document;
+  const iframe = parentDocument.querySelector(".iframes-container iframe");
+  if (false === options.script) {
+    if (false !== Cypress.config("chromeWebSecurity")) {
+      throw new TypeError(
+        "When you disable script you also have to set 'chromeWebSecurity' in your config to 'false'"
+      );
+    }
+    iframe.sandbox = "";
+  } else {
+    /**
+     * In case it was added by a visit before, the attribute has to be removed
+     * from the iframe.
+     */
+    iframe.removeAttribute("sandbox");
+  }
+  return visit(url, options);
+});
