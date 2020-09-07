4ad322c63a2be8481cc59dff94b3db873188e8f3
Luis Herranz <luisherranz@gmail.com>
2020-09-07 18:41:18 +0200

    Revert frontity settings api URL

--- examples/mars-theme-example/frontity.settings.js
+++ examples/mars-theme-example/frontity.settings.js
@@ -32,7 +32,7 @@ const settings = {
       name: "@frontity/wp-source",
       state: {
         source: {
-          api: "http://frontity.local/wp-json",
+          api: "https://test.frontity.org/wp-json",
         },
       },
     },
