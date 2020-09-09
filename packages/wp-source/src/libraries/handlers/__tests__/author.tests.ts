164db257d39064adf7658d9f228f311d0f3d92ab
Luis Herranz <luisherranz@gmail.com>
2020-09-09 12:03:13 +0200

    Add tests for archive searches

--- packages/wp-source/src/libraries/handlers/__tests__/author.tests.ts
+++ packages/wp-source/src/libraries/handlers/__tests__/author.tests.ts
@@ -13,7 +13,7 @@ import author1PostsCpt from "./mocks/author/author-1-posts-cpt.json";
 let store: InitializedStore<WpSource>;
 let api: jest.Mocked<Api>;
 beforeEach(() => {
-  store = createStore(clone(wpSource()));
+  store = createStore<WpSource>(clone(wpSource()));
   store.state.source.api = "https://test.frontity.org/wp-json";
   store.actions.source.init();
   api = store.libraries.source.api as jest.Mocked<Api>;
