d1fbff89d291503591f99f53b955296c44151d0f
Michal Czaplinski <mmczaplinski@gmail.com>
2020-09-11 18:39:17 -0500

    Add first basic test

---
+++ packages/wp-source/src/libraries/__tests__/custom-headers.test.ts
@@ -0,0 +1,34 @@
+import clone from "clone-deep";
+import { createStore, InitializedStore } from "@frontity/connect";
+import * as frontity from "frontity";
+
+import wpSource from "../../";
+import { mockResponse } from "../handlers/__tests__/mocks/helpers";
+import WpSource from "../../../types";
+
+const mockedFetch: jest.MockedFunction<typeof fetch> = jest
+  .fn()
+  .mockResolvedValueOnce(mockResponse({ id: 1 }));
+(frontity.fetch as typeof fetch) = mockedFetch;
+
+let store: InitializedStore<WpSource>;
+
+beforeEach(() => {
+  store = createStore(clone(wpSource()));
+  store.state.source.api = "https://test.frontity.org/wp-json";
+  store.actions.source.init();
+});
+
+describe("Should work", () => {
+  test("first test", async () => {
+    await store.actions.source.fetch("/");
+
+    expect(mockedFetch.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "https://test.frontity.org/wp-json/wp/v2/posts?_embed=true&page=1",
+        ],
+      ]
+    `);
+  });
+});
