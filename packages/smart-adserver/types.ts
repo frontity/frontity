d7bfb2b90b6138abd9ffad4a060d82f30b3794ac
Michal Czaplinski <mmczaplinski@gmail.com>
2020-10-01 17:53:41 -0500

    Add TSDocs to all the types

--- packages/smart-adserver/types.ts
+++ packages/smart-adserver/types.ts
@@ -1,5 +1,42 @@
 import { Package } from "frontity/types";
 
+/**
+ * Smart Adserver library.
+ */
+interface SAS {
+  /**
+   * The array of function callbacks that the SmartAdserver library uses
+   * to make the ad calls.
+   */
+  cmd: Function[];
+
+  /**
+   * The setup function which initializes the ad manager.
+   */
+  setup?: (params: {
+    /**
+     * ID of the network (account) at Smart.
+     */
+    networkid: string;
+
+    /**
+     * Domain of the network (account) at Smart.
+     */
+    domain: string;
+
+    /**
+     * Specifies if the calls are done asynchronously.
+     * Asynchronous calls are recommended.
+     */
+    async: boolean;
+  }) => void;
+
+  /**
+   * The function which performs a specific ad call.
+   */
+  call?: (calltype: string, options: CallOptions) => void;
+}
+
 declare global {
   /**
    * Extend window with the Smart Adserver globals.
@@ -8,69 +45,46 @@ declare global {
     /**
      * Smart Adserver library.
      */
-    sas: {
-      /**
-       *
-       */
-      cmd: Function[];
-      /**
-       *
-       */
-      setup?: (params: {
-        /**
-         *
-         */
-        networkid: string;
-        /**
-         *
-         */
-        domain: string;
-        /**
-         *
-         */
-        async: boolean;
-      }) => void;
-
-      /**
-       *
-       */
-      call?: (calltype: string, options: CallOptions) => void;
-    };
+    sas: SAS;
   }
 }
 
 /**
+ * The options used for a particular ad call.
+ *
+ * All the possible options are visible in https://support.smartadserver.com/s/article/Tagging-guide.
  *
  */
 export interface CallOptions {
   /**
-   *
+   * Identifies the website; parent element of a page.
    */
   siteId: number;
   /**
-   *
+   * Identifies the page on a website; child element of the website.
    */
   pageId: number;
   /**
-   *
+   * Identifies the format (medium rectangle, skyscraper, etc.).
    */
   formatId: number;
   /**
-   *
+   * The `id` of the container of the page that will contain the ad.
    */
-  tagId: string;
+  tagId?: string;
   /**
-   *
+   * The width of the ad. Used in the calls of type `iframe`.
    */
-  width: number;
+  width?: number;
   /**
-   *
+   * The height of the ad. Used in the calls of type `iframe`.
    */
-  height: number;
+  height?: number;
   /**
-   *
+   * Used to pass keywords and key=value pairs.
+   * See [Using keyword targeting](https://support.smartadserver.com/s/article/Using-keyword-targeting).
    */
-  target: number;
+  target?: number;
 }
 
 /**
@@ -88,20 +102,27 @@ interface SmartAdserver extends Package {
   };
 
   /**
-   * State.
+   * State exposed by this package.
    */
   state: {
     /**
-     *
+     * The smartAdserver namespace.
      */
     smartAdserver: {
       /**
-       *
+       * Reference to window.sas that is used internally by the SmartAds library.
+       */
+      sas: SAS;
+
+      /**
+       * ID of the network (account) at Smart.
        */
       networkId?: string;
 
       /**
+       * The subdomain of your network (account) at Smart.
        *
+       * @example `"www", "www3"`
        */
       subdomain?: string;
     };
@@ -111,7 +132,7 @@ interface SmartAdserver extends Package {
      */
     fills: {
       /**
-       *
+       * The smartAdserver namespace.
        */
       smartAdserver: {};
     };
@@ -126,11 +147,11 @@ interface SmartAdserver extends Package {
      */
     fills: {
       /**
-       *
+       * The smartAdserver namespace.
        */
       smartAdserver: {
         /**
-         *
+         * The component that is used to render the ads.
          */
         SmartAd: React.FC;
       };
