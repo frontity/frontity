6d6146039c7de6cee0f15ba2e7630459c5fd478e
Michal Czaplinski <mmczaplinski@gmail.com>
2020-10-02 18:30:55 -0500

    Add a super basic unit test

---
+++ packages/smart-adserver/src/components/__tests__/smart-ad.test.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import SmartAd from "../smart-ad";
+import SmartAdserver from "../../../types";
+import { State } from "frontity/types";
+
+describe("SmartAdserver", () => {
+  const getState = (): State<SmartAdserver> => ({
+    smartAdserver: {
+      isLoaded: true,
+    },
+    fills: {
+      smartAdserver: {},
+    },
+  });
+
+  test("Should render the SmartAd component", () => {
+    const state = getState();
+
+    const smartAd = TestRenderer.create(
+      <SmartAd
+        state={state}
+        callType="std"
+        siteId={1}
+        pageId={1}
+        formatId={1}
+        tagId="test-smartad"
+      />
+    ).toJSON();
+
+    expect(smartAd).toMatchInlineSnapshot(`
+      <div
+        css={
+          Object {
+            "map": undefined,
+            "name": "0",
+            "next": undefined,
+            "styles": "",
+            "toString": [Function],
+          }
+        }
+        id="test-smartad"
+      />
+    `);
+  });
+});
