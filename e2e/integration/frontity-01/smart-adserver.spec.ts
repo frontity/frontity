a4823c7d792ee7151e53ebfd51b06915a40b5451
Michal Czaplinski <mmczaplinski@gmail.com>
2020-10-01 19:57:32 -0500

    Add more test stubs

--- e2e/integration/frontity-01/smart-adserver.spec.ts
+++ e2e/integration/frontity-01/smart-adserver.spec.ts
@@ -8,13 +8,18 @@ describe("Smart Adserver", () => {
     cy.get(`script[src="http://ced.sascdn.com/tag/123/smart.js"][async]`);
   });
 
-  it("Should render the ad in the slot", () => {});
-
   it("Should render the ad using the SmartAd component from the libraries", () => {});
+
+  it("Should warn if you forget to put the networkId || subdomain", () => {});
 
   it("Should make the iframe call", () => {});
 
   it("Should make the std call", () => {});
+
+  it("Should render the ad in the slot", () => {
+    // Get ad unit from header.
+    cy.get("#header > div").should("have.descendants", "iframe");
+  });
 
   it("Should render with various configurations of options: pageId, formatId, siteId, tagId, target", () => {});
 });
