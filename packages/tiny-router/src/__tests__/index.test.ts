64799b92169053fb4417ed3c4304a85ed8f0cbca
David Arenas <darensr@gmail.com>
2020-10-05 13:01:05 +0200

    TinyRouter: Spy on `warn` in unit tests

--- packages/tiny-router/src/__tests__/index.test.ts
+++ packages/tiny-router/src/__tests__/index.test.ts
@@ -205,15 +205,13 @@ describe("actions", () => {
     test("should warn if autoFetch is enabled but there is no source pkg", () => {
       const ctx = {} as Context;
       get.mockReturnValue({});
-      Object.defineProperty(frontity, "warn", {
-        value: jest.fn(),
-      });
+      const frontityWarn = jest.spyOn(frontity, "warn");
       const store = createStore(config);
       store.actions.source = undefined;
       store.actions.router.beforeSSR({ ctx });
 
-      expect(frontity.warn).toHaveBeenCalledTimes(1);
-      expect(frontity.warn).toHaveBeenCalledWith(
+      expect(frontityWarn).toHaveBeenCalledTimes(1);
+      expect(frontityWarn).toHaveBeenCalledWith(
         "You are trying to use autoFetch but no source package is installed."
       );
     });
