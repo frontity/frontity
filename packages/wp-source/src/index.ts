ff6c2f2c44d759100e10a36f7b53abe38f0e1ac9
David Arenas <darensr@gmail.com>
2020-10-27 17:06:48 +0100

    WpSource: Add some missing TSDocs

--- packages/wp-source/src/index.ts
+++ packages/wp-source/src/index.ts
@@ -3,6 +3,11 @@ import state from "./state";
 import actions from "./actions";
 import { Api, populate, routeUtils, responseUtils } from "./libraries";
 
+/**
+ * Return an instance of the {@link WpSource} package.
+ *
+ * @returns WpSource package.
+ */
 const wpSource = (): WpSource => ({
   name: "@frontity/wp-source",
   state: {
