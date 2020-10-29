ec905486067ed4032510023c7ef4168e8f4b09f2
cristianbote <botecristian@yahoo.com>
2020-10-29 17:46:12 +0200

    Dynamically import serve, build and dev scripts

--- packages/core/src/scripts/index.ts
+++ packages/core/src/scripts/index.ts
@@ -1,3 +1,8 @@
-export { default as dev } from "./dev";
-export { default as build } from "./build";
-export { default as serve } from "./serve";
+// We are exporting each script, with a dynamic import in order to avoid
+// loading the rest of the scripts which might have side-effects
+
+export const serve = (args) =>
+  import("./serve").then((mod) => mod.default(args));
+export const build = (args) =>
+  import("./build").then((mod) => mod.default(args));
+export const dev = (args) => import("./dev").then((mod) => mod.default(args));
