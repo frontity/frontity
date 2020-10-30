fc0869561c67fff8ce5508f99bbe987fc4d8f9ff
Michal Czaplinski <mmczaplinski@gmail.com>
2020-10-29 21:52:04 -0500

    Add a proper comment to the derived state

--- packages/types/src/derived.ts
+++ packages/types/src/derived.ts
@@ -4,7 +4,17 @@ import Package from "./package";
 import { ResolveState, ResolveActions, Serializable } from "./utils";
 
 /**
- * Derived state can be
+ * Derived state is state that is calculated based on other piece of state in
+ * your application.
+ *
+ * Derived state can have one of three types:
+ * 1. JS serializable value (number, string, etc). In this case, there is no
+ *   "derivation" happening.
+ * 2. Function which takes the application state, actions and libraries as
+ *   arguments. The final state is calculated by invoking that function.
+ * 3. A curried function, which takes takes the application state, actions and libraries as
+ *   arguments and returns a function that takes any number of parameters. That
+ *   function works like a "getter" for a piece of state.
  */
 export type Derived<Pkg extends Package, InputOrOutput, Output = null> = [
   InputOrOutput
