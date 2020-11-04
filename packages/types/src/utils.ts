84961683771c5114aa61d92be816af2d2b56ae3e
David Arenas <darensr@gmail.com>
2020-11-04 18:55:27 +0100

    Types:  Improve TSDocs for action types

--- packages/types/src/utils.ts
+++ packages/types/src/utils.ts
@@ -1,23 +1,45 @@
 import { Package } from ".";
 
-// Resolve derived state to its final form.
+/**
+ * Frontity properties injected to actions.
+ *
+ * @typeparam Packages - All package definitions together.
+ */
+export interface ResolvePackages<Packages extends Package> {
+  /**
+   * State from all packages resolved.
+   */
+  state: ResolveState<Packages["state"]>;
+
+  /**
+   * Actions from all packages resolved.
+   */
+  actions: ResolveActions<Packages["actions"]>;
+
+  /**
+   * Libraries from all packages.
+   */
+  libraries: Packages["libraries"];
+}
+
+/**
+ * Resolve derived state to its final form.
+ */
 export type ResolveState<State extends Package["state"]> = {
   [P in keyof State]: State[P] extends (state: Package["state"]) => any
     ? ReturnType<State[P]>
     : ResolveState<State[P]>;
 };
 
-// Resolve actions to its final form.
+/**
+ * Resolve actions to its final form.
+ */
 export type ResolveActions<Actions extends Package["state"]> = {
   [P in keyof Actions]: Actions[P] extends ({
     state,
     actions,
     libraries,
-  }: {
-    state: ResolveState<Package["state"]>;
-    actions: ResolveActions<Package["actions"]>;
-    libraries: Package["libraries"];
-  }) => (...args: any[]) => void | Promise<void>
+  }: ResolvePackages<Package>) => (...args: any[]) => void | Promise<void>
     ? (
         ...args: Parameters<ReturnType<Actions[P]>>
       ) => ReturnType<ReturnType<Actions[P]>>
@@ -25,16 +47,14 @@ export type ResolveActions<Actions extends Package["state"]> = {
         state,
         actions,
         libraries,
-      }: {
-        state: ResolveState<Package["state"]>;
-        actions: ResolveActions<Package["actions"]>;
-        libraries: Package["libraries"];
-      }) => void | Promise<void>
+      }: ResolvePackages<Package>) => void | Promise<void>
     ? () => ReturnType<Actions[P]>
     : ResolveActions<Actions[P]>;
 };
 
-// Util to filter the injected props from connect.
+/**
+ * Util to filter the injected props from connect.
+ */
 export type FilterInjectedProps<T extends Package> = Omit<
   T,
   "state" | "actions" | "libraries" | "roots" | "fills"
