e0a6dc0ef1cb8bc11410290f52c129d161c797e0
David Arenas <darensr@gmail.com>
2020-11-10 13:20:36 +0100

    Types: Move `Frontity` and `Fill` to files

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,7 @@
 export { default as Settings } from "./settings";
-export { default as Package, Fill, Frontity } from "./package";
+export { default as Package } from "./package";
+export { default as Fill } from "./fill";
+export { default as Frontity } from "./frontity";
 export { Action, AsyncAction, ServerAction, Context } from "./action";
 export { default as Derived } from "./derived";
 export { default as State } from "./state";
