566d4bf1d306e3622007b848d9775b38dffc9b45
Luis Herranz <luisherranz@gmail.com>
2020-11-26 22:15:33 +0100

    Add BrowserStack CLI [WIP]

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 // @ts-check
+require("dotenv").config();
+const { readFile } = require("fs").promises;
+const { platform } = require("os");
 const waitOn = require("wait-on");
 const execa = require("execa");
 const cypress = require("cypress");
 const argv = require("minimist")(process.argv.slice(2));
-const { platform } = require("os");
+const fetch = require("node-fetch").default;
 
 /**
  * Validates the CLI arguments and throws an error if a value is not correct.
@@ -37,17 +40,81 @@ prod = prod || false;
 cypressCommand = cypressCommand || "open";
 suite = suite || "all";
 
-// Flag to know if we have started Docker.
+// Flag to know if we have started Docker or BrowserStack Local.
 let isDockerRunning = false;
+let isBrowserStackLocalRunning = false;
 
 // Validate CLI args.
 validateArgs(target, { possibleValues: ["es5", "module", "both"] });
 validateArgs(browser, { possibleValues: ["firefox", "chrome", "edge"] });
-validateArgs(cypressCommand, { possibleValues: ["open", "run", "off"] });
+validateArgs(cypressCommand, {
+  possibleValues: ["open", "run", "off", "browserstack"],
+});
 
 // We have to make sure that we are runnng inside of the e2e directory The
 // script assumes that files are relative to this location.
 process.chdir(__dirname);
+
+/**
+ * Stops the processes that are still running.
+ */
+const stop = async () => {
+  if (isDockerRunning) {
+    // Stop all the containers and remove all the volumes that they use (the
+    // `-v` option).
+    await execa("docker-compose", ["down", "-v"], {
+      stdio: "inherit",
+    });
+    isDockerRunning = false;
+  }
+
+  if (isBrowserStackLocalRunning) {
+    // Stop BrowserStackLoca.
+    await execa(
+      "./browserstack-local/macOS",
+      ["--key", process.env.BROWSERSTACK_ACCESS_KEY, "--daemon", "stop"],
+      {
+        stdio: "inherit",
+      }
+    );
+    isBrowserStackLocalRunning = false;
+  }
+};
+
+/**
+ * Sleep for a number of seconds.
+ *
+ * @param sec - The number of seconds to wait.
+ * @returns Resolves once the seconds have passed.
+ */
+const sleep = (sec) => {
+  return new Promise((resolve) => setTimeout(resolve, sec * 1000));
+};
+
+/**
+ * Retrieve the status of the tests that are running.
+ *
+ * @param buildId - The id of the current build.
+ * @returns The status of the build.
+ */
+const browserStackStatus = async (buildId) => {
+  const response = await fetch(
+    `https://api.browserstack.com/automate/cypress/v1/builds/${buildId}`,
+    {
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            process.env.BROWSERSTACK_USERNAME +
+              ":" +
+              process.env.BROWSERSTACK_ACCESS_KEY
+          ).toString("base64"),
+      },
+    }
+  );
+  const { status } = await response.json();
+  return status;
+};
 
 (async () => {
   try {
@@ -144,49 +211,115 @@ process.chdir(__dirname);
     // eslint-disable-next-line require-atomic-updates
     process.env.NODE_OPTIONS = "";
 
-    // Run Cypress if the `cypressCommnand` is not "off".
-    if (cypressCommand !== "off") {
+    // Run Cypress in the Browserstack cloud.
+    if (cypressCommand === "browserstack") {
+      if (!process.env.BROWSERSTACK_ACCESS_KEY)
+        throw new Error(
+          "The $BROWSERSTACK_ACCESS_KEY env variable is required. Please create one. You can use the .env file."
+        );
+
+      // Start Browserstack local.
+      console.log("\nStarting BrowserStack Local...");
+
+      switch (platform()) {
+        case "darwin":
+          await execa(
+            "./browserstack-local/macOS",
+            ["--key", process.env.BROWSERSTACK_ACCESS_KEY, "--daemon", "start"],
+            {
+              stdio: "inherit",
+            }
+          );
+          break;
+        case "win32":
+          await execa(
+            "./browserstack-local/win.exe",
+            ["--key", process.env.BROWSERSTACK_ACCESS_KEY, "--daemon", "start"],
+            {
+              stdio: "inherit",
+            }
+          );
+          break;
+        default:
+          await execa(
+            "./browserstack-local/linux",
+            ["--key", process.env.BROWSERSTACK_ACCESS_KEY, "--daemon", "start"],
+            {
+              stdio: "inherit",
+            }
+          );
+          break;
+      }
+
+      isBrowserStackLocalRunning = true;
+
+      // Launch the cloud tests.
+      console.log("\nSending the tests to BrowserStack...");
+      await execa("npx", ["browserstack-cypress", "run"], { stdio: "inherit" });
+
+      // Get info for this build.
+      const buildResults = await readFile("log/build_results.txt", "utf-8");
+      const {
+        buildId,
+        buildUrl,
+      } = /BUILD_ID=(?<buildId>[^\n]+)\nBUILD_URL=(?<buildUrl>.+)/.exec(
+        buildResults
+      ).groups;
+      console.log(
+        `\nTests started. You can take a look at the dashboard here: ${buildUrl}`
+      );
+
+      // Wait until the tests finish.
+      let status = "running";
+      while (status === "running") {
+        console.log("Test are running, please wait...");
+        await sleep(10);
+        status = await browserStackStatus(buildId);
+      }
+      console.log(`\nTests have finished! Final status is: ${status}`);
+
+      // Stop the processes that we started.
+      await stop();
+      // Exit the process to indicate that everything went fine.
+      process.exit(0);
+    } else if (cypressCommand !== "off") {
+      // Run Cypress if the `cypressCommnand` is not "off".
       if (cypressCommand === "open") {
         await cypress.open({ env: { WORDPRESS_VERSION: wpVersion }, browser });
       } else if (cypressCommand === "run") {
         if (suite === "all") {
           await cypress.run({
             env: { WORDPRESS_VERSION: wpVersion },
-            spec: `./integration/**/*.spec.js`,
+            spec: `**/integration/**/*.spec.{js|ts}`,
             browser,
           });
         } else {
           await cypress.run({
             env: { WORDPRESS_VERSION: wpVersion },
             browser,
-            spec: `./integration/${suite}/**/*.spec.js`,
+            spec: `**/integration/${suite}/**/*.spec.{js|ts}`,
           });
         }
       }
 
-      if (isDockerRunning) {
-        // Stop all the containers and remove all the volumes that they use (the
-        // `-v` option).
-        await execa("docker-compose", ["down", "-v"], {
-          stdio: "inherit",
-        });
-      }
-
+      // Stop the processes that we started.
+      await stop();
       // Exit the process to indicate that everything went fine.
       process.exit(0);
     }
   } catch (err) {
     console.error(err);
-
-    if (isDockerRunning) {
-      // Stop all the containers and remove all the volumes that they use (the
-      // `-v` option).
-      await execa("docker-compose", ["down", "-v"], {
-        stdio: "inherit",
-      });
-    }
-
+    // Stop the processes that we started.
+    await stop();
     // We need to return the exit code so that the GitHub action returns a fail.
     process.exit(1);
   }
 })();
+
+// Capture CTRL+C.
+process.on("SIGINT", async () => {
+  console.log("\n\nStopping the processes that we started, please wait.");
+  // Stop the processes that we started.
+  await stop();
+  process.exit();
+});
