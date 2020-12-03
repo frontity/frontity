0c4eef827176fe6ccf9777bae0f81fcffb66514a
David Arenas <darensr@gmail.com>
2020-12-03 11:23:03 +0100

    IsDerived: fix typo

--- packages/frontity/src/utils/is-derived.ts
+++ packages/frontity/src/utils/is-derived.ts
@@ -7,7 +7,7 @@
  *
  * @param obj - The object from the state that contains the property.
  * @param propName - The property name.
- * @returns True if derived, False if not derived or overwritten.
+ * @returns True if derived, false if not derived or overwritten.
  */
 const isDerived = (obj, propName) =>
   typeof Object.getOwnPropertyDescriptor(obj, propName).value === "function";
