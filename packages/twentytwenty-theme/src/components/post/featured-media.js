01f11de95d20cc71ebf8ceb5393810446b1de5ad
SantosGuillamot <santosguillamot@gmail.com>
2020-12-16 09:07:50 +0100

    Remove featured images min-height in twentytwenty

--- packages/twentytwenty-theme/src/components/post/featured-media.js
+++ packages/twentytwenty-theme/src/components/post/featured-media.js
@@ -39,7 +39,6 @@ export default connect(FeaturedMedia);
 const Figure = styled.figure`
   margin-top: 5rem;
   position: relative;
-  min-height: 300px;
 
   @media (min-width: 700px) {
     margin-top: 6rem;
