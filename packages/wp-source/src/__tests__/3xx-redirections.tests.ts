751a80d096844e400fd2dc23cf8c6c974f0a9b71
Luis Herranz <luisherranz@gmail.com>
2021-01-12 11:44:12 +0100

    Update 30x tests [WIP]

--- packages/wp-source/src/__tests__/3xx-redirections.tests.ts
+++ packages/wp-source/src/__tests__/3xx-redirections.tests.ts
@@ -1,16 +1,15 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import * as frontity from "frontity";
-import merge from "deepmerge";
 import clone from "clone-deep";
 import { InitializedStore, createStore } from "@frontity/connect";
-import WpSource, { Pattern, Handler } from "../../types";
+import { Pattern, Handler, Packages } from "../../types";
 import wpSource from "..";
 import { ServerError } from "@frontity/source";
 import { RedirectionData } from "@frontity/source/types/data";
 import { Headers } from "node-fetch";
 
 let handler: jest.Mocked<Pattern<Handler>>;
-let store: InitializedStore<WpSource>;
+let store: InitializedStore<Packages>;
 let mockedFetch: jest.MockedFunction<typeof fetch>;
 
 declare global {
@@ -22,7 +21,8 @@ declare global {
 }
 
 /**
- * Add a `.toHaveBeenCalledBefore()` matcher to jest to make the tests more clear.
+ * Add a `.toHaveBeenCalledBefore()` matcher to jest to make the tests more
+ * clear.
  */
 expect.extend({
   toHaveBeenCalledBefore(first, second) {
@@ -43,7 +43,7 @@ expect.extend({
 });
 
 const serverFetchParams = [
-  "https://localhost:8080/some-post/",
+  "https://wp.domain.com/some-post/",
   {
     method: "HEAD",
     redirect: "manual",
@@ -52,17 +52,17 @@ const serverFetchParams = [
 const serverResponse = {
   status: 301,
   headers: new Headers({
-    location: "https://localhost:8080/redirected-url",
+    location: "https://wp.domain.com/redirected-url",
   }),
 };
 const clientFetchParams = [
-  "https://localhost:8080/some-post/",
+  "https://wp.domain.com/some-post/",
   {
     method: "HEAD",
   },
 ];
 const clientResponse = {
-  url: "https://localhost:8080/redirected-url",
+  url: "https://wp.domain.com/redirected-url",
   redirected: true,
   status: 301,
 };
@@ -78,25 +78,13 @@ describe.each`
     );
     (frontity.fetch as typeof fetch) = mockedFetch;
 
-    // Initialize the store
-    store = createStore<WpSource>(
-      clone(
-        merge(
-          wpSource(),
-          {
-            state: {
-              frontity: {
-                platform,
-              },
-              source: {
-                url: "https://localhost:8080",
-              },
-            },
-          },
-          { clone: false }
-        )
-      )
-    );
+    // Initialize the store.
+    store = createStore<Packages>(clone(wpSource()));
+    store.state.source.url = "https://wp.domain.com";
+    store.state.frontity = {
+      platform,
+      url: "https://www.domain.com",
+    };
 
     handler = {
       name: "always",
@@ -108,8 +96,6 @@ describe.each`
           type: "example",
           id: 1,
           isPostType: true,
-          isFetching: true,
-          isReady: false,
         });
       }),
     };
@@ -118,7 +104,7 @@ describe.each`
   });
 
   describe("redirections: all", () => {
-    it(`${platform}:  Should handle a redirect with 'all`, async () => {
+    it(`${platform}: Should handle a redirect with 'all`, async () => {
       store.state.source.redirections = "all";
 
       await store.actions.source.fetch("/some-post/");
@@ -156,10 +142,10 @@ describe.each`
       }) as any;
     });
 
-    it(`${platform}:  Should ignore the redirection if there is no error`, async () => {
+    it(`${platform}: Should ignore the redirection if there is no error`, async () => {
       store.state.source.redirections = "404";
 
-      // Restore the handler to the original implementation
+      // Restore the handler to the original implementation.
       handler.func.mockRestore();
 
       await store.actions.source.fetch("/some-post/");
@@ -168,10 +154,10 @@ describe.each`
       expect(handler.func).toHaveBeenCalledTimes(1);
     });
 
-    it(`${platform}:  Should NOT redirect on an error that is not a 404`, async () => {
+    it(`${platform}: Should NOT redirect on an error that is not a 404`, async () => {
       store.state.source.redirections = "404";
 
-      // Throw a 500 inside of the handler
+      // Throw a 500 inside of the handler.
       handler.func = jest.fn(() => {
         throw new ServerError("There was an error", 500);
       }) as any;
@@ -182,12 +168,12 @@ describe.each`
       expect(handler.func).toHaveBeenCalledTimes(1);
     });
 
-    it(`${platform}:  Should redirect on 404`, async () => {
+    it(`${platform}: Should redirect on 404`, async () => {
       store.state.source.redirections = "404";
 
       await store.actions.source.fetch("/some-post/");
 
-      // the `fetch()` was called
+      // The `fetch()` was called.
       expect(mockedFetch).toHaveBeenCalledTimes(1);
       expect(mockedFetch).toHaveBeenCalledWith(...fetchRedirectionParams);
 
@@ -198,6 +184,7 @@ describe.each`
               Object {
                 "/some-post/": Object {
                   "is301": true,
+                  "isExternal": false,
                   "isFetching": false,
                   "isReady": true,
                   "isRedirection": true,
@@ -212,7 +199,7 @@ describe.each`
           `);
     });
 
-    it(`${platform}:  Should redirect on 404 when the redirection is a 302 redirection`, async () => {
+    it(`${platform}: Should redirect on 404 when the redirection is a 302 redirection`, async () => {
       store.state.source.redirections = "404";
 
       if (platform === "server") {
@@ -234,22 +221,18 @@ describe.each`
 
       await store.actions.source.fetch("/some-post/");
 
-      // the `fetch()` was called
+      // The `fetch()` was called.
       expect(mockedFetch).toHaveBeenCalledTimes(1);
       expect(mockedFetch).toHaveBeenCalledWith(...fetchRedirectionParams);
 
       expect(handler.func).toHaveBeenCalledTimes(1);
       expect(handler.func).toHaveBeenCalledBefore(mockedFetch);
 
-      if (platform === "server") {
-        expect(
-          (store.state.source.data["/some-post/"] as RedirectionData).is302
-        ).toBe(true);
-
-        expect(store.state.source.data).toMatchInlineSnapshot(`
+      const snapshot = `
         Object {
           "/some-post/": Object {
-            "is302": true,
+            "is30${platform === "server" ? "2" : "1"}": true,
+            "isExternal": false,
             "isFetching": false,
             "isReady": true,
             "isRedirection": true,
@@ -257,39 +240,25 @@ describe.each`
             "location": "/redirected-url",
             "page": 1,
             "query": Object {},
-            "redirectionStatus": 302,
+            "redirectionStatus": 30${platform === "server" ? "2" : "1"},
             "route": "/some-post/",
           },
         }
-      `);
-      } else {
-        expect(
-          (store.state.source.data["/some-post/"] as RedirectionData).is301
-        ).toBe(true);
+      `;
 
-        expect(store.state.source.data).toMatchInlineSnapshot(`
-        Object {
-          "/some-post/": Object {
-            "is301": true,
-            "isFetching": false,
-            "isReady": true,
-            "isRedirection": true,
-            "link": "/some-post/",
-            "location": "/redirected-url",
-            "page": 1,
-            "query": Object {},
-            "redirectionStatus": 301,
-            "route": "/some-post/",
-          },
-        }
-      `);
-      }
+      const is302 = platform === "server";
+      const is301 = platform === "client";
+      const data = store.state.source.data["/some-post/"] as RedirectionData;
+
+      expect(!!data.is302).toBe(is302);
+      expect(!!data.is301).toBe(is301);
+      expect(store.state.source.data).toMatchInlineSnapshot(snapshot);
     });
 
-    it(`${platform}:  Should return a normal 404 error if fetching the redirection fails`, async () => {
+    it(`${platform}: Should return a normal 404 error if fetching the redirection fails`, async () => {
       store.state.source.redirections = "404";
 
-      // Fetching the redirection fails
+      // Fetching the redirection fails.
       mockedFetch = jest.fn().mockRejectedValueOnce("Fetch Error");
       (frontity.fetch as typeof fetch) = mockedFetch;
 
@@ -314,7 +283,7 @@ describe.each`
           `);
     });
 
-    it(`${platform}:  Should handle redirections if the URL contains a query string`, async () => {
+    it(`${platform}: Should handle redirections if the URL contains a query string`, async () => {
       store.state.source.redirections = "404";
 
       await store.actions.source.fetch("/some-post?key=value&key2=value2");
@@ -323,6 +292,7 @@ describe.each`
               Object {
                 "/some-post/?key=value&key2=value2": Object {
                   "is301": true,
+                  "isExternal": false,
                   "isFetching": false,
                   "isReady": true,
                   "isRedirection": true,
@@ -349,11 +319,11 @@ describe.each`
       }) as any;
     });
 
-    it(`${platform}:  Should fetch the redirection when the redirection is a single regex`, async () => {
+    it(`${platform}: Should fetch the redirection when the redirection is a single regex`, async () => {
       store.state.source.redirections = "RegExp:/some-(\\w*)";
       await store.actions.source.fetch("/some-post");
 
-      // the `fetch()` was called.
+      // The `fetch()` was called.
       expect(mockedFetch).toHaveBeenCalledTimes(1);
       expect(mockedFetch).toHaveBeenCalledWith(...fetchRedirectionParams);
 
@@ -377,11 +347,11 @@ describe.each`
           `);
     });
 
-    it(`${platform}:  Should just return a 404 if the redirection does not match the regex`, async () => {
+    it(`${platform}: Should just return a 404 if the redirection does not match the regex`, async () => {
       store.state.source.redirections = "RegExp:/some-other-post";
       await store.actions.source.fetch("/some-post");
 
-      // the `fetch()` was NOT called
+      // The `fetch()` was NOT called.
       expect(mockedFetch).toHaveBeenCalledTimes(0);
       expect(handler.func).toHaveBeenCalledTimes(1);
 
@@ -403,14 +373,14 @@ describe.each`
           `);
     });
 
-    it(`${platform}:  Should fetch the redirection when the redirection is an array of regexes`, async () => {
+    it(`${platform}: Should fetch the redirection when the redirection is an array of regexes`, async () => {
       store.state.source.redirections = [
         "RegExp:/some-(.*)",
         "RegExp:/some-other-post",
       ];
       await store.actions.source.fetch("/some-post");
 
-      // the `fetch()` was called.
+      // The `fetch()` was called.
       expect(mockedFetch).toHaveBeenCalledTimes(1);
       expect(mockedFetch).toHaveBeenCalledWith(...fetchRedirectionParams);
 
@@ -434,14 +404,14 @@ describe.each`
           `);
     });
 
-    it(`${platform}:  Should fetch the redirection when the redirection is an array of regexes that includes a 404 AND the regex does NOT match the route`, async () => {
+    it(`${platform}: Should fetch the redirection when the redirection is an array of regexes that includes a 404 AND the regex does NOT match the route`, async () => {
       store.state.source.redirections = [
         "404",
         "RegExp:/some-other-post", // This regex does not match
       ];
       await store.actions.source.fetch("/some-post");
 
-      // the `fetch()` was called.
+      // The `fetch()` was called.
       expect(mockedFetch).toHaveBeenCalledTimes(1);
       expect(mockedFetch).toHaveBeenCalledWith(...fetchRedirectionParams);
 
@@ -469,7 +439,7 @@ describe.each`
           `);
     });
 
-    it(`${platform}:  Should fetch the redirection when the redirection is an array of regexes that includes a 404 AND the regex matches a route`, async () => {
+    it(`${platform}: Should fetch the redirection when the redirection is an array of regexes that includes a 404 AND the regex matches a route`, async () => {
       store.state.source.redirections = [
         "404",
         "RegExp:/some-(\\w*)", // This regex DOES match
