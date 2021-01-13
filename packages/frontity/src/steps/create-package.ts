66f3b2f0d3271455cf34046ed3c147d27b7e2f3e
cristianbote <botecristian@yahoo.com>
2021-01-13 15:12:27 +0200

    Switch to jsx runtime syntax

--- packages/frontity/src/steps/create-package.ts
+++ packages/frontity/src/steps/create-package.ts
@@ -9,13 +9,24 @@ import { fetchPackageVersion } from "../utils";
  * Options passed to the `create-package` function.
  */
 export type Options = {
-  // Name of the package.
+  /**
+   * Name of the package.
+   */
   name?: string;
-  // Namespace of the package.
+
+  /**
+   * Namespace of the package.
+   */
   namespace?: string;
-  // Path of the Frontity project.
+
+  /**
+   * Path of the Frontity project.
+   */
   projectPath?: string;
-  // Path where the package should be created (relative to `projectPath`).
+
+  /**
+   * Path where the package should be created (relative to `projectPath`).
+   */
   packagePath?: string;
 };
 
