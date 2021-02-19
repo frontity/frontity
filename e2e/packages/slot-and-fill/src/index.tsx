d2d2978fb9f3d311ea77e674d1a314217874909d
Mario <santosguillamot@gmail.com>
2021-02-19 13:58:39 +0100

    Add a SSR fill to test it in Selenium

--- e2e/packages/slot-and-fill/src/index.tsx
+++ e2e/packages/slot-and-fill/src/index.tsx
@@ -6,6 +6,7 @@ const Root: FC = connect(() => {
   const { actions } = useConnect<SlotAndFill>();
   const fills1 = useFills("slot 1");
   const fills2 = useFills("slot 2");
+  const SSRfills = useFills("slot SSR");
 
   return (
     <>
@@ -25,6 +26,12 @@ const Root: FC = connect(() => {
         )}
       </div>
       <hr />
+      <div id="useFills-SSRslot">
+        {SSRfills.map(({ Fill, key, props }) => (
+          <Fill key={key} {...props} />
+        ))}
+      </div>
+      <hr />
       <button id="addFill1" onClick={actions.slotAndFill.addFill1}>
         Add Fill1
       </button>
@@ -39,7 +46,7 @@ const Root: FC = connect(() => {
 });
 
 /**
- * The props of the {@link Fill1} and {@link Fill2} components.
+ * The props of the {@link Fill1}, {@link Fill2}, and {@link FillSSR} components.
  */
 interface FillProps {
   /**
@@ -66,11 +73,28 @@ const Fill1: FC<FillProps> = ({ text }) => <div id="Fill1">{text}</div>;
  */
 const Fill2: FC<FillProps> = ({ text }) => <div id="Fill2">{text}</div>;
 
+/**
+ * A Fill component that renders some text and has some id.
+ *
+ * @param props - The props of the component, defined by {@link FillProps}.
+ *
+ * @returns A div tag with some text.
+ */
+const SSRFill: FC<FillProps> = ({ text }) => <div id="SSRFill">{text}</div>;
+
 const slotAndFill: SlotAndFill = {
   name: "e2e-slot-and-fill",
   state: {
     fills: {
-      slotAndFill: {},
+      slotAndFill: {
+        fillSSR: {
+          slot: "slot SSR",
+          library: "slotAndFill.SSRFill",
+          props: {
+            text: "I am SSRFill",
+          },
+        },
+      },
     },
   },
   actions: {
@@ -107,6 +131,7 @@ const slotAndFill: SlotAndFill = {
       slotAndFill: {
         Fill1,
         Fill2,
+        SSRFill,
       },
     },
   },
