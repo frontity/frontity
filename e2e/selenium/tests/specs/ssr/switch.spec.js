5166ddbcd1c35cd5d3cef6bc3d4d0a70aaf11a6a
Mario <santosguillamot@gmail.com>
2021-02-23 12:22:59 +0100

    Add switch SSR spec

---
+++ e2e/selenium/tests/specs/ssr/switch.spec.js
@@ -0,0 +1,18 @@
+const { By } = require("selenium-webdriver");
+const assert = require("assert");
+
+describe("switch", function () {
+  this.timeout(600000);
+  beforeEach(async function () {
+    await driver.get("http://localhost:3000/?frontity_name=switch");
+  });
+
+  it("should render last component when no matching component is found", async function () {
+    assert.equal(
+      await driver.findElement(By.id("default")).getText(),
+      "Default"
+    );
+    assert(!(await driver.findElements(By.id("one"))).length);
+    assert(!(await driver.findElements(By.id("two"))).length);
+  });
+});
