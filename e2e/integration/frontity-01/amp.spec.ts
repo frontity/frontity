beb71bee9279e07f5689f9c1668d6772dd004075
cristianbote <botecristian@yahoo.com>
2021-02-23 09:09:21 +0200

    Added proper count assertion for style tags

--- e2e/integration/frontity-01/amp.spec.ts
+++ e2e/integration/frontity-01/amp.spec.ts
@@ -2,7 +2,6 @@ const assertTemplate = () => {
   cy.get("html").should("have.attr", "amp");
   cy.get("style[amp-boilerplate]").should("exist");
   cy.get("style[amp-custom]").should("exist");
-  cy.get("style").should("have.length", 2);
   cy.get("body script").should("not.exist");
 };
 
@@ -10,6 +9,11 @@ describe("AMP", () => {
   it("should render the template from the server", () => {
     cy.visitSSR("http://localhost:3001/?frontity_name=amp").then(() => {
       assertTemplate();
+
+      // The static content should contain only 2 stlye tags.
+      cy.get("style[amp-boilerplate]").should("have.length", 2);
+      cy.get("style[amp-custom]").should("have.length", 1);
+      cy.get("style").should("have.length", 3);
     });
   });
 
