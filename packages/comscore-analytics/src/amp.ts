acc1c20549f2b02edf143ba3b0d714e867cdb25e
David Arenas <darensr@gmail.com>
2021-03-02 18:28:28 +0100

    Revert "Comscore: Add AMP files"
    This reverts commit 6907297cd7ce08cb26679ecd2bc8d6109ee3dbdc.

--- packages/comscore-analytics/src/amp.ts
+++
@@ -1,9 +0,0 @@
-import ComscoreAnalytics from "../types/amp";
-import Root from "./components";
-
-export default {
-  name: "@frontity/comscore-analytics",
-  roots: {
-    comscoreAnalytics: Root,
-  },
-} as ComscoreAnalytics;
