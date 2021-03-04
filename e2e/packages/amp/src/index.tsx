2ac6e43f4c1cbfbedd2087af5af029a38d4bff7c
Michal Czaplinski <mmczaplinski@gmail.com>
2021-03-03 22:10:30 -0500

    Add container to amp e2e tests

--- e2e/packages/amp/src/index.tsx
+++ e2e/packages/amp/src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import Package from "../types";
-import { connect, Head } from "frontity";
+import { connect, Head, styled } from "frontity";
+
+const Container = styled.main`
+  margin: 0 auto;
+  max-width: 58rem;
+`;
 
 /**
  * A React component to render the test AMP theme.
@@ -13,7 +18,7 @@ const Theme = connect(({ state, libraries }) => {
   const Html2React = libraries.html2react.Component;
 
   return (
-    <>
+    <Container>
       <Head>
         {/* AMP needs a link in head with rel="canonical" */}
         <link
@@ -29,7 +34,7 @@ const Theme = connect(({ state, libraries }) => {
           <Html2React html={post.content.rendered} />
         </div>
       </div>
-    </>
+    </Container>
   );
 });
 
