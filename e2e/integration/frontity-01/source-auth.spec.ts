31b0eeaa694ee5a3cc3a1c084f2eeb1be9d3c757
Michal Czaplinski <mmczaplinski@gmail.com>
2021-03-23 16:36:57 -0500

    Add a test for SSR

--- e2e/integration/frontity-01/source-auth.spec.ts
+++ e2e/integration/frontity-01/source-auth.spec.ts
@@ -1,8 +1,19 @@
-describe("Source auth", () => {
-  it("Should remove state.source.auth", () => {
+describe("Source Auth", () => {
+  it("Should remove the auth token from the client", () => {
     cy.visit(
       // We load the 'render' project but it doesn't matter as we're only
       // interested in window.frontity
+      "http://localhost:3001/?frontity_name=render&frontity_source_auth=test"
+    );
+
+    cy.window().its("frontity.state.source.auth").should("be.undefined");
+    cy.window()
+      .its("frontity.state.frontity.options.sourceAuth")
+      .should("be.undefined");
+  });
+
+  it("Should remove auth token in a site with SSR-only", () => {
+    cy.visitSSR(
       "http://localhost:3001/?frontity_name=render&frontity_source_auth=test"
     );
 
