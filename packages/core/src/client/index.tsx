bc9eb6b14a81ce741a4b7addd7b64cf8cb97120d
Luis Herranz <luisherranz@gmail.com>
2021-04-15 12:54:38 +0200

    Pass the isHmr value from the entry point

--- packages/core/src/client/index.tsx
+++ packages/core/src/client/index.tsx
@@ -3,8 +3,30 @@ import { loadableReady } from "@loadable/component";
 import { getSnapshot } from "@frontity/connect";
 import App from "../app";
 import createStore from "./store";
+import { Package } from "@frontity/types";
 
-export default async ({ packages }) => {
+/**
+ * The options of the client function.
+ */
+interface ClientOptions {
+  /**
+   * Map of packages for the site that is being loaded.
+   */
+  packages: Record<string, Package>;
+
+  /**
+   * True in development mode when doing a refresh with HMR.
+   */
+  isHmr?: boolean;
+}
+
+/**
+ * The entry funciton for the client application. It handles the bootstrap and
+ * hydration of Frontity in the client.
+ *
+ * @param options - Defined in {@link ClientOptions}.
+ */
+const client = async ({ packages, isHmr = false }: ClientOptions) => {
   if (typeof window !== "undefined" && window["Proxy"]) {
     // Hydrate Connect state.
     const stateElement = document.getElementById("__FRONTITY_CONNECT_STATE__");
@@ -13,18 +35,17 @@ export default async ({ packages }) => {
         "State for Frontity Connect hydratation not found. If you need help please visit https://community.frontity.org."
       );
     } else {
-      const isHMR = !!window["frontity"];
       // Get a merged object with roots, fills, state, actions...
       const store = createStore({
         // Use initial state from server only if we are not in a HMR reload.
-        state: isHMR
+        state: isHmr
           ? getSnapshot(window["frontity"].state)
           : JSON.parse(stateElement.innerHTML),
         packages,
       });
 
       // If we are in a HMR refresh, set the flag.
-      if (isHMR) store.state.frontity.hmr = true;
+      if (isHmr) store.state.frontity.hmr = true;
 
       store.libraries.frontity.App = function FrontityApp() {
         return <App store={store} />;
@@ -71,3 +92,5 @@ export default async ({ packages }) => {
     );
   }
 };
+
+export default client;
