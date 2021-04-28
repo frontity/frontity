22cacc73227f213d9016a0ffdf40833958a88d59
Michal Czaplinski <mmczaplinski@gmail.com>
2021-04-28 13:35:33 -0500

    CLI: Add the missing `"` in the commit message

--- packages/frontity/src/steps/index.ts
+++ packages/frontity/src/steps/index.ts
@@ -326,7 +326,7 @@ export const initializeGit = async (path: string) => {
   await writeFile(filePath, fileTemplate);
   await promisify(exec)("git init", { cwd: path });
   await promisify(exec)("git add .", { cwd: path });
-  await promisify(exec)('git commit -m "Initialized with Frontity', {
+  await promisify(exec)('git commit -m "Initialized with Frontity"', {
     cwd: path,
   });
 };
