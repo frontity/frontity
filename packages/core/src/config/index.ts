c6fc6003d483b56da386c19d1eed3454e30795f3
cristianbote <botecristian@yahoo.com>
2021-05-04 12:33:47 +0300

    Add types

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -1,4 +1,11 @@
-import { Mode, Config, EntryPoints } from "../../types";
+import {
+  Mode,
+  Config,
+  EntryPoints,
+  FrontityConfig,
+  BabelConfigs,
+  WebpackConfigs,
+} from "../../types";
 import getBabel from "./babel";
 import getWebpack from "./webpack";
 import getFrontity from "./frontity";
@@ -37,6 +44,55 @@ interface ConfigOptions {
 }
 
 /**
+ * Interface for configuration function.
+ */
+interface ConfigurationFunctionArguments {
+  /**
+   * The running mode.
+   */
+  mode: Mode;
+
+  /**
+   * The configuration object.
+   */
+  config: Record<string, any>;
+
+  /**
+   * The current target of the config.
+   */
+  target?: string;
+}
+
+/**
+ * The configuration function.
+ */
+type ConfigurationFunction = (arg0: ConfigurationFunctionArguments) => void;
+
+/**
+ * Runs the config for each entry.
+ *
+ * @param config - The config.
+ * @param entries - The list of entries.
+ * @param mode - The current framework mode.
+ */
+const runEntriesWithConfig = (
+  config: FrontityConfig,
+  entries: any[],
+  mode: Mode
+) => {
+  if (entries && entries.length) {
+    entries.forEach((configuration: ConfigurationFunction) => {
+      // And call the configuration function with the current
+      // config, target and mode.
+      configuration({
+        config,
+        mode,
+      });
+    });
+  }
+};
+
+/**
  * Runs through the source targets and calls the entries configurations
  * with the current target configuration, target name and mode.
  *
@@ -44,13 +100,17 @@ interface ConfigOptions {
  * @param entries - The entries for the current target.
  * @param mode - The current running mode.
  */
-const runForEachTarget = (source, entries, mode) => {
+const runForEachTarget = (
+  source: WebpackConfigs | BabelConfigs | FrontityConfig,
+  entries: any[],
+  mode: Mode
+) => {
   // If there are entries to be ran.
   if (entries && entries.length) {
     // Iterate for each target inside the source.
     for (const target in source) {
       // And run through each entry configuration.
-      entries.forEach((configuration) => {
+      entries.forEach((configuration: ConfigurationFunction) => {
         // And call the configuration function with the current
         // config, target and mode.
         configuration({
@@ -76,9 +136,13 @@ const config = ({
   entryPoints,
   publicPath,
   analyze = false,
-  extraConfigurations = { babel: [], webpack: [] },
+  extraConfigurations = { babel: [], webpack: [], frontity: [] },
 }: ConfigOptions): Config => {
   const frontity = getFrontity();
+
+  // Run the entries for frontity.
+  runEntriesWithConfig(frontity, extraConfigurations.frontity, mode);
+
   const babel = getBabel();
 
   // Runs the extra configurations for babel, if any.
