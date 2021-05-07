c7c0c15b9eebaee10de6cd36e3ffc74e58711048
Michal Czaplinski <mmczaplinski@gmail.com>
2021-05-06 22:54:19 -0500

    CLI: Update the gh-actions file

--- packages/frontity/e2e/gh-action.ts
+++ packages/frontity/e2e/gh-action.ts
@@ -2,46 +2,53 @@
 
 import { readFile } from "fs-extra";
 import { resolve as resolvePath } from "path";
-import { testContainer } from "./utils";
+import execa from "execa";
 
 // We need to set a high timeout because building the docker container and
 // running `frontity create` takes a long time.
 jest.setTimeout(180000);
 
-test(
-  "in a container with git installed and configured & when a git repo already exists",
-  testContainer(async ({ runCommand }) => {
-    await runCommand(
-      `node_modules/.bin/frontity create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
-      { stdio: "inherit" }
-    );
+/**
+ * A helper to run CLI commands.
+ *
+ * @param cmd - The command to run.
+ * @returns The stdout of the command.
+ */
+const runCommand = async (cmd: string) => {
+  const { stdout } = await execa.command(cmd, {
+    stdio: "pipe",
+  });
+  return stdout;
+};
 
-    let output: string;
+test("in a container with git installed and configured & when a git repo already exists", async () => {
+  await execa(
+    `packages/frontity/node_modules/.bin/frontity create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
+    { stdio: "inherit" }
+  );
 
-    if (process.platform === "win32") {
-      output = await runCommand("dir /a:hd test-frontity-app");
-      expect(output).toMatchInlineSnapshot();
-    } else {
-      output = await runCommand("ls -a test-frontity-app");
-      expect(output).toMatchInlineSnapshot();
-    }
+  let output: string;
 
-    let command: string;
-    if (process.platform === "win32") {
-      command = "type test-frontity-app/.gitignore";
-    } else {
-      command = "cat test-frontity-app/.gitignore";
-    }
+  if (process.platform === "win32") {
+    output = await runCommand("dir /a:hd test-frontity-app");
+    expect(output).toMatchInlineSnapshot();
+  } else {
+    output = await runCommand("ls -a test-frontity-app");
+    expect(output).toMatchInlineSnapshot();
+  }
 
-    // The .gitignore should be the same as the template file.
-    const gitignore = await runCommand(command);
-    expect(gitignore).toEqual(
-      await readFile(
-        resolvePath(__dirname, "../templates/gitignore-template"),
-        {
-          encoding: "utf8",
-        }
-      )
-    );
-  })
-);
+  let command: string;
+  if (process.platform === "win32") {
+    command = "type test-frontity-app/.gitignore";
+  } else {
+    command = "cat test-frontity-app/.gitignore";
+  }
+
+  // The .gitignore should be the same as the template file.
+  const gitignore = await runCommand(command);
+  expect(gitignore).toEqual(
+    await readFile(resolvePath(__dirname, "../templates/gitignore-template"), {
+      encoding: "utf8",
+    })
+  );
+});
