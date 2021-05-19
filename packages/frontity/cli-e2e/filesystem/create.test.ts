72345e326f3e089b410bae4252de0116cb11a153
Michal Czaplinski <mmczaplinski@gmail.com>
2021-05-19 12:37:46 -0500

    CLI-e2e: Add the `describe()` back.

--- packages/frontity/cli-e2e/filesystem/create.test.ts
+++ packages/frontity/cli-e2e/filesystem/create.test.ts
@@ -2,14 +2,15 @@ import { readdir, readFile, remove } from "fs-extra";
 import { resolve as resolvePath } from "path";
 import execa from "execa";
 
-it("should create the initial files", async () => {
-  try {
-    await execa.command(
-      `ts-node src/cli/index.ts create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
-      { stdio: "inherit" }
-    );
+describe("npx frontity create", () => {
+  it("should create the initial files", async () => {
+    try {
+      await execa.command(
+        `ts-node src/cli/index.ts create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
+        { stdio: "inherit" }
+      );
 
-    expect(await readdir("test-frontity-app")).toMatchInlineSnapshot(`
+      expect(await readdir("test-frontity-app")).toMatchInlineSnapshot(`
         Array [
           ".gitignore",
           "README.md",
@@ -20,27 +21,28 @@ it("should create the initial files", async () => {
           "package.json",
           "packages",
         ]
-      `);
-  } finally {
-    await remove("test-frontity-app");
-  }
-});
+        `);
+    } finally {
+      await remove("test-frontity-app");
+    }
+  });
 
-it("should add a .gitignore file even if inside a git repo", async () => {
-  try {
-    await execa.command(
-      `ts-node src/cli/index.ts create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
-      { stdio: "inherit" }
-    );
+  it("should add a .gitignore file even if inside a git repo", async () => {
+    try {
+      await execa.command(
+        `ts-node src/cli/index.ts create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
+        { stdio: "inherit" }
+      );
 
-    // The .gitignore should be the same as the template file.
-    const gitignore = await readFile("test-frontity-app/.gitignore", "utf8");
-    const template = await readFile(
-      resolvePath(__dirname, "../../templates/gitignore-template"),
-      { encoding: "utf8" }
-    );
-    expect(gitignore).toEqual(template);
-  } finally {
-    await remove("test-frontity-app");
-  }
+      // The .gitignore should be the same as the template file.
+      const gitignore = await readFile("test-frontity-app/.gitignore", "utf8");
+      const template = await readFile(
+        resolvePath(__dirname, "../../templates/gitignore-template"),
+        { encoding: "utf8" }
+      );
+      expect(gitignore).toEqual(template);
+    } finally {
+      await remove("test-frontity-app");
+    }
+  });
 });
