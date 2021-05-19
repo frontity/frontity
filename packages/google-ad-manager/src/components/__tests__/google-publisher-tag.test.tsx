09d389770e9a8d1db82c1ffb1144c900fb8e0840
David Arenas <darensr@gmail.com>
2021-05-19 19:31:37 +0200

    GAdManager: Refactor `GooglePublisherTag`

--- packages/google-ad-manager/src/components/__tests__/google-publisher-tag.test.tsx
+++ packages/google-ad-manager/src/components/__tests__/google-publisher-tag.test.tsx
@@ -1,44 +1,63 @@
 import TestRenderer from "react-test-renderer";
+import { HelmetProvider } from "frontity";
+import { FilledContext } from "react-helmet-async";
 import GooglePublisherTag from "../google-publisher-tag";
 
+/**
+ * Render a mocked instance of the Google Analytics' root component.
+ *
+ * @param children - Frontity state object.
+ * @returns The rendered component in JSON format, along with the head
+ * attributes.
+ */
+const renderApp = (children: React.ReactChild) => {
+  const helmetContext = {};
+  const rendered = TestRenderer.create(
+    <HelmetProvider context={helmetContext}>{children}</HelmetProvider>
+  ).toJSON();
+  const head = (helmetContext as FilledContext).helmet;
+
+  return { rendered, head };
+};
+
 describe("GooglePublisherTag", () => {
-  test("should use `id` as the container ID if data is not specified", () => {
-    const gpt = TestRenderer.create(
+  it("should use `id` as the container ID if data is not specified", () => {
+    const { rendered } = renderApp(
       <GooglePublisherTag
         id="gpt-id-123"
         unit="/1234567/sport/"
         size={[320, 100]}
       />
-    ).toJSON();
+    );
 
-    expect(gpt).toMatchInlineSnapshot(`
+    expect(rendered).toMatchInlineSnapshot(`
       <div
-        className="css-urmuyr-GooglePublisherTag"
+        className="css-edpvri-GooglePublisherTag"
         id="gpt-id-123"
       />
     `);
   });
 
-  test("should append `link` to the container ID if data is specified", () => {
-    const gpt = TestRenderer.create(
+  it("should append `link` to the container ID if data is specified", () => {
+    const { rendered } = renderApp(
       <GooglePublisherTag
         id="gpt-id-123"
         unit="/1234567/sport/"
         size={[320, 100]}
         data={{ link: "/2020/08/post-with-long-link/" }}
       />
-    ).toJSON();
+    );
 
-    expect(gpt).toMatchInlineSnapshot(`
+    expect(rendered).toMatchInlineSnapshot(`
       <div
-        className="css-urmuyr-GooglePublisherTag"
+        className="css-edpvri-GooglePublisherTag"
         id="gpt-id-123_2020_08_post-with-long-link"
       />
     `);
   });
 
   test("should render the container with the minimum size", () => {
-    const gpt = TestRenderer.create(
+    const { rendered } = renderApp(
       <GooglePublisherTag
         id="gpt-id-123"
         unit="/1234567/sport/"
@@ -47,11 +66,11 @@ describe("GooglePublisherTag", () => {
           [300, 600],
         ]}
       />
-    ).toJSON();
+    );
 
-    expect(gpt).toMatchInlineSnapshot(`
+    expect(rendered).toMatchInlineSnapshot(`
       <div
-        className="css-1o73ait-GooglePublisherTag"
+        className="css-1g79uk7-GooglePublisherTag"
         id="gpt-id-123"
       />
     `);
