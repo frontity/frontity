fe4ef789659b0afcd4bb45aea1bda67e5a0ed5c3
David Arenas <darensr@gmail.com>
2021-05-28 14:44:17 +0200

    GAdManager: Simplify `AmpAd`

--- packages/google-ad-manager/src/components/amp-ad.tsx
+++ packages/google-ad-manager/src/components/amp-ad.tsx
@@ -2,29 +2,6 @@ import { Head } from "frontity";
 import { AmpAdProps } from "../../types";
 
 /**
- * Turn prop names from camelCase into kebab-case, prefixing `data-`. Also,
- * filter out those props with value `undefined`.
- *
- * @param props - Component props.
- * @returns Filtered props.
- */
-export const getDataProps = (props: Record<string, any>): Record<string, any> =>
-  Object.entries(props)
-    .filter(([, value]) => value !== undefined)
-    .map(([key, value]) => [toKebabCase(key, { prefix: "data-" }), value])
-    .reduce((output, [key, value]) => ((output[key] = value), output), {});
-
-/**
- * Convert string from camelCase to kebab-case.
- *
- * @param camelCase - String in camelCase format.
- * @param options - Options object.
- * @returns String in kebab-case, with the specified prefix.
- */
-export const toKebabCase = (camelCase: string, { prefix = "" } = {}) =>
-  prefix + camelCase.replace(/[A-Z]/g, (char) => `-${char.toLowerCase()}`);
-
-/**
  * AMP ad tag for Google Ad Manager.
  *
  * @param props - Props of type {@link AmpAdProps}.
@@ -34,9 +11,12 @@ const AmpAd: React.FC<AmpAdProps> = ({
   width,
   height,
   layout,
-  json = {},
-  children,
-  ...otherProps
+  json,
+  slot,
+  overrideWidth,
+  overrideHeight,
+  multiSize,
+  multiSizeValidation,
 }) => (
   <>
     <Head>
@@ -51,8 +31,12 @@ const AmpAd: React.FC<AmpAdProps> = ({
       width={width}
       height={height}
       layout={layout}
-      json={JSON.stringify(json)}
-      {...getDataProps(otherProps)}
+      json={json && JSON.stringify(json)}
+      data-slot={slot}
+      data-override-width={overrideWidth}
+      data-override-height={overrideHeight}
+      data-multi-size={multiSize}
+      data-multi-size-validation={multiSizeValidation}
     />
   </>
 );
