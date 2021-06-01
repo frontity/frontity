86aedc648d92459d8fd116af883504920693dbec
Michal Czaplinski <mmczaplinski@gmail.com>
2021-06-01 09:53:19 -0500

    e2e&mars-theme: Add additional e2e test cases

--- e2e/integration/wordpress-01/mars-theme-amp.spec.ts
+++ e2e/integration/wordpress-01/mars-theme-amp.spec.ts
@@ -1,19 +1,30 @@
-import expect from "expect";
 import type { taskTypes } from "../../plugins";
 
 const task: taskTypes = cy.task;
 
-describe("AMP", () => {
+describe("mars-theme with AMP", () => {
   before(() => {
     task("loadDatabase", {
       path: "./wp-data/amp-mars-theme/data.sql",
     });
   });
 
-  it("amp-iframe", () => {
+  it("front page", () => {
     const url = "http://localhost:3001/?frontity_name=amp-mars-theme";
 
     cy.validateAMP(url);
     cy.visit(url);
+
+    cy.get("section > article").should("have.length", 10);
+  });
+
+  it("individual post page", () => {
+    const url =
+      "http://localhost:3001/block-image/?frontity_name=amp-mars-theme";
+
+    cy.validateAMP(url);
+    cy.visit(url);
+
+    cy.get("figure.wp-block-image > amp-img").should("have.descendants", "img");
   });
 });
