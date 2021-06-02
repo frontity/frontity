f68fa5f32ce525b5a9c623e2e3e63f8eaf537eed
Michal Czaplinski <mmczaplinski@gmail.com>
2021-06-02 14:35:39 -0500

    A bunch of changes to e2e-cli tests that might be undone later

--- packages/frontity/cli-e2e/filesystem/create.test.ts
+++ packages/frontity/cli-e2e/filesystem/create.test.ts
@@ -1,17 +1,34 @@
-import { readdir, readFile, remove } from "fs-extra";
-import { resolve as resolvePath } from "path";
+import { readdir, readFile } from "fs-extra";
+import { resolve as resolvePath, join } from "path";
 import execa from "execa";
+import tempy from "tempy";
+
+beforeAll(async () => {
+  // Initialize a test user needed for commits.
+  await execa.command('git config --global user.email "user@frontity.com"', {
+    stdio: "inherit",
+    shell: true,
+  });
+  await execa.command('git config --global user.name "Test User"', {
+    stdio: "inherit",
+    shell: true,
+  });
+});
 
 describe("npx frontity create", () => {
+  const cliPath = join(process.cwd(), "dist/src/cli/index.js");
+
   it("should create the initial files", async () => {
-    try {
+    await tempy.directory.task(async (tempFolder) => {
       await execa.command(
-        `ts-node src/cli/index.ts create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
-        { stdio: "inherit" }
+        `cd ${tempFolder} && node ${cliPath} create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
+        { stdio: "inherit", shell: true }
       );
 
-      expect(await readdir("test-frontity-app")).toMatchInlineSnapshot(`
+      expect(await readdir(join(tempFolder, "test-frontity-app")))
+        .toMatchInlineSnapshot(`
         Array [
+          ".git",
           ".gitignore",
           "README.md",
           "favicon.ico",
@@ -21,28 +38,27 @@ describe("npx frontity create", () => {
           "package.json",
           "packages",
         ]
-        `);
-    } finally {
-      await remove("test-frontity-app");
-    }
+      `);
+    });
   });
 
-  it("should add a .gitignore file even if inside a git repo", async () => {
-    try {
+  it("should add a .gitignore file", async () => {
+    await tempy.directory.task(async (tempFolder) => {
       await execa.command(
-        `ts-node src/cli/index.ts create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
-        { stdio: "inherit" }
+        `cd ${tempFolder} && node ${cliPath} create --no-prompt --theme @frontity/mars-theme test-frontity-app`,
+        { stdio: "inherit", shell: true }
       );
 
       // The .gitignore should be the same as the template file.
-      const gitignore = await readFile("test-frontity-app/.gitignore", "utf8");
+      const gitignore = await readFile(
+        join(tempFolder, "test-frontity-app", ".gitignore"),
+        "utf8"
+      );
       const template = await readFile(
         resolvePath(__dirname, "../../templates/gitignore-template"),
         { encoding: "utf8" }
       );
       expect(gitignore).toEqual(template);
-    } finally {
-      await remove("test-frontity-app");
-    }
+    });
   });
 });
