750307976b043752b0d25d10bfeaa83b0f9bc326
Luis Herranz <luisherranz@gmail.com>
2021-06-02 15:34:37 +0200

    Refactor module tests

--- packages/core/src/config/webpack/__tests__/modules.tests.ts
+++ packages/core/src/config/webpack/__tests__/modules.tests.ts
@@ -1,123 +1,63 @@
 import { exclude } from "../modules";
 
 describe("Transpilation excludes", () => {
-  it("should exclude webpack from transpilation (Unix)", () => {
-    expect(exclude.some((regex) => regex.exec("../webpack"))).toBe(true);
-    expect(exclude.some((regex) => regex.exec("node_modules/webpack"))).toBe(
-      true
-    );
-    expect(exclude.some((regex) => regex.exec("node_modules/webpack/"))).toBe(
-      true
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules/webpack/internal.js"))
-    ).toBe(true);
+  it("should exclude webpack from transpilation", () => {
+    const paths = [
+      "../webpack",
+      "node_modules/webpack",
+      "node_modules/webpack/",
+      "node_modules/webpack/internal.js",
+    ];
+    for (const path of paths) {
+      expect(exclude.some((regex) => regex.exec(path))).toBe(true);
+      expect(
+        exclude.some((regex) => regex.exec(path.replace(/\//g, "\\")))
+      ).toBe(true);
+    }
   });
 
-  it("should exclude webpack from transpilation (Windows)", () => {
-    expect(exclude.some((regex) => regex.exec("..\\webpack"))).toBe(true);
-    expect(exclude.some((regex) => regex.exec("node_modules\\webpack"))).toBe(
-      true
-    );
-    expect(exclude.some((regex) => regex.exec("node_modules\\webpack\\"))).toBe(
-      true
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules\\webpack\\internal.js"))
-    ).toBe(true);
+  it("should exclude core-js from transpilation", () => {
+    const paths = [
+      "../core-js",
+      "node_modules/core-js",
+      "node_modules/core-js/",
+      "node_modules/core-js/internal.js",
+    ];
+    for (const path of paths) {
+      expect(exclude.some((regex) => regex.exec(path))).toBe(true);
+      expect(
+        exclude.some((regex) => regex.exec(path.replace(/\//g, "\\")))
+      ).toBe(true);
+    }
   });
 
-  it("should exclude core-js from transpilation (Unix)", () => {
-    expect(exclude.some((regex) => regex.exec("../core-js"))).toBe(true);
-    expect(exclude.some((regex) => regex.exec("node_modules/core-js"))).toBe(
-      true
-    );
-    expect(exclude.some((regex) => regex.exec("node_modules/core-js/"))).toBe(
-      true
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules/core-js/internal.js"))
-    ).toBe(true);
+  it("should exclude regenerator-runtime from transpilation", () => {
+    const paths = [
+      "../regenerator-runtime",
+      "node_modules/regenerator-runtime",
+      "node_modules/regenerator-runtime/",
+      "node_modules/regenerator-runtime/internal.js",
+    ];
+    for (const path of paths) {
+      expect(exclude.some((regex) => regex.exec(path))).toBe(true);
+      expect(
+        exclude.some((regex) => regex.exec(path.replace(/\//g, "\\")))
+      ).toBe(true);
+    }
   });
 
-  it("should exclude core-js from transpilation (Windows)", () => {
-    expect(exclude.some((regex) => regex.exec("..\\core-js"))).toBe(true);
-    expect(exclude.some((regex) => regex.exec("node_modules\\core-js"))).toBe(
-      true
-    );
-    expect(exclude.some((regex) => regex.exec("node_modules\\core-js\\"))).toBe(
-      true
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules\\core-js\\internal.js"))
-    ).toBe(true);
-  });
-
-  it("should exclude regenerator-runtime from transpilation (Unix)", () => {
-    expect(exclude.some((regex) => regex.exec("../regenerator-runtime"))).toBe(
-      true
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules/regenerator-runtime"))
-    ).toBe(true);
-    expect(
-      exclude.some((regex) => regex.exec("node_modules/regenerator-runtime/"))
-    ).toBe(true);
-    expect(
-      exclude.some((regex) =>
-        regex.exec("node_modules/regenerator-runtime/internal.js")
-      )
-    ).toBe(true);
-  });
-
-  it("should exclude regenerator-runtime from transpilation (Windows)", () => {
-    expect(exclude.some((regex) => regex.exec("..\\regenerator-runtime"))).toBe(
-      true
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules\\regenerator-runtime"))
-    ).toBe(true);
-    expect(
-      exclude.some((regex) => regex.exec("node_modules\\regenerator-runtime\\"))
-    ).toBe(true);
-    expect(
-      exclude.some((regex) =>
-        regex.exec("node_modules\\regenerator-runtime\\internal.js")
-      )
-    ).toBe(true);
-  });
-
-  it("should not exclude package with webpack names from transpilation (Unix)", () => {
-    expect(exclude.some((regex) => regex.exec("../my-webpack-package"))).toBe(
-      false
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules/my-webpack-package"))
-    ).toBe(false);
-    expect(
-      exclude.some((regex) => regex.exec("node_modules/my-webpack-package/"))
-    ).toBe(false);
-    expect(
-      exclude.some((regex) =>
-        regex.exec("node_modules/my-webpack-package/internal.js")
-      )
-    ).toBe(false);
-  });
-
-  it("should not exclude package with webpack names from transpilation (Windows)", () => {
-    expect(exclude.some((regex) => regex.exec("..\\my-webpack-package"))).toBe(
-      false
-    );
-    expect(
-      exclude.some((regex) => regex.exec("node_modules\\my-webpack-package"))
-    ).toBe(false);
-    expect(
-      exclude.some((regex) => regex.exec("node_modules\\my-webpack-package\\"))
-    ).toBe(false);
-    expect(
-      exclude.some((regex) =>
-        regex.exec("node_modules\\my-webpack-package\\internal.js")
-      )
-    ).toBe(false);
+  it("should not exclude package with webpack names from transpilation", () => {
+    const paths = [
+      "../my-webpack-package",
+      "node_modules/my-webpack-package",
+      "node_modules/my-webpack-package/",
+      "node_modules/my-webpack-package/internal.js",
+    ];
+    for (const path of paths) {
+      expect(exclude.some((regex) => regex.exec(path))).toBe(false);
+      expect(
+        exclude.some((regex) => regex.exec(path.replace(/\//g, "\\")))
+      ).toBe(false);
+    }
   });
 });
