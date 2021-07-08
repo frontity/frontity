b129476246c5e38d3d34437b4a118b12ef988c57
Michal Czaplinski <mmczaplinski@gmail.com>
2021-07-08 17:45:39 -0500

    Update the formatting of the TSDocs

--- packages/mars-theme-typescript/src/index.ts
+++ packages/mars-theme-typescript/src/index.ts
@@ -7,16 +7,14 @@ import link from "@frontity/html2react/processors/link";
 const marsThemeTypeScript: MarsThemeTypeScript = {
   name: "@frontity/mars-theme-typescript",
   roots: {
-    
-/**
+    /**
      * In Frontity, any package can add React components to the site.
      * We use roots for that, scoped to the `theme` namespace.
      */
     theme: Theme,
   },
   state: {
-    
-/**
+    /**
      * State is where the packages store their default settings and other
      * relevant state. It is scoped to the `theme` namespace.
      */
@@ -31,8 +29,7 @@ const marsThemeTypeScript: MarsThemeTypeScript = {
     },
   },
 
-  
-/**
+  /**
    * Actions are functions that modify the state or deal with other parts of
    * Frontity like libraries.
    */
@@ -48,8 +45,7 @@ const marsThemeTypeScript: MarsThemeTypeScript = {
   },
   libraries: {
     html2react: {
-      
-/**
+      /**
        * Add a processor to `html2react` so it processes the `<img>` tags
        * and internal link inside the content HTML.
        * You can add your own processors too.
