bb6f84bb0701b8f7d00dac3c8291879b4020be40
Luis Herranz <luisherranz@gmail.com>
2022-02-18 15:50:12 +0100

    Disable E2E tests that use Embedded mode

--- e2e/integration/wordpress-01/client-server-link.spec.ts
+++ e2e/integration/wordpress-01/client-server-link.spec.ts
@@ -1,78 +1,78 @@
 import type { taskTypes } from "../../plugins";
 const task: taskTypes = cy.task;
 
-describe("Tiny Router", () => {
-  before(() => {
-    // Go first to the main URL to avoid a restart when the WordPress site is
-    // visited (baseUrl is different here).
-    cy.visit("http://localhost:8080");
+// describe("Tiny Router", () => {
+//   before(() => {
+//     // Go first to the main URL to avoid a restart when the WordPress site is
+//     // visited (baseUrl is different here).
+//     cy.visit("http://localhost:8080");
 
-    task("installPlugin", { name: "code-snippets" });
-    task("installPlugin", {
-      name: "https://github.com/frontity/frontity-embedded/archive/master.zip",
-    });
-    task("loadDatabase", {
-      path: "./wp-data/tiny-router/client-server-mismatch.sql",
-    });
-  });
+//     task("installPlugin", { name: "code-snippets" });
+//     task("installPlugin", {
+//       name: "https://github.com/frontity/frontity-embedded/archive/master.zip",
+//     });
+//     task("loadDatabase", {
+//       path: "./wp-data/tiny-router/client-server-mismatch.sql",
+//     });
+//   });
 
-  after(() => {
-    task("resetDatabase");
-    task("removeAllPlugins");
-  });
+//   after(() => {
+//     task("resetDatabase");
+//     task("removeAllPlugins");
+//   });
 
-  it("should work if there's a link mismatch between the server and the client", () => {
-    cy.visit("http://localhost:8080/?utm=client&frontity_name=wp-basic-tests");
+//   it("should work if there's a link mismatch between the server and the client", () => {
+//     cy.visit("http://localhost:8080/?utm=client&frontity_name=wp-basic-tests");
 
-    // This test only checks that the link has actually changed in the server.
-    cy.window()
-      .its("frontity")
-      .its("state")
-      .its("frontity")
-      .its("initialLink")
-      .should("equal", "/?utm=SERVER");
+//     // This test only checks that the link has actually changed in the server.
+//     cy.window()
+//       .its("frontity")
+//       .its("state")
+//       .its("frontity")
+//       .its("initialLink")
+//       .should("equal", "/?utm=SERVER");
 
-    // The "utm" parameter should be respected.
-    cy.window()
-      .its("frontity")
-      .its("state")
-      .its("router")
-      .its("link")
-      .should("equal", "/?utm=client");
+//     // The "utm" parameter should be respected.
+//     cy.window()
+//       .its("frontity")
+//       .its("state")
+//       .its("router")
+//       .its("link")
+//       .should("equal", "/?utm=client");
 
-    // The home should render correctly.
-    cy.get("[data-test-id='1']").should("contain.text", "/hello-world");
+//     // The home should render correctly.
+//     cy.get("[data-test-id='1']").should("contain.text", "/hello-world");
 
-    // Go to "/hello-world/"
-    cy.get("[data-test-id='/hello-world/'").click();
-    cy.get("[data-test-id='post']").should("contain.text", "Hello world");
+//     // Go to "/hello-world/"
+//     cy.get("[data-test-id='/hello-world/'").click();
+//     cy.get("[data-test-id='post']").should("contain.text", "Hello world");
 
-    // Go back to the home. It should render the home again.
-    cy.go("back");
+//     // Go back to the home. It should render the home again.
+//     cy.go("back");
 
-    cy.window()
-      .its("frontity")
-      .its("state")
-      .its("router")
-      .its("link")
-      .should("equal", "/?utm=client");
+//     cy.window()
+//       .its("frontity")
+//       .its("state")
+//       .its("router")
+//       .its("link")
+//       .should("equal", "/?utm=client");
 
-    cy.location().its("search").should("equal", "?utm=client");
+//     cy.location().its("search").should("equal", "?utm=client");
 
-    cy.get("[data-test-id='1']").should("contain.text", "/hello-world");
-  });
+//     cy.get("[data-test-id='1']").should("contain.text", "/hello-world");
+//   });
 
-  it("should work when the URL rendered is different than the URL returned", () => {
-    cy.visit("http://localhost:8080/?b=1&a=2&frontity_name=wp-basic-tests");
+//   it("should work when the URL rendered is different than the URL returned", () => {
+//     cy.visit("http://localhost:8080/?b=1&a=2&frontity_name=wp-basic-tests");
 
-    // Check that Frontity has loaded.
-    cy.window().its("frontity").should("not.be.undefined");
-  });
+//     // Check that Frontity has loaded.
+//     cy.window().its("frontity").should("not.be.undefined");
+//   });
 
-  it("should work when the URL rendered is different than the URL returned and the normalized link is different than the server link", () => {
-    cy.visit("http://localhost:8080/hello-world/?frontity_name=wp-basic-tests");
+//   it("should work when the URL rendered is different than the URL returned and the normalized link is different than the server link", () => {
+//     cy.visit("http://localhost:8080/hello-world/?frontity_name=wp-basic-tests");
 
-    // Check that Frontity has loaded.
-    cy.window().its("frontity").should("not.be.undefined");
-  });
-});
+//     // Check that Frontity has loaded.
+//     cy.window().its("frontity").should("not.be.undefined");
+//   });
+// });
