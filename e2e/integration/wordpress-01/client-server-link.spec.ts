680fc69c0524c737973523247c45728e9151f2c8
Luis Herranz <luisherranz@gmail.com>
2022-02-18 13:07:22 +0100

    Remove visit to see what happens

--- e2e/integration/wordpress-01/client-server-link.spec.ts
+++ e2e/integration/wordpress-01/client-server-link.spec.ts
@@ -3,8 +3,6 @@ const task: taskTypes = cy.task;
 
 describe("Tiny Router", () => {
   before(() => {
-    // Go first to the main URL to avoid a restart when the WordPress site is
-    // visited (baseUrl is different here).
     task("installPlugin", { name: "code-snippets" });
     task("installPlugin", {
       name: "https://github.com/frontity/frontity-embedded/archive/master.zip",
@@ -12,7 +10,6 @@ describe("Tiny Router", () => {
     task("loadDatabase", {
       path: "./wp-data/tiny-router/client-server-mismatch.sql",
     });
-    cy.visit("http://localhost:8080");
   });
 
   after(() => {
