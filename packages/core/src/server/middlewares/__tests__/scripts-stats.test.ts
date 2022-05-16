7fdb9d338083bea2330dcc526343eabf6db865f3
Eduardo Campaña <asrielo@gmail.com>
2022-05-16 22:36:18 +0200

    fixes unit tests

--- packages/core/src/server/middlewares/__tests__/scripts-stats.test.ts
+++ packages/core/src/server/middlewares/__tests__/scripts-stats.test.ts
@@ -1,4 +1,5 @@
 import { scriptsStats } from "../scripts-stats";
+import { init } from "../init";
 import { createKoaContext } from "./__utilities__/create-koa-context";
 import { fakeNext } from "./__utilities__/fake-next";
 
@@ -6,6 +7,7 @@ describe("scriptStats", () => {
   it("should define the stats on context", async () => {
     const ctx: any = createKoaContext();
 
+    await init(ctx, fakeNext);
     await scriptsStats(ctx, fakeNext);
 
     expect(ctx.frontity.stats).toMatchSnapshot();
